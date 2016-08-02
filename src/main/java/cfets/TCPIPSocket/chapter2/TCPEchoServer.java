ac63fb571fbca295f8836834e6a6d3a217cd370c
ykliu <moneyflying_2006@hotmail.com>
2016-08-02 16:18:06 +0800

    tcp id chapter 3

--- src/main/java/cfets/TCPIPSocket/chapter2/TCPEchoServer.java
+++ src/main/java/cfets/TCPIPSocket/chapter2/TCPEchoServer.java
@@ -25,6 +25,7 @@ public class TCPEchoServer {
 
         int recvMsgSize;   // Size of received message
         byte[] receiveBuf = new byte[BUFSIZE];  // Receive buffer
+        byte[] responseBuf = new byte[BUFSIZE];  // Receive buffer
 
         while (true) { // Run forever, accepting and servicing connections
             Socket clntSock = servSock.accept();     // Get client connection
@@ -41,10 +42,10 @@ public class TCPEchoServer {
             OutputStream out = clntSock.getOutputStream();
 
             // Receive until client closes connection, indicated by -1 return
-            while ((recvMsgSize = in.read(receiveBuf)) != -1) {
+            while ((recvMsgSize = in.read(responseBuf)) != -1) {
                 //out.write(123456);
-                out.write(receiveBuf, 0, recvMsgSize);
-                out.write(123456);
+                out.write(responseBuf, 0, recvMsgSize);
+                //out.write(123456);
                 //break;
             }
 
