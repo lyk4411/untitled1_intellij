7612f52c35d3b10fbe130eee7ca0418084a9d6a1
ykliu <moneyflying_2006@hotmail.com>
2017-03-07 09:50:20 +0800

    core java ch 8

---
+++ src/main/java/leetcode/MinimumDepthofBinaryTree.java
@@ -0,0 +1,69 @@
+package leetcode;
+
+import java.util.LinkedList;
+
+/**
+ * Created by lyk on 2017/3/7.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class MinimumDepthofBinaryTree {
+    public static void main(String[] args) {
+        MinimumDepthofBinaryTree mdbt = new MinimumDepthofBinaryTree();
+        TreeNode a1 = new TreeNode(1);
+        TreeNode a2 = new TreeNode(1);
+        TreeNode a3 = new TreeNode(2);
+        TreeNode a4 = new TreeNode(2);
+        TreeNode a5 = new TreeNode(3);
+        TreeNode a6 = new TreeNode(3);
+        TreeNode a7 = new TreeNode(4);
+        TreeNode a8 = new TreeNode(4);
+        TreeNode a9 = new TreeNode(5);
+
+        a1.left = a2;
+        a1.right = a3;
+        a3.left = a4;
+        a3.right = a5;
+        a5.left = a6;
+        a5.right = a7;
+        a7.left = a8;
+        a7.right = a9;
+
+        System.out.println(mdbt.minDepth(a1));
+        System.out.println(mdbt.minDepth(a2));
+        System.out.println(mdbt.minDepth(a3));
+
+
+    }
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        LinkedList<TreeNode> nodes = new LinkedList<TreeNode>();
+        LinkedList<Integer> counts = new LinkedList<Integer>();
+
+        nodes.add(root);
+        counts.add(1);
+
+        while(!nodes.isEmpty()){
+            TreeNode curr = nodes.remove();
+            int count = counts.remove();
+
+            if(curr.left == null && curr.right == null){
+                return count;
+            }
+
+            if(curr.left != null){
+                nodes.add(curr.left);
+                counts.add(count+1);
+            }
+
+            if(curr.right != null){
+                nodes.add(curr.right);
+                counts.add(count+1);
+            }
+        }
+
+        return 0;
+    }
+}
--- src/main/java/leetcode/Triangle.java
+++ src/main/java/leetcode/Triangle.java
@@ -37,6 +37,10 @@ public class Triangle {
 
     public int minimumTotal1(List<List<Integer>> triangle) {
         int[] A = new int[triangle.size()+1];
+        for (int k = 0; k < A.length; k++) {
+            System.out.print(A[k] + ";");
+        }
+        System.out.println();
         for(int i=triangle.size()-1;i>=0;i--){
             for(int j=0;j<triangle.get(i).size();j++){
                 A[j] = Math.min(A[j],A[j+1])+triangle.get(i).get(j);
