1f28b1dcb7a265f05dc971e1f767c21695f30c4c
ykliu <moneyflying_2006@hotmail.com>
2017-03-08 09:45:16 +0800

    core java ch 8

--- src/main/java/leetcode/SingleNumberIII.java
+++ src/main/java/leetcode/SingleNumberIII.java
@@ -6,6 +6,27 @@ package leetcode;
  * Porject name: untitled1
  */
 public class SingleNumberIII {
+    public static void main(String[] args) {
+        SingleNumberIII sniii = new SingleNumberIII();
+        int[] nums = new int[]{1, 2, 1, 3, 2, 5};
+        for (int i = 0; i < 2; i++) {
+            System.out.println(sniii.singleNumber(nums)[i]);
+        }
+    }
     public int[] singleNumber(int[] nums) {
+        int[] ans = new int[2];
+        int diff = 0;
+        for(int num : nums) {
+            diff ^= num;
+        }
+        diff &= -diff;
+        for(int num : nums) {
+            if((num & diff) == 0) {
+                ans[0] ^= num;
+            }else {
+                ans[1] ^= num;
+            }
+        }
+        return ans;
     }
 }
