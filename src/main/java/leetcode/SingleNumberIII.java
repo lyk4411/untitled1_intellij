fcf75686b31db321480f9eaedb17ac1c5213a5a6
ykliu <moneyflying_2006@hotmail.com>
2017-03-08 10:08:13 +0800

    core java ch 8

--- src/main/java/leetcode/SingleNumberIII.java
+++ src/main/java/leetcode/SingleNumberIII.java
@@ -9,8 +9,10 @@ public class SingleNumberIII {
     public static void main(String[] args) {
         SingleNumberIII sniii = new SingleNumberIII();
         int[] nums = new int[]{1, 2, 1, 3, 2, 5};
+
+        //sniii.singleNumber(nums);
         for (int i = 0; i < 2; i++) {
-            System.out.println(sniii.singleNumber(nums)[i]);
+            System.out.println("########" +sniii.singleNumber(nums)[i]);
         }
     }
     public int[] singleNumber(int[] nums) {
@@ -20,11 +22,16 @@ public class SingleNumberIII {
             diff ^= num;
         }
         diff &= -diff;
+        //System.out.println("diff:"+diff);
         for(int num : nums) {
             if((num & diff) == 0) {
+                System.out.println("num111:" + num);
                 ans[0] ^= num;
+                System.out.println("ans[0]:" + ans[0]);
             }else {
+                System.out.println("num222:" + num);
                 ans[1] ^= num;
+                System.out.println("ans[1]:" + ans[1]);
             }
         }
         return ans;
