3d5a974b87eea6e99af41589180a1df8181745b3
liuyongkai <liuyongkai@liuyongkaideMacBook-Pro.local>
2017-03-12 11:07:38 +0800

    IntegertoRoman

---
+++ src/main/java/leetcode/IntegertoRoman.java
@@ -0,0 +1,25 @@
+package main.java.leetcode;
+
+/**
+ * Created by liuyongkai on 17/3/12.
+ */
+public class IntegertoRoman {
+    public static void main(String[] args) {
+        IntegertoRoman ir = new IntegertoRoman();
+        System.out.println(ir.intToRoman(1234));
+    }
+    public static String intToRoman(int num) {
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] strs = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<values.length;i++) {
+            while(num >= values[i]) {
+                num -= values[i];
+                sb.append(strs[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
