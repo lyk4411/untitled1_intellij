f19d4e3156b0b2f9cd529cf3998357b6fadb0922
liuyongkai <liuyongkai@liuyongkaideMacBook-Pro.local>
2017-03-12 10:23:21 +0800

    addtwonumber

---
+++ src/main/java/leetcode/AddTwoNumbers.java
@@ -0,0 +1,63 @@
+package main.java.leetcode;
+
+/**
+ * Created by liuyongkai on 17/3/12.
+ */
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        AddTwoNumbers atn = new AddTwoNumbers();
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = l6;
+
+        ListNode ln1 = new ListNode(1);
+        ListNode ln2 = new ListNode(1);
+        ListNode ln3 = new ListNode(2);
+        ListNode ln4 = new ListNode(2);
+        ListNode ln5 = new ListNode(2);
+        ListNode ln6 = new ListNode(3);
+        ListNode ln7 = new ListNode(4);
+        ListNode ln8 = new ListNode(5);
+        ln1.next = ln2;
+        ln2.next = ln3;
+        ln3.next = ln4;
+        ln4.next = ln5;
+        ln5.next = ln6;
+        ln6.next = ln7;
+        ln7.next = ln8;
+        ListNode ln = atn.addTwoNumbers(l1,ln1);
+        System.out.println(ln);
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+        ListNode sentinel = new ListNode(0);
+        ListNode d = sentinel;
+        int sum = 0;
+        while (c1 != null || c2 != null) {
+            sum /= 10;
+            if (c1 != null) {
+                sum += c1.val;
+                c1 = c1.next;
+            }
+            if (c2 != null) {
+                sum += c2.val;
+                c2 = c2.next;
+            }
+            d.next = new ListNode(sum % 10);
+            d = d.next;
+        }
+        if (sum / 10 == 1)
+            d.next = new ListNode(1);
+        return sentinel.next;
+    }
+}
+
