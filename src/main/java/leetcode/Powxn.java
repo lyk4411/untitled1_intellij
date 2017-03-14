03c249bf04e525057d681d6ffcf4f576f7b328e7
ykliu <moneyflying_2006@hotmail.com>
2017-03-14 17:56:35 +0800

    core java ch 8

--- src/main/java/leetcode/Powxn.java
+++ src/main/java/leetcode/Powxn.java
@@ -8,15 +8,53 @@ package leetcode;
 public class Powxn {
     public static void main(String[] args) {
         Powxn p = new Powxn();
-        System.out.println(p.pow(4.15,8));
+
+        System.out.println(p.myPow(1.00000,-2147483648));
+        System.out.println(p.myPow(2.00000,-2147483648));
+
     }
-    public double pow(double x, int n) {
+
+    public double myPow(double x, int n) {
         if(n == 0)
             return 1;
         if(n<0){
+            if(n == -2147483648) {n = n+2;}
             n = -n;
-            x = 1/x;
+            x = 1.0/x;
         }
-        return (n%2 == 0) ? pow(x*x, n/2) : x*pow(x*x, n/2);
+        return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
     }
+
+//    public double myPow(double x, int n) {
+//        if(n<0){
+//            return 1.0/power(x, -n);
+//        }else {
+//            return power(x, n);
+//        }
+//    }
+//    public double power(double x, int n){
+//        if(n==0)
+//            return 1;
+//        double v = power(x, n/2);
+//        if(n%2==0)
+//            return v*v;
+//        else
+//            return v*v*x;
+//    }
+//    public double myPow(double x, int n){
+//        if(n==0)
+//            return 1;
+//
+//        if(n<0){
+//            return 1.0/myPow(x, -n);
+//        }
+//
+//        double v = myPow(x, n/2);
+//
+//        if(n%2==0){
+//            return v*v;
+//        }else{
+//            return v*v*x;
+//        }
+//    }
 }
