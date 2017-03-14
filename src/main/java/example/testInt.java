0883410b661a6a1ccbce0574e689c0e37b64dedd
ykliu <moneyflying_2006@hotmail.com>
2017-03-14 17:49:57 +0800

    core java ch 8

---
+++ src/main/java/example/testInt.java
@@ -0,0 +1,15 @@
+package example;
+
+/**
+ * Created by lyk on 2017/3/14.
+ * Package name: example
+ * Porject name: untitled1
+ */
+public class testInt {
+    public static void main(String[] args) {
+        int N = -2147483648;
+        int n = - N;
+        System.out.println(N);
+        System.out.println(n);
+    }
+}
