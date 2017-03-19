d808538ddcf85408cc91fa88e30ba92840d3b13c
lyk4411 <moneyflying_2006@hotmail.com>
2017-03-19 10:32:25 +0800

    Update BinaryTreeInorderTraversal.java

--- src/main/java/leetcode/BinaryTreeInorderTraversal.java
+++ src/main/java/leetcode/BinaryTreeInorderTraversal.java
@@ -10,7 +10,26 @@ import java.util.Stack;
 public class BinaryTreeInorderTraversal {
     public static void main(String[] args) {
         BinaryTreeInorderTraversal btit = new BinaryTreeInorderTraversal();
+        TreeNode tn1 = new TreeNode(1);
+        TreeNode tn2 = new TreeNode(2);
+        TreeNode tn3 = new TreeNode(3);
+        TreeNode tn4 = new TreeNode(4);
+        TreeNode tn5 = new TreeNode(5);
+        TreeNode tn6 = new TreeNode(6);
+        TreeNode tn7 = new TreeNode(7);
+        TreeNode tn8 = new TreeNode(8);
+        TreeNode tn9 = new TreeNode(9);
 
+        tn1.left = tn2;
+        tn1.right = tn3;
+        tn3.left = tn4;
+        tn3.right = tn5;
+        tn4.left = tn6;
+        tn4.right = tn7;
+        tn7.left = tn8;
+        tn7.right = tn9;
+        System.out.println(tn1);
+        System.out.println(btit.inorderTraversal(tn1));
     }
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<Integer>();
@@ -34,8 +53,13 @@ public class BinaryTreeInorderTraversal {
 
 
 class TreeNode {
-         int val;
-         TreeNode left;
-         TreeNode right;
-         TreeNode(int x) { val = x; }
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+
+    @Override
+    public String toString() {
+        return ((this.left!= null) ? left.toString():"") + ":" + val + ":" + ((this.right!=null) ? right.toString():"");
+    }
 }
