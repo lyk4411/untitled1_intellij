d3c7ba3db03a8464ce4847d63e628595c8758cff
ykliu <moneyflying_2006@hotmail.com>
2017-03-20 16:31:14 +0800

    core java ch 8

---
+++ src/main/java/leetcode/ConstructBinaryTreefromInorderandPostorderTraversal.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+/**
+ * Created by lyk on 2017/3/20.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class ConstructBinaryTreefromInorderandPostorderTraversal {
+    public static void main(String[] args) {
+        int[] postorder = new int[]{4,5,2,6,7,8,3,1};
+        int[] inorder = new int[]{4,2,5,1,6,7,3,8};
+        ConstructBinaryTreefromInorderandPostorderTraversal cbtipt = new ConstructBinaryTreefromInorderandPostorderTraversal();
+        System.out.println(cbtipt.buildTree(inorder,postorder));
+    }
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        int inStart = 0;
+        int inEnd = inorder.length - 1;
+        int postStart = 0;
+        int postEnd = postorder.length - 1;
+
+        return buildTree(inorder, inStart, inEnd, postorder, postStart, postEnd);
+    }
+
+    public TreeNode buildTree(int[] inorder, int inStart, int inEnd,
+                              int[] postorder, int postStart, int postEnd) {
+        if (inStart > inEnd || postStart > postEnd)
+            return null;
+
+        int rootValue = postorder[postEnd];
+        TreeNode root = new TreeNode(rootValue);
+
+        int k = 0;
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == rootValue) {
+                k = i;
+                break;
+            }
+        }
+
+        root.left = buildTree(inorder, inStart, k - 1, postorder, postStart,
+                postStart + k - (inStart + 1));
+        // Becuase k is not the length, it it need to -(inStart+1) to get the length
+        root.right = buildTree(inorder, k + 1, inEnd, postorder, postStart + k- inStart, postEnd - 1);
+        // postStart+k-inStart = postStart+k-(inStart+1) +1
+
+        return root;
+    }
+}
