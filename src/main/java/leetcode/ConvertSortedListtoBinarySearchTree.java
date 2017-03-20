0a2416e115e8399c7df3c7dca1110653122774bb
ykliu <moneyflying_2006@hotmail.com>
2017-03-20 18:22:17 +0800

    core java ch 8

--- src/main/java/leetcode/ConvertSortedListtoBinarySearchTree.java
+++ src/main/java/leetcode/ConvertSortedListtoBinarySearchTree.java
@@ -26,67 +26,67 @@ public class ConvertSortedListtoBinarySearchTree {
         System.out.println(cslbst.sortedListToBST(ln1));
     }
 
-    static ListNode h;
-
-    public TreeNode sortedListToBST(ListNode head) {
-        if (head == null)
-            return null;
-
-        h = head;
-        int len = getLength(head);
-        return sortedListToBST(0, len - 1);
-    }
-
-    // get list length
-    public int getLength(ListNode head) {
-        int len = 0;
-        ListNode p = head;
-
-        while (p != null) {
-            len++;
-            p = p.next;
-        }
-        return len;
-    }
-
-    // build tree bottom-up
-    public TreeNode sortedListToBST(int start, int end) {
-        if (start > end)
-            return null;
-
-        // mid
-        int mid = (start + end) / 2;
-
-        TreeNode left = sortedListToBST(start, mid - 1);
-        TreeNode root = new TreeNode(h.val);
-        h = h.next;
-        TreeNode right = sortedListToBST(mid + 1, end);
-
-        root.left = left;
-        root.right = right;
-
-        return root;
-    }
-//    public TreeNode sortedListToBST(ListNode head) {
-//        if(head == null)
-//            return null;
-//        ListNode fast = head;
-//        ListNode slow = head;
-//        ListNode prev =null;
-//        while(fast != null && fast.next != null)
-//        {
-//            fast = fast.next.next;
-//            prev =slow;
-//            slow=slow.next;
-//        }
-//        TreeNode root = new TreeNode(slow.val);
-//        if(prev != null)
-//            prev.next = null;
-//        else
-//            head  = null;
+//    static ListNode h;
 //
-//        root.left = sortedListToBST(head);
-//        root.right = sortedListToBST(slow.next);
+//    public TreeNode sortedListToBST(ListNode head) {
+//        if (head == null)
+//            return null;
+//
+//        h = head;
+//        int len = getLength(head);
+//        return sortedListToBST(0, len - 1);
+//    }
+//
+//    // get list length
+//    public int getLength(ListNode head) {
+//        int len = 0;
+//        ListNode p = head;
+//
+//        while (p != null) {
+//            len++;
+//            p = p.next;
+//        }
+//        return len;
+//    }
+//
+//    // build tree bottom-up
+//    public TreeNode sortedListToBST(int start, int end) {
+//        if (start > end)
+//            return null;
+//
+//        // mid
+//        int mid = (start + end) / 2;
+//
+//        TreeNode left = sortedListToBST(start, mid - 1);
+//        TreeNode root = new TreeNode(h.val);
+//        h = h.next;
+//        TreeNode right = sortedListToBST(mid + 1, end);
+//
+//        root.left = left;
+//        root.right = right;
+//
 //        return root;
 //    }
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null)
+            return null;
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev =null;
+        while(fast != null && fast.next != null)
+        {
+            fast = fast.next.next;
+            prev =slow;
+            slow=slow.next;
+        }
+        TreeNode root = new TreeNode(slow.val);
+        if(prev != null)
+            prev.next = null;
+        else
+            head  = null;
+
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+        return root;
+    }
 }
