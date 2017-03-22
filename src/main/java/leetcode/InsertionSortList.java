257a4919d4b0a1e3e5f4fbdf9503875e953e0988
ykliu <moneyflying_2006@hotmail.com>
2017-03-22 18:21:43 +0800

    core java ch 8

--- src/main/java/leetcode/InsertionSortList.java
+++ src/main/java/leetcode/InsertionSortList.java
@@ -26,28 +26,23 @@ public class InsertionSortList {
         System.out.println(isl.insertionSortList(ln1));
     }
     public ListNode insertionSortList(ListNode head) {
-        if( head == null ){
-            return head;
-        }
-
-        ListNode helper = new ListNode(0); //new starter of the sorted list
-        ListNode cur = head; //the node will be inserted
-        ListNode pre = helper; //insert node between pre and pre.next
-        ListNode next = null; //the next node will be inserted
-        //not the end of input list
-        while( cur != null ){
-            next = cur.next;
-            //find the right place to insert
-            while( pre.next != null && pre.next.val < cur.val ){
+        if(head == null)
+            return null;
+        ListNode helper = new ListNode(0);
+        ListNode pre = helper;
+        ListNode cur = head;
+        while(cur!=null)
+        {
+            ListNode next = cur.next;
+            pre = helper;
+            while(pre.next!=null && pre.next.val<cur.val)
+            {
                 pre = pre.next;
             }
-            //insert between pre and pre.next
             cur.next = pre.next;
             pre.next = cur;
-            pre = helper;
             cur = next;
         }
-
         return helper.next;
     }
 }
