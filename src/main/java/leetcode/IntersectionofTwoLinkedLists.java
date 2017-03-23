e9c631774002deb70f8d0596427f84340b4659c4
ykliu <moneyflying_2006@hotmail.com>
2017-03-23 18:07:11 +0800

    core java ch 8

---
+++ src/main/java/leetcode/IntersectionofTwoLinkedLists.java
@@ -0,0 +1,60 @@
+package leetcode;
+
+/**
+ * Created by lyk on 2017/3/23.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class IntersectionofTwoLinkedLists {
+    public static void main(String[] args) {
+        IntersectionofTwoLinkedLists itll = new IntersectionofTwoLinkedLists();
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = l6;
+
+        ListNode ln1 = new ListNode(1);
+        ListNode ln2 = new ListNode(2);
+        ListNode ln3 = new ListNode(3);
+        ListNode ln4 = new ListNode(4);
+        ListNode ln5 = new ListNode(5);
+        ListNode ln6 = new ListNode(6);
+        ln1.next = l5;
+
+        System.out.println(itll.getIntersectionNode(l1,ln1));
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = length(headA), lenB = length(headB);
+        // move headA and headB to the same start point
+        while (lenA > lenB) {
+            headA = headA.next;
+            lenA--;
+        }
+        while (lenA < lenB) {
+            headB = headB.next;
+            lenB--;
+        }
+        // find the intersection until end
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+
+    private int length(ListNode node) {
+        int length = 0;
+        while (node != null) {
+            node = node.next;
+            length++;
+        }
+        return length;
+    }
+}
