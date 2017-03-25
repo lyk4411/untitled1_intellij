cec1ff2abec9a2b140bb45ae6ef731f454e231e9
ykliu <moneyflying_2006@hotmail.com>
2017-03-25 11:09:09 +0800

    core java ch 8

--- src/main/java/leetcode/FirstBadVersion.java
+++ src/main/java/leetcode/FirstBadVersion.java
@@ -13,20 +13,13 @@ public class FirstBadVersion {
 
     }
     public int firstBadVersion(int n) {
-        int low=1, high=n;
-
-        while(low<high) {
-            int mid=low + (high-low)/2;
-            if(isBadVersion(mid)) {
-                high = mid;
-            } else {
-                low = mid + 1;
-
-                System.out.println("mid:"+ mid);
-                System.out.println("low:"+ low);
-            }
+        int start = 1, end = n;
+        while (start < end) {
+            int mid = start + (end-start) / 2;
+            if (!isBadVersion(mid)) start = mid + 1;
+            else end = mid;
         }
-        return low;
+        return start;
     }
 
     private boolean isBadVersion(int mid) {
