9a90653fdf76687b9839ed07b5ee82e79fdc3fe2
ykliu <moneyflying_2006@hotmail.com>
2017-03-27 14:01:44 +0800

    core java ch 8

--- src/main/java/leetcode/FindtheDuplicateNumber.java
+++ src/main/java/leetcode/FindtheDuplicateNumber.java
@@ -8,7 +8,7 @@ package leetcode;
 public class FindtheDuplicateNumber {
     public static void main(String[] args) {
         FindtheDuplicateNumber fdn = new FindtheDuplicateNumber();
-        int[] nums = new int[]{8,7,6,5,4,3,2,1,2};
+        int[] nums = new int[]{8,7,3,1,5,4,3,2,1};
         System.out.println(fdn.findDuplicate(nums));
     }
     public int findDuplicate(int[] nums) {
@@ -18,15 +18,17 @@ public class FindtheDuplicateNumber {
         do{
             slow = nums[slow];
             fast = nums[nums[fast]];
+            System.out.println("slow:" + slow + "  fast:" + fast);
         } while(slow != fast);
-
+        System.out.println("=======================================");
         int find = 0;
 
         while(find != slow){
             slow = nums[slow];
             find = nums[find];
+            System.out.println("slow:" + slow + "  find:" + find);
         }
-        return find;
+        return slow;
     }
 //    public int findDuplicate(int[] nums) {
 //        int l=1,r=nums.length-1;
