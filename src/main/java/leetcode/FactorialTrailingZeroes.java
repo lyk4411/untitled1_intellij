ac0189dfaec02a3cedb2c5ed4145202fc447902f
ykliu <moneyflying_2006@hotmail.com>
2017-03-28 16:10:30 +0800

    core java ch 8

--- src/main/java/leetcode/FactorialTrailingZeroes.java
+++ src/main/java/leetcode/FactorialTrailingZeroes.java
@@ -6,6 +6,10 @@ package leetcode;
  * Porject name: untitled1
  */
 public class FactorialTrailingZeroes {
+    public static void main(String[] args) {
+        FactorialTrailingZeroes ftz = new FactorialTrailingZeroes();
+        System.out.println(ftz.trailingZeroes(10));
+    }
     public int trailingZeroes(int n) {
         int r = 0;
         while (n > 0) {
