8392d1850f79d23187cf7224d03faa14a7ac4ef0
ykliu <moneyflying_2006@hotmail.com>
2017-03-30 18:58:41 +0800

    core java ch 8

--- src/main/java/leetcode/CourseSchedule.java
+++ src/main/java/leetcode/CourseSchedule.java
@@ -1,7 +1,6 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
  * Created by lyk on 2017/3/29.
@@ -14,6 +13,7 @@ public class CourseSchedule {
         int[][] prerequests = new int[][]{{0,3},{3,4},{1,2},{4,2},{1,3}};
         System.out.println(cs.canFinish(5,prerequests));
     }
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         if(prerequisites == null){
             throw new IllegalArgumentException("illegal prerequisites array");
@@ -25,92 +25,40 @@ public class CourseSchedule {
             return true;
         }
 
-        //track visited courses
-        int[] visit = new int[numCourses];
-
-        // use the map to store what courses depend on a course
-        HashMap<Integer,ArrayList<Integer>> map = new HashMap<Integer,ArrayList<Integer>>();
-        for(int[] a: prerequisites){
-            if(map.containsKey(a[1])){
-                map.get(a[1]).add(a[0]);
-            }else{
-                ArrayList<Integer> l = new ArrayList<Integer>();
-                l.add(a[0]);
-                map.put(a[1], l);
-            }
+        // counter for number of prerequisites
+        int[] pCounter = new int[numCourses];
+        for(int i=0; i<len; i++){
+            pCounter[prerequisites[i][0]]++;
         }
+//        for (int i = 0; i < pCounter.length; i++) {
+//            System.out.println(pCounter[i]);
+//        }
 
+        //store courses that have no prerequisites
+        LinkedList<Integer> queue = new LinkedList<Integer>();
         for(int i=0; i<numCourses; i++){
-            if(!canFinishDFS(map, visit, i))
-                return false;
-        }
-
-        return true;
-    }
-
-    private boolean canFinishDFS(HashMap<Integer,ArrayList<Integer>> map, int[] visit, int i){
-        if(visit[i]==-1)
-            return false;
-        if(visit[i]==1)
-            return true;
-
-        visit[i]=-1;
-        if(map.containsKey(i)){
-            for(int j: map.get(i)){
-                if(!canFinishDFS(map, visit, j))
-                    return false;
+            if(pCounter[i]==0){
+                queue.add(i);
             }
         }
 
-        visit[i]=1;
+        // number of courses that have no prerequisites
+        int numNoPre = queue.size();
 
-        return true;
+        while(!queue.isEmpty()){
+            int top = queue.remove();
+            for(int i=0; i<len; i++){
+                // if a course's prerequisite can be satisfied by a course in queue
+                if(prerequisites[i][1]==top){
+                    pCounter[prerequisites[i][0]]--;
+                    if(pCounter[prerequisites[i][0]]==0){
+                        numNoPre++;
+                        queue.add(prerequisites[i][0]);
+                    }
+                }
+            }
+        }
+
+        return numNoPre == numCourses;
     }
-//    public boolean canFinish(int numCourses, int[][] prerequisites) {
-//        if(prerequisites == null){
-//            throw new IllegalArgumentException("illegal prerequisites array");
-//        }
-//
-//        int len = prerequisites.length;
-//
-//        if(numCourses == 0 || len == 0){
-//            return true;
-//        }
-//
-//        // counter for number of prerequisites
-//        int[] pCounter = new int[numCourses];
-//        for(int i=0; i<len; i++){
-//            pCounter[prerequisites[i][0]]++;
-//        }
-////        for (int i = 0; i < pCounter.length; i++) {
-////            System.out.println(pCounter[i]);
-////        }
-//
-//        //store courses that have no prerequisites
-//        LinkedList<Integer> queue = new LinkedList<Integer>();
-//        for(int i=0; i<numCourses; i++){
-//            if(pCounter[i]==0){
-//                queue.add(i);
-//            }
-//        }
-//
-//        // number of courses that have no prerequisites
-//        int numNoPre = queue.size();
-//
-//        while(!queue.isEmpty()){
-//            int top = queue.remove();
-//            for(int i=0; i<len; i++){
-//                // if a course's prerequisite can be satisfied by a course in queue
-//                if(prerequisites[i][1]==top){
-//                    pCounter[prerequisites[i][0]]--;
-//                    if(pCounter[prerequisites[i][0]]==0){
-//                        numNoPre++;
-//                        queue.add(prerequisites[i][0]);
-//                    }
-//                }
-//            }
-//        }
-//
-//        return numNoPre == numCourses;
-//    }
 }
