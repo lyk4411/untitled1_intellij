dc645935528f977766c010be426843a384b58b30
ykliu <moneyflying_2006@hotmail.com>
2017-03-30 18:27:53 +0800

    core java ch 8

---
+++ src/main/java/leetcode/CourseSchedule.java
@@ -0,0 +1,63 @@
+package leetcode;
+
+import java.util.LinkedList;
+
+/**
+ * Created by lyk on 2017/3/29.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class CourseSchedule {
+    public static void main(String[] args) {
+        CourseSchedule cs = new CourseSchedule();
+        int[][] prerequests = new int[][]{{0,3},{3,4},{1,2},{4,2},{1,3}};
+        System.out.println(cs.canFinish(5,prerequests));
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if(prerequisites == null){
+            throw new IllegalArgumentException("illegal prerequisites array");
+        }
+
+        int len = prerequisites.length;
+
+        if(numCourses == 0 || len == 0){
+            return true;
+        }
+
+        // counter for number of prerequisites
+        int[] pCounter = new int[numCourses];
+        for(int i=0; i<len; i++){
+            pCounter[prerequisites[i][0]]++;
+        }
+        for (int i = 0; i < pCounter.length; i++) {
+            System.out.println(pCounter[i]);
+        }
+
+        //store courses that have no prerequisites
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        for(int i=0; i<numCourses; i++){
+            if(pCounter[i]==0){
+                queue.add(i);
+            }
+        }
+
+        // number of courses that have no prerequisites
+        int numNoPre = queue.size();
+
+        while(!queue.isEmpty()){
+            int top = queue.remove();
+            for(int i=0; i<len; i++){
+                // if a course's prerequisite can be satisfied by a course in queue
+                if(prerequisites[i][1]==top){
+                    pCounter[prerequisites[i][0]]--;
+                    if(pCounter[prerequisites[i][0]]==0){
+                        numNoPre++;
+                        queue.add(prerequisites[i][0]);
+                    }
+                }
+            }
+        }
+
+        return numNoPre == numCourses;
+    }
+}
