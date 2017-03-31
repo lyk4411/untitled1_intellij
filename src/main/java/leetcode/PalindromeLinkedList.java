4e192bacf957e651efe1c4157411ca102e6f3597
ykliu <moneyflying_2006@hotmail.com>
2017-03-31 17:53:50 +0800

    core java ch 8

---
+++ src/main/java/leetcode/PalindromeLinkedList.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+/**
+ * Created by lyk on 2017/3/31.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class PalindromeLinkedList {
+    public static void main(String[] args) {
+        PalindromeLinkedList pll = new PalindromeLinkedList();
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(3);
+        ListNode l5 = new ListNode(2);
+        ListNode l6 = new ListNode(1);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = l6;
+        System.out.println(pll.isPalindrome(l1));
+        System.out.println(pll.isPalindrome(l2));
+    }
+    public boolean isPalindrome(ListNode head) {
+        if(head == null)
+            return true;
+
+        ListNode p = head;
+        ListNode prev = new ListNode(head.val);
+
+        while(p.next != null){
+            ListNode temp = new ListNode(p.next.val);
+            temp.next = prev;
+            prev = temp;
+            p = p.next;
+        }
+
+        ListNode p1 = head;
+        ListNode p2 = prev;
+
+        while(p1!=null){
+            if(p1.val != p2.val)
+                return false;
+
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return true;
+    }
+}
