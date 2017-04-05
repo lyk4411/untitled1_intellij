aadad5c83ddd2c1c8f21030d5b8d75de52de4e8e
ykliu <moneyflying_2006@hotmail.com>
2017-04-05 18:58:32 +0800

    core java ch 8

--- src/main/java/leetcode/EditDistance.java
+++ src/main/java/leetcode/EditDistance.java
@@ -47,7 +47,12 @@ public class EditDistance {
                 }
             }
         }
-
+//        for (int i = 0; i < dp.length; i++) {
+//            for (int j = 0; j < dp[0].length; j++) {
+//                System.out.print(dp[i][j] + "  ");
+//            }
+//            System.out.println();
+//        }
         return dp[len1][len2];
     }
 }
