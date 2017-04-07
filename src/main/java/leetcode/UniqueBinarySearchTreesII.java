c5822c78a6a5b42a1c9f4df5b455f38071d4da20
ykliu <moneyflying_2006@hotmail.com>
2017-04-07 09:01:45 +0800

    core java ch 8

---
+++ src/main/java/leetcode/UniqueBinarySearchTreesII.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lyk on 2017/4/7.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class UniqueBinarySearchTreesII {
+    public static void main(String[] args) {
+        UniqueBinarySearchTreesII ubstii = new UniqueBinarySearchTreesII();
+        System.out.println(ubstii.generateTrees(5));
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        if(n==0){
+            return new ArrayList<TreeNode>();
+        }
+
+        return helper(1, n);
+    }
+
+    public List<TreeNode> helper(int m, int n){
+        List<TreeNode> result = new ArrayList<TreeNode>();
+        if(m>n){
+            result.add(null);
+            return result;
+        }
+
+        for(int i=m; i<=n; i++){
+            List<TreeNode> ls = helper(m, i-1);
+            List<TreeNode> rs = helper(i+1, n);
+            for(TreeNode l: ls){
+                for(TreeNode r: rs){
+                    TreeNode curr = new TreeNode(i);
+                    curr.left=l;
+                    curr.right=r;
+                    result.add(curr);
+                }
+            }
+        }
+
+        return result;
+    }
+}
