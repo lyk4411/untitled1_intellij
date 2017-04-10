30b834a3e9ea0d4e2f0896407753ae978443966b
ykliu <moneyflying_2006@hotmail.com>
2017-04-10 14:51:43 +0800

    core java ch 8

--- src/main/java/leetcode/RecoverBinarySearchTree.java
+++ src/main/java/leetcode/RecoverBinarySearchTree.java
@@ -8,15 +8,15 @@ package leetcode;
 public class RecoverBinarySearchTree {
     public static void main(String[] args) {
         RecoverBinarySearchTree rbst = new RecoverBinarySearchTree();
-        TreeNode a1 = new TreeNode(1);
+        TreeNode a1 = new TreeNode(2);
         TreeNode a2 = new TreeNode(1);
-        TreeNode a3 = new TreeNode(2);
-        TreeNode a4 = new TreeNode(2);
-        TreeNode a5 = new TreeNode(6);
-        TreeNode a6 = new TreeNode(3);
-        TreeNode a7 = new TreeNode(4);
-        TreeNode a8 = new TreeNode(4);
-        TreeNode a9 = new TreeNode(5);
+        TreeNode a3 = new TreeNode(4);
+        TreeNode a4 = new TreeNode(3);
+        TreeNode a5 = new TreeNode(7);
+        TreeNode a6 = new TreeNode(5);
+        TreeNode a7 = new TreeNode(8);
+        TreeNode a8 = new TreeNode(6);
+        TreeNode a9 = new TreeNode(9);
 
         a1.left = a2;
         a1.right = a3;
@@ -30,6 +30,7 @@ public class RecoverBinarySearchTree {
         System.out.println(a1);
         rbst.recoverTree(a1);
         System.out.println(a1);
+
     }
     TreeNode first;
     TreeNode second;
