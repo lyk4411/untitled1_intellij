01bd65a61ed8d20ccd00cac958c9951cfa10a0ac
ykliu <moneyflying_2006@hotmail.com>
2017-04-10 15:24:18 +0800

    core java ch 8

--- src/main/java/leetcode/DecodeWays.java
+++ src/main/java/leetcode/DecodeWays.java
@@ -8,6 +8,7 @@ package leetcode;
 public class DecodeWays {
     public static void main(String[] args) {
         DecodeWays dw = new DecodeWays();
+        System.out.println(dw.numDecodings("00123456321212"));
         System.out.println(dw.numDecodings("123456321212"));
     }
     public int numDecodings(String s) {
