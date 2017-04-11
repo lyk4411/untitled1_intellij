3c87fec0b40deb7efa24ca0f84bd1909dfea7497
ykliu <moneyflying_2006@hotmail.com>
2017-04-11 10:11:34 +0800

    core java ch 8

--- src/main/java/leetcode/PalindromePartitioning.java
+++ src/main/java/leetcode/PalindromePartitioning.java
@@ -1,7 +1,6 @@
 package leetcode;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by lyk on 2017/4/11.
@@ -28,42 +27,93 @@ public class PalindromePartitioning {
 //
 //    line 13: is the boundary to check if the current string contains the last element.
 //            l>=s.length()
-    List<List<String>> resultLst;
-    ArrayList<String> currLst;
 
-    public List<List<String>> partition(String s) {
-        resultLst = new ArrayList<List<String>>();
-        currLst = new ArrayList<String>();
-        backTrack(s, 0);
-        return resultLst;
+    public ArrayList<ArrayList<String>> partition(String s) {
+        ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+
+        if (s == null || s.length() == 0) {
+            return result;
+        }
+
+        ArrayList<String> partition = new ArrayList<String>();//track each possible partition
+        addPalindrome(s, 0, partition, result);
+
+        return result;
     }
 
-    public void backTrack(String s, int l) {
-        if (currLst.size() > 0 //the initial str could be palindrome
-                && l >= s.length()) {
-            List<String> r = (ArrayList<String>) currLst.clone();
-            resultLst.add(r);
+    private void addPalindrome(String s, int start, ArrayList<String> partition,
+                               ArrayList<ArrayList<String>> result) {
+        //stop condition
+        if (start == s.length()) {
+            ArrayList<String> temp = new ArrayList<String>(partition);
+            result.add(temp);
+            return;
         }
-        for (int i = l; i < s.length(); i++) {
-            if (isPalindrome(s, l, i)) {
-                if (l == i)
-                    currLst.add(Character.toString(s.charAt(i)));
-                else
-                    currLst.add(s.substring(l, i + 1));
-                backTrack(s, i + 1);
-                currLst.remove(currLst.size() - 1);
+
+        for (int i = start + 1; i <= s.length(); i++) {
+            String str = s.substring(start, i);
+            if (isPalindrome(str)) {
+                partition.add(str);
+                addPalindrome(s, i, partition, result);
+                partition.remove(partition.size() - 1);
             }
         }
     }
 
-    public boolean isPalindrome(String str, int l, int r) {
-        if (l == r) return true;
-        while (l < r) {
-            if (str.charAt(l) != str.charAt(r)) return false;
-            l++;
-            r--;
+    private boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
         }
+
         return true;
     }
+
+
+
+//    List<List<String>> resultLst;
+//    ArrayList<String> currLst;
+//
+//    public List<List<String>> partition(String s) {
+//        resultLst = new ArrayList<List<String>>();
+//        currLst = new ArrayList<String>();
+//        backTrack(s, 0);
+//        return resultLst;
+//    }
+//
+//    public void backTrack(String s, int l) {
+//        if (currLst.size() > 0 //the initial str could be palindrome
+//                && l >= s.length()) {
+//            List<String> r = (ArrayList<String>) currLst.clone();
+//            resultLst.add(r);
+//        }
+//        for (int i = l; i < s.length(); i++) {
+//            if (isPalindrome(s, l, i)) {
+//                if (l == i)
+//                    currLst.add(Character.toString(s.charAt(i)));
+//                else
+//                    currLst.add(s.substring(l, i + 1));
+//                backTrack(s, i + 1);
+//                currLst.remove(currLst.size() - 1);
+//            }
+//        }
+//    }
+//
+//    public boolean isPalindrome(String str, int l, int r) {
+//        if (l == r) return true;
+//        while (l < r) {
+//            if (str.charAt(l) != str.charAt(r)) return false;
+//            l++;
+//            r--;
+//        }
+//        return true;
+//    }
 }
 
