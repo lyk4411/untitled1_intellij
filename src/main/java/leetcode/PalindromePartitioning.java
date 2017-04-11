f8dcde81f409a915008aba51242995d83c140de8
ykliu <moneyflying_2006@hotmail.com>
2017-04-11 10:24:40 +0800

    core java ch 8

--- src/main/java/leetcode/PalindromePartitioning.java
+++ src/main/java/leetcode/PalindromePartitioning.java
@@ -12,6 +12,7 @@ public class PalindromePartitioning {
     public static void main(String[] args) {
         PalindromePartitioning pp = new PalindromePartitioning();
         System.out.println(pp.partition("hafehabahliuil"));
+        //System.out.println(pp.palindromePartitioning("hafehabahliuil"));
     }
 //    图解在：https://leetcode.com/problems/palindrome-partitioning/#/solutions
 //    if the input is "aab", check if [0,0] "a" is palindrome. then check [0,1] "aa", then [0,2] "aab".
@@ -76,6 +77,48 @@ public class PalindromePartitioning {
 
         return true;
     }
+
+
+
+
+
+//    public static List<String> palindromePartitioning(String s) {
+//
+//        List<String> result = new ArrayList<String>();
+//
+//        if (s == null)
+//            return result;
+//
+//        if (s.length() <= 1) {
+//            result.add(s);
+//            return result;
+//        }
+//
+//        int length = s.length();
+//
+//        int[][] table = new int[length][length];
+//
+//        // l is length, i is index of left boundary, j is index of right boundary
+//        for (int l = 1; l <= length; l++) {
+//            for (int i = 0; i <= length - l; i++) {
+//                int j = i + l - 1;
+//                if (s.charAt(i) == s.charAt(j)) {
+//                    if (l == 1 || l == 2) {
+//                        table[i][j] = 1;
+//                    } else {
+//                        table[i][j] = table[i + 1][j - 1];
+//                    }
+//                    if (table[i][j] == 1) {
+//                        result.add(s.substring(i, j + 1));
+//                    }
+//                } else {
+//                    table[i][j] = 0;
+//                }
+//            }
+//        }
+//
+//        return result;
+//    }
 
 
 
