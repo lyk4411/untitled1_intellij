2d780d3cd6b1dc4441e71f28a6702cb3c280cfd5
ykliu <moneyflying_2006@hotmail.com>
2017-04-12 10:01:08 +0800

    core java ch 8

--- src/main/java/example/WordLadder.java
+++ src/main/java/example/WordLadder.java
@@ -1,7 +1,6 @@
 package example;
 
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by lyk on 2017/4/12.
@@ -9,48 +8,91 @@ import java.util.Set;
  * Porject name: untitled1
  */
 public class WordLadder {
-    public int ladderLength(String beginWord, String endWord, Set<String> wordDict) {
-        LinkedList<WordNode> queue = new LinkedList<WordNode>();
-        queue.add(new WordNode(beginWord, 1));
+    public static void main(String[] args) {
+        WordLadder wl = new WordLadder();
+        List<String> list = new ArrayList<String>();
+        list.add("hot");
+        list.add("dot");
+        list.add("dog");
+        list.add("lot");
+        list.add("log");
+        //list.add("cog");
+        System.out.println(wl.ladderLength("hit","cog",list));
+    }
 
-        wordDict.add(endWord);
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
 
-        while(!queue.isEmpty()){
-            WordNode top = queue.remove();
-            String word = top.word;
+        Queue<String> q = new LinkedList<String>();
+        q.add(beginWord);
+        Set<String> visited = new HashSet<String>();
+        visited.add(beginWord);
+        int level = 0;
 
-            if(word.equals(endWord)){
-                return top.numSteps;
-            }
-
-            char[] arr = word.toCharArray();
-            for(int i=0; i<arr.length; i++){
-                for(char c='a'; c<='z'; c++){
-                    char temp = arr[i];
-                    if(arr[i]!=c){
-                        arr[i]=c;
+        while (!q.isEmpty()) {
+            int len = q.size();
+            level++;
+            for (int i = 0; i < len; i++) {
+                String word = q.remove();
+                for (int j = 0; j < word.length(); j++) {
+                    char[] chars = word.toCharArray();
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        chars[j] = c;
+                        String nextWord = new String(chars);
+                        if (nextWord.equals(endWord)) {
+                            return level + 1;
+                        }
+                        if (!visited.contains(nextWord) && wordList.contains(nextWord)) {
+                            q.add(nextWord);
+                            visited.add(nextWord);
+                        }
                     }
-
-                    String newWord = new String(arr);
-                    if(wordDict.contains(newWord)){
-                        queue.add(new WordNode(newWord, top.numSteps+1));
-                        wordDict.remove(newWord);
-                    }
-
-                    arr[i]=temp;
                 }
             }
         }
 
         return 0;
     }
-}
-class WordNode{
-    String word;
-    int numSteps;
 
-    public WordNode(String word, int numSteps){
-        this.word = word;
-        this.numSteps = numSteps;
-    }
+
+//    class WordNode{
+//        String word;
+//        int numSteps;
+//
+//        public WordNode(String word, int numSteps){
+//            this.word = word;
+//            this.numSteps = numSteps;
+//        }
+//    }
+//
+//    public int ladderLength(String beginWord, String endWord, List<String> wordDict) {
+//        LinkedList<WordNode> queue = new LinkedList<WordNode>();
+//        queue.add(new WordNode(beginWord, 1));
+//
+//        while(!queue.isEmpty()){
+//            WordNode top = queue.remove();
+//            String word = top.word;
+//            if(word.equals(endWord)){
+//                return top.numSteps;
+//            }
+//
+//            char[] arr = word.toCharArray();
+//            for(int i=0; i<arr.length; i++){
+//                for(char c='a'; c<='z'; c++){
+//                    char temp = arr[i];
+//                    if(arr[i]!=c){
+//                        arr[i]=c;
+//                    }
+//
+//                    String newWord = new String(arr);
+//                    if(wordDict.contains(newWord)){
+//                        queue.add(new WordNode(newWord, top.numSteps+1));
+//                        wordDict.remove(newWord);
+//                    }
+//
+//                    arr[i]=temp;
+//                }
+//            }
+//        }
+//        return 0;
+//    }
 }
