13f7650813ce4e91b33b54d781eeb7887103095a
ykliu <moneyflying_2006@hotmail.com>
2017-04-12 16:35:01 +0800

    core java ch 8

--- src/main/java/leetcode/MinimumAbsoluteDifferenceinBST.java
+++ src/main/java/leetcode/MinimumAbsoluteDifferenceinBST.java
@@ -6,6 +6,28 @@ package leetcode;
  * Porject name: untitled1
  */
 public class MinimumAbsoluteDifferenceinBST {
+    public static void main(String[] args) {
+        MinimumAbsoluteDifferenceinBST madbst = new MinimumAbsoluteDifferenceinBST();
+        TreeNode a1 = new TreeNode(1);
+        TreeNode a2 = new TreeNode(1);
+        TreeNode a3 = new TreeNode(2);
+        TreeNode a4 = new TreeNode(2);
+        TreeNode a5 = new TreeNode(3);
+        TreeNode a6 = new TreeNode(3);
+        TreeNode a7 = new TreeNode(4);
+        TreeNode a8 = new TreeNode(4);
+        TreeNode a9 = new TreeNode(5);
+
+        a1.left = a2;
+        a1.right = a3;
+        a3.left = a4;
+        a3.right = a5;
+        a5.left = a6;
+        a5.right = a7;
+        a7.left = a8;
+        a7.right = a9;
+        System.out.println(madbst.getMinimumDifference(a1));
+    }
     int min = Integer.MAX_VALUE;
     Integer prev = null;
 
