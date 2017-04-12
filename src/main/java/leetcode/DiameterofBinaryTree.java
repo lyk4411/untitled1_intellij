8ade942cadf8bfaedf0820d4fd0a70b779353205
ykliu <moneyflying_2006@hotmail.com>
2017-04-12 16:49:49 +0800

    core java ch 8

---
+++ src/main/java/leetcode/DiameterofBinaryTree.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+/**
+ * Created by lyk on 2017/4/12.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class DiameterofBinaryTree {
+    public static void main(String[] args) {
+        DiameterofBinaryTree dbt = new DiameterofBinaryTree();
+        TreeNode a1 = new TreeNode(1);
+        TreeNode a2 = new TreeNode(1);
+        TreeNode a3 = new TreeNode(2);
+        TreeNode a4 = new TreeNode(2);
+        TreeNode a5 = new TreeNode(3);
+        TreeNode a6 = new TreeNode(3);
+        TreeNode a7 = new TreeNode(4);
+        TreeNode a8 = new TreeNode(4);
+        TreeNode a9 = new TreeNode(5);
+
+        a1.left = a2;
+        a1.right = a3;
+        a3.left = a4;
+        a3.right = a5;
+        a5.left = a6;
+        a5.right = a7;
+        a7.left = a8;
+        a7.right = a9;
+        System.out.println(dbt.diameterOfBinaryTree(a1));
+    }
+
+    int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
+
+    private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        max = Math.max(max, left + right);
+
+        return Math.max(left, right) + 1;
+    }
+}
