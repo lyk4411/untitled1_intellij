0942345c10b3eed5b31df44b932addcb8f405cc5
ykliu <moneyflying_2006@hotmail.com>
2017-04-12 11:21:07 +0800

    core java ch 8

--- src/main/java/leetcode/WordBreakII.java
+++ src/main/java/leetcode/WordBreakII.java
@@ -18,25 +18,26 @@ public class WordBreakII {
         lists.add("and");
         lists.add("sand");
         lists.add("dog");
-        System.out.println(wbii.wordBreak("catsanddog",lists));
+        System.out.println(wbii.wordBreak("catsanddog", lists));
     }
+
     public static List<String> wordBreak(String s, List<String> dict) {
         //create an array of ArrayList<String>
-        List<String> dp[] = new ArrayList[s.length()+1];
+        List<String> dp[] = new ArrayList[s.length() + 1];
         dp[0] = new ArrayList<String>();
 
-        for(int i=0; i<s.length(); i++){
-            if( dp[i] == null )
+        for (int i = 0; i < s.length(); i++) {
+            if (dp[i] == null)
                 continue;
 
-            for(String word:dict){
+            for (String word : dict) {
                 int len = word.length();
-                int end = i+len;
-                if(end > s.length())
+                int end = i + len;
+                if (end > s.length())
                     continue;
 
-                if(s.substring(i,end).equals(word)){
-                    if(dp[end] == null){
+                if (s.substring(i, end).equals(word)) {
+                    if (dp[end] == null) {
                         dp[end] = new ArrayList<String>();
                     }
                     dp[end].add(word);
@@ -45,7 +46,7 @@ public class WordBreakII {
         }
 
         List<String> result = new LinkedList<String>();
-        if(dp[s.length()] == null)
+        if (dp[s.length()] == null)
             return result;
 
         ArrayList<String> temp = new ArrayList<String>();
@@ -54,21 +55,21 @@ public class WordBreakII {
         return result;
     }
 
-    public static void dfs(List<String> dp[],int end,List<String> result, ArrayList<String> tmp){
-        if(end <= 0){
-            String path = tmp.get(tmp.size()-1);
-            for(int i=tmp.size()-2; i>=0; i--){
-                path += " " + tmp.get(i) ;
+    public static void dfs(List<String> dp[], int end, List<String> result, ArrayList<String> tmp) {
+        if (end <= 0) {
+            String path = tmp.get(tmp.size() - 1);
+            for (int i = tmp.size() - 2; i >= 0; i--) {
+                path += " " + tmp.get(i);
             }
 
             result.add(path);
             return;
         }
 
-        for(String str : dp[end]){
+        for (String str : dp[end]) {
             tmp.add(str);
-            dfs(dp, end-str.length(), result, tmp);
-            tmp.remove(tmp.size()-1);
+            dfs(dp, end - str.length(), result, tmp);
+            tmp.remove(tmp.size() - 1);
         }
     }
 }
