fc2fe690b3d5af8cae861e723b5eab1ebae49395
ykliu <moneyflying_2006@hotmail.com>
2017-04-13 15:13:32 +0800

    core java ch 8

--- src/main/java/leetcode/BitwiseANDofNumbersRange.java
+++ src/main/java/leetcode/BitwiseANDofNumbersRange.java
@@ -10,10 +10,10 @@ public class BitwiseANDofNumbersRange {
         BitwiseANDofNumbersRange banr = new BitwiseANDofNumbersRange();
         System.out.println(banr.rangeBitwiseAnd(101,108));
     }
-//    当m!=n，那么最末位必定等0，因为[m, n]必定包含奇偶数，相与最末位等0。
+//    当m!=n，那么最末位必定等0，因为[m, n]必定包含奇偶数，相与最末位等于0。
 //    可以将m，n都右移一位，记为mk、 nk，这样就相当于将[m, n]之间的所有的
-//    数都右移动了一位，当mk=nk的时候，说明之前[m, n]之间的数右移一位后是
-//    相等的，右移后的数作AND操作，结果还是m(=n)，所以操作就可以停止了记录
+//    数都右移动了一位，当m！=n 时，继续右移，当mk=nk的时候，说明之前[m, n]之
+//    间的数右移一位后是相等的，右移后的数作AND操作，所以操作就可以停止了记录
 //    右移的次数，offset，m>>offset即为所求结果
     public int rangeBitwiseAnd(int m, int n) {
         if(m == 0){
