6d61c6f93e1e2ac0371cd5e930cb1deacba08467
ykliu <moneyflying_2006@hotmail.com>
2017-04-13 15:30:44 +0800

    core java ch 8

--- src/main/java/leetcode/BeautifulArrangement.java
+++ src/main/java/leetcode/BeautifulArrangement.java
@@ -10,7 +10,28 @@ public class BeautifulArrangement {
         BeautifulArrangement ba = new BeautifulArrangement();
         System.out.println(ba.countArrangement(5));
     }
+
     int count = 0;
+
+//    private int countArrangement(int Number) {
+//        if(Number == 0) return 0;
+//        helper(Number,1,new int[Number + 1]);
+//        return count;
+//    }
+//
+//    private void helper(int number, int pos, int[] used) {
+//        if(pos == (number + 1)){
+//            count++;
+//            return;
+//        }
+//        for (int i = 1; i <= number; i++) {
+//            if(used[i] == 0 && ( i % pos == 0 || pos % i == 0)) {
+//                used[i] = 1;
+//                helper(number, pos + 1, used);
+//                used[i] = 0;
+//            }
+//        }
+//    }
 
     public int countArrangement(int N) {
         if (N == 0) return 0;
