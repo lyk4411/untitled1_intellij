5bec104c1378b61b6f4916f732b29bbeaf3f1a3f
ykliu <moneyflying_2006@hotmail.com>
2017-04-17 19:03:27 +0800

    core java ch 8

--- src/main/java/leetcode/FindtheDifference.java
+++ src/main/java/leetcode/FindtheDifference.java
@@ -20,4 +20,15 @@ public class FindtheDifference {
         // Return the difference between 2 strings as char
         return (char)(charCodeT - charCodeS);
     }
+
+//    public char findTheDifference(String s, String t) {
+//        char c = 0;
+//        for (int i = 0; i < s.length(); ++i) {
+//            c ^= s.charAt(i);
+//        }
+//        for (int i = 0; i < t.length(); ++i) {
+//            c ^= t.charAt(i);
+//        }
+//        return c;
+//    }
 }
