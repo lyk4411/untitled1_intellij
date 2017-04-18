ed0d4b9e82e1c89c2217a64a891ec469cccf0f89
ykliu <moneyflying_2006@hotmail.com>
2017-04-18 10:21:19 +0800

    core java ch 8

--- src/main/java/leetcode/IntersectionofTwoArrays.java
+++ src/main/java/leetcode/IntersectionofTwoArrays.java
@@ -17,23 +17,14 @@ public class IntersectionofTwoArrays {
     }
     public int[] intersection(int[] nums1, int[] nums2) {
         HashSet<Integer> set1 = new HashSet<Integer>();
-        for(int i: nums1){
-            set1.add(i);
-        }
+
+        IntStream.of(nums1).forEach(a -> set1.add(a));
 
         HashSet<Integer> set2 = new HashSet<Integer>();
-        for(int i: nums2){
-            if(set1.contains(i)){
-                set2.add(i);
-            }
-        }
 
-        int[] result = new int[set2.size()];
-        int i=0;
-        for(int n: set2){
-            result[i++] = n;
-        }
+        IntStream.of(nums2).filter(a -> set1.contains(a)).forEach(a -> set2.add(a));
 
-        return result;
+        return set2.stream().mapToInt(Integer::intValue).toArray();
+
     }
 }
