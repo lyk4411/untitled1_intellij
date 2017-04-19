72e590183ded433d106beb891cb97f21e90be3e3
ykliu <moneyflying_2006@hotmail.com>
2017-04-19 18:31:38 +0800

    core java ch 8

---
+++ src/main/java/example/matrix.java
@@ -0,0 +1,17 @@
+package example;
+
+/**
+ * Created by lyk on 2017/4/19.
+ * Package name: example
+ * Porject name: untitled1
+ */
+public class matrix {
+    public static void main(String[] args) {
+        int[][] matrixes = new int[][]{
+                {1, 0, 1, -2, 3},
+                {-1, 1, 1, 2, -3}
+        };
+        System.out.println(matrixes.length);
+        System.out.println(matrixes[0].length);
+    }
+}
