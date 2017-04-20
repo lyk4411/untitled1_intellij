b3bd728c2f4256cae55e7b9ed51aa50de81a28e6
ykliu <moneyflying_2006@hotmail.com>
2017-04-20 18:06:44 +0800

    core java ch 8

--- src/main/java/leetcode/VerifyPreorderSerializationofaBinaryTree.java
+++ src/main/java/leetcode/VerifyPreorderSerializationofaBinaryTree.java
@@ -12,6 +12,7 @@ public class VerifyPreorderSerializationofaBinaryTree {
         System.out.println(vpsbt.isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#"));
         System.out.println(vpsbt.isValidSerialization("9,#,#"));
         System.out.println(vpsbt.isValidSerialization("9,#,#,1"));
+        System.out.println(vpsbt.isValidSerialization("#,#,1"));
     }
     public boolean isValidSerialization(String preorder) {
         String[] nodes = preorder.split(",");
@@ -22,4 +23,29 @@ public class VerifyPreorderSerializationofaBinaryTree {
         }
         return diff == 0;
     }
+
+//    public boolean isValidSerialization(String preorder) {
+//        LinkedList<String> stack = new LinkedList<String>();
+//        String[] arr = preorder.split(",");
+//
+//        for(int i=0; i<arr.length; i++){
+//            stack.add(arr[i]);
+//
+//            while(stack.size()>=3
+//                    && stack.get(stack.size()-1).equals("#")
+//                    && stack.get(stack.size()-2).equals("#")
+//                    && !stack.get(stack.size()-3).equals("#")){
+//
+//                stack.remove(stack.size()-1);
+//                stack.remove(stack.size()-1);
+//                stack.remove(stack.size()-1);
+//
+//                stack.add("#");
+//            }
+//        }
+//        if(stack.size()==1 && stack.get(0).equals("#"))
+//            return true;
+//        else
+//            return false;
+//    }
 }
