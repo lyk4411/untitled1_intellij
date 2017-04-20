dae97b683b010d0ff78138e911ccd457c957d6bc
ykliu <moneyflying_2006@hotmail.com>
2017-04-20 18:13:59 +0800

    core java ch 8

---
+++ src/main/java/leetcode/MaximumProductofWordLengths.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+/**
+ * Created by lyk on 2017/4/20.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class MaximumProductofWordLengths {
+    public static void main(String[] args) {
+        MaximumProductofWordLengths mpwl = new MaximumProductofWordLengths();
+        String[] words = new String[]{"abcw", "baz", "foo", "bar", "xtfn", "abcdef"};
+        System.out.println(mpwl.maxProduct(words));
+    }
+    public static int maxProduct(String[] words) {
+        if (words == null || words.length == 0)
+            return 0;
+        int len = words.length;
+        int[] value = new int[len];
+        for (int i = 0; i < len; i++) {
+            String tmp = words[i];
+            value[i] = 0;
+            for (int j = 0; j < tmp.length(); j++) {
+                value[i] |= 1 << (tmp.charAt(j) - 'a');
+            }
+        }
+        int maxProduct = 0;
+        for (int i = 0; i < len; i++)
+            for (int j = i + 1; j < len; j++) {
+                if ((value[i] & value[j]) == 0 && (words[i].length() * words[j].length() > maxProduct))
+                    maxProduct = words[i].length() * words[j].length();
+            }
+        return maxProduct;
+    }
+}
