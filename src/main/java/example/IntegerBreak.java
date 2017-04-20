64e706cd298f7846cf19bd2a5763f02b9e31998f
ykliu <moneyflying_2006@hotmail.com>
2017-04-20 09:48:26 +0800

    core java ch 8

--- src/main/java/example/IntegerBreak.java
+++ src/main/java/example/IntegerBreak.java
@@ -13,18 +13,38 @@ public class IntegerBreak {
         System.out.println(ib.integerBreak(30));
         System.out.println(ib.integerBreak(40));
         System.out.println(ib.integerBreak(50));
-
     }
-    public int integerBreak(int n) {
-        int[] dp = new int[n+1];
+//    public int integerBreak(int n) {
+//        int[] dp = new int[n+1];
+//
+//        for(int i=1; i<n; i++){
+//            for(int j=1; j<i+1; j++){
+//                if(i+j<=n){
+//                    dp[i+j]=Math.max(Math.max(dp[i],i)*Math.max(dp[j],j), dp[i+j]);
+//                }
+//            }
+//        }
+//        return dp[n];
+//    }
 
-        for(int i=1; i<n; i++){
-            for(int j=1; j<i+1; j++){
-                if(i+j<=n){
-                    dp[i+j]=Math.max(Math.max(dp[i],i)*Math.max(dp[j],j), dp[i+j]);
-                }
-            }
+    public int integerBreak(int n) {
+
+        if(n==2) return 1;
+        if(n==3) return 2;
+        if(n==4) return 4;
+
+        int result=1;
+        if(n%3==0){
+            int m = n/3;
+            result = (int) Math.pow(3, m);
+        }else if(n%3==2){
+            int m=n/3;
+            result = (int) Math.pow(3, m) * 2;
+        }else if(n%3==1){
+            int m=(n-4)/3;
+            result = (int) Math.pow(3, m) *4;
         }
-        return dp[n];
+
+        return result;
     }
 }
