ec266d85df611c8b36b4889a83a595c1bf7fb701
ykliu <moneyflying_2006@hotmail.com>
2017-04-20 17:44:20 +0800

    core java ch 8

---
+++ src/main/java/leetcode/FindAllDuplicatesinanArray.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lyk on 2017/4/20.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class FindAllDuplicatesinanArray {
+    public static void main(String[] args) {
+        FindAllDuplicatesinanArray fada = new FindAllDuplicatesinanArray();
+        int[] nums = new int[]{1,2,3,4,5,6,4,3,2,7,8,9,4,4};
+        System.out.println(fada.findDuplicates(nums));
+    }
+
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int index = Math.abs(nums[i])-1;
+            if (nums[index] < 0)
+                res.add(Math.abs(index+1));
+            nums[index] = -nums[index];
+        }
+        return res;
+    }
+}
