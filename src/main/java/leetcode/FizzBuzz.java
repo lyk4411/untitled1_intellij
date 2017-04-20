edecb8479b83e2551d42b6e701a620c678a42694
ykliu <moneyflying_2006@hotmail.com>
2017-04-20 13:54:28 +0800

    core java ch 8

--- src/main/java/leetcode/FizzBuzz.java
+++ src/main/java/leetcode/FizzBuzz.java
@@ -1,9 +1,31 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by lyk on 2017/4/20.
  * Package name: leetcode
  * Porject name: untitled1
  */
 public class FizzBuzz {
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        System.out.println(fb.fizzBuzz(200));
+    }
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
 }
