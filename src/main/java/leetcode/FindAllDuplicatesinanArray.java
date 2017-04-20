06fff645b4c1b796cb18c2b857a2240dbfe3fc2f
ykliu <moneyflying_2006@hotmail.com>
2017-04-20 17:48:47 +0800

    core java ch 8

--- src/main/java/leetcode/FindAllDuplicatesinanArray.java
+++ src/main/java/leetcode/FindAllDuplicatesinanArray.java
@@ -20,7 +20,7 @@ public class FindAllDuplicatesinanArray {
         for (int i = 0; i < nums.length; ++i) {
             int index = Math.abs(nums[i])-1;
             if (nums[index] < 0)
-                res.add(Math.abs(index+1));
+                res.add(Math.abs(index+1));//res.add(nums[i]);
             nums[index] = -nums[index];
         }
         return res;
