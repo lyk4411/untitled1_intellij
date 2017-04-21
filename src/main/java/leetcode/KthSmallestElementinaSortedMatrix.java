4eaf783033dbf08000c5549d373cdd9ff16c31bd
ykliu <moneyflying_2006@hotmail.com>
2017-04-21 13:51:11 +0800

    core java ch 8

--- src/main/java/leetcode/KthSmallestElementinaSortedMatrix.java
+++ src/main/java/leetcode/KthSmallestElementinaSortedMatrix.java
@@ -20,28 +20,16 @@ public class KthSmallestElementinaSortedMatrix {
         System.out.println(ksesm.kthSmallest(matrix,4));
     }
     public int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
-        PriorityQueue<Tuple> pq = new PriorityQueue<Tuple>();
-        for(int j = 0; j <= n-1; j++) pq.offer(new Tuple(0, j, matrix[0][j]));
-        for(int i = 0; i < k-1; i++) {
-            Tuple t = pq.poll();
-            if(t.x == n-1) continue;
-            pq.offer(new Tuple(t.x+1, t.y, matrix[t.x+1][t.y]));
+        // heap
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k + 1, (a, b) -> b - a);
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                maxHeap.offer(matrix[i][j]);
+                if(maxHeap.size() > k) maxHeap.poll();
+            }
         }
-        return pq.poll().val;
-    }
-}
 
-class Tuple implements Comparable<Tuple> {
-    int x, y, val;
-    public Tuple (int x, int y, int val) {
-        this.x = x;
-        this.y = y;
-        this.val = val;
-    }
-
-    @Override
-    public int compareTo (Tuple that) {
-        return this.val - that.val;
+        return maxHeap.poll();
     }
 }
