7aab7b8770538ce98cf4c95976c270fe9de1067d
ykliu <moneyflying_2006@hotmail.com>
2017-05-03 18:43:38 +0800

    core java ch 8

--- src/main/java/leetcode/LexicographicalNumbers.java
+++ src/main/java/leetcode/LexicographicalNumbers.java
@@ -20,22 +20,43 @@ public class LexicographicalNumbers {
     // 一直往后遍历，如果超过了，我们除以10，然后再加1，如果加1后末尾
     // 形成了很多0，那么我们要用个while循环把0都去掉，然后继续运算，
     // 参见代码如下：
+//    public List<Integer> lexicalOrder(int n) {
+//        List<Integer> list = new ArrayList<>(n);
+//        int curr = 1;
+//        for (int i = 1; i <= n; i++) {
+//            list.add(curr);
+//            if (curr * 10 <= n) {
+//                curr *= 10;
+//            } else if (curr % 10 != 9 && curr + 1 <= n) {
+//                curr++;
+//            } else {
+//                while ((curr / 10) % 10 == 9) {
+//                    curr /= 10;
+//                }
+//                curr = curr / 10 + 1;
+//            }
+//        }
+//        return list;
+//    }
+
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> list = new ArrayList<>(n);
-        int curr = 1;
-        for (int i = 1; i <= n; i++) {
-            list.add(curr);
-            if (curr * 10 <= n) {
-                curr *= 10;
-            } else if (curr % 10 != 9 && curr + 1 <= n) {
-                curr++;
-            } else {
-                while ((curr / 10) % 10 == 9) {
-                    curr /= 10;
-                }
-                curr = curr / 10 + 1;
+        List<Integer> res = new ArrayList<>();
+        for(int i=1;i<10;++i){
+            dfs(i, n, res);
+        }
+        return res;
+    }
+
+    public void dfs(int cur, int n, List<Integer> res){
+        if(cur>n)
+            return;
+        else{
+            res.add(cur);
+            for(int i=0;i<10;++i){
+                if(10*cur+i>n)
+                    return;
+                dfs(10*cur+i, n, res);
             }
         }
-        return list;
     }
 }
