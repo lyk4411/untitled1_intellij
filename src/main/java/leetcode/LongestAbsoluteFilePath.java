fdc4382dcb1b5f9028d09ff876c34ddbeb7dabd4
ykliu <moneyflying_2006@hotmail.com>
2017-05-08 18:25:49 +0800

    core java ch 8

--- src/main/java/leetcode/LongestAbsoluteFilePath.java
+++ src/main/java/leetcode/LongestAbsoluteFilePath.java
@@ -13,12 +13,16 @@ public class LongestAbsoluteFilePath {
     }
     public int lengthLongestPath(String input) {
         String[] paths = input.split("\n");
-        int[] stack = new int[paths.length+1];
+        int[] stk = new int[paths.length + 1];
         int maxLen = 0;
-        for(String s:paths){
-            int lev = s.lastIndexOf("\t")+1, curLen = stack[lev+1] = stack[lev]+s.length()-lev+1;
-            if(s.contains(".")) maxLen = Math.max(maxLen, curLen-1);
+        // 路径是定好的，覆盖的时候已经走完上一层，放心覆盖。
+        for(String str: paths){
+            int lev = str.lastIndexOf("\t") + 1;
+            // 路径长度         “/” 占一格
+            stk[lev+1] = stk[lev] + str.length() - lev + 1;
+            if(str.contains("."))  maxLen = Math.max(maxLen, stk[lev+1]-1);   // 结尾处没有“/”, 所以减一
         }
         return maxLen;
     }
+
 }
