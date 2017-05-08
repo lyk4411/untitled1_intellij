4aa2ea619bb28ed48e6d07b2a61e77ffcf010e0b
ykliu <moneyflying_2006@hotmail.com>
2017-05-08 18:11:35 +0800

    core java ch 8

--- src/main/java/leetcode/LongestAbsoluteFilePath.java
+++ src/main/java/leetcode/LongestAbsoluteFilePath.java
@@ -1,8 +1,5 @@
 package leetcode;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 /**
  * Created by lyk on 2017/5/8.
  * Package name: leetcode
@@ -15,16 +12,12 @@ public class LongestAbsoluteFilePath {
                 "\\n\\t\\tsubsubdir1\\n\\tsubdir2\\n\\t\\tsubsubdir2\\n\\t\\t\\tfile2.ext"));
     }
     public int lengthLongestPath(String input) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        stack.push(0); // "dummy" length
+        String[] paths = input.split("\n");
+        int[] stack = new int[paths.length+1];
         int maxLen = 0;
-        for(String s:input.split("\n")){
-            int lev = s.lastIndexOf("\t")+1; // number of "\t"
-            while(lev+1<stack.size()) stack.pop(); // find parent
-            int len = stack.peek()+s.length()-lev+1; // remove "/t", add"/"
-            stack.push(len);
-            // check if it is file
-            if(s.contains(".")) maxLen = Math.max(maxLen, len-1);
+        for(String s:paths){
+            int lev = s.lastIndexOf("\t")+1, curLen = stack[lev+1] = stack[lev]+s.length()-lev+1;
+            if(s.contains(".")) maxLen = Math.max(maxLen, curLen-1);
         }
         return maxLen;
     }
