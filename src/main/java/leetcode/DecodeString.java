d5a71a960c32a2ba14ee41e805003c09a83ef3c0
ykliu <moneyflying_2006@hotmail.com>
2017-05-08 17:53:16 +0800

    core java ch 8

---
+++ src/main/java/leetcode/DecodeString.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * Created by lyk on 2017/5/8.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class DecodeString {
+    public static void main(String[] args) {
+        DecodeString ds = new DecodeString();
+        System.out.println(ds.decodeString("3[a]2[bc]"));
+    }
+    public String decodeString(String s) {
+        String res = "";
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> resStack = new Stack<>();
+        int idx = 0;
+        while (idx < s.length()) {
+            if (Character.isDigit(s.charAt(idx))) {
+                int count = 0;
+                while (Character.isDigit(s.charAt(idx))) {
+                    count = 10 * count + (s.charAt(idx) - '0');
+                    idx++;
+                }
+                countStack.push(count);
+            }
+            else if (s.charAt(idx) == '[') {
+                resStack.push(res);
+                res = "";
+                idx++;
+            }
+            else if (s.charAt(idx) == ']') {
+                StringBuilder temp = new StringBuilder (resStack.pop());
+                int repeatTimes = countStack.pop();
+                for (int i = 0; i < repeatTimes; i++) {
+                    temp.append(res);
+                }
+                res = temp.toString();
+                idx++;
+            }
+            else {
+                res += s.charAt(idx++);
+            }
+        }
+        return res;
+    }
+}
