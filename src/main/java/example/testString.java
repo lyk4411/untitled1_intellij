6f8401d115e4dd4ce07aeaa0e04fc2db2fd05e6e
ykliu <moneyflying_2006@hotmail.com>
2017-05-09 09:08:43 +0800

    core java ch 8

---
+++ src/main/java/example/testString.java
@@ -0,0 +1,15 @@
+package example;
+
+/**
+ * Created by lyk on 2017/5/9.
+ * Package name: example
+ * Porject name: untitled1
+ */
+public class testString {
+    public static void main(String[] args) {
+        System.out.println("\t" + "t");
+        System.out.println("\t\t" + "t");
+        System.out.println("\t".length());
+        System.out.println("\t\t".length());
+    }
+}
