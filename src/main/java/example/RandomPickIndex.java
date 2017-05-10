a375a8b442c9ef40a065cb749e9478ae7caa4aa9
ykliu <moneyflying_2006@hotmail.com>
2017-05-10 18:20:41 +0800

    core java ch 8

--- src/main/java/example/RandomPickIndex.java
+++ src/main/java/example/RandomPickIndex.java
@@ -24,14 +24,14 @@ public class RandomPickIndex {
     }
     public int pick(int target) {
         int total = 0;
-        int res = -1;
+        int result = -1;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == target) {
                 // randomly select an int from 0 to the nums of target. If x equals 0, set the res as the current index. The probability is always 1/nums for the latest appeared number. For example, 1 for 1st num, 1/2 for 2nd num, 1/3 for 3nd num (1/2 * 2/3 for each of the first 2 nums).
                 int x = rand.nextInt(++total);
-                res = x == 0 ? i : res;
+                result = x == 0 ? i : result;
             }
         }
-        return res;
+        return result;
     }
 }
