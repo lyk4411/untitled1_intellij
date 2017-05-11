7b7ae21125a547f78fa2e9675dd6f4b4cb5f74d3
ykliu <moneyflying_2006@hotmail.com>
2017-05-11 18:54:30 +0800

    core java ch 8

---
+++ src/main/java/leetcode/ArithmeticSlices.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * Created by lyk on 2017/5/11.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class ArithmeticSlices {
+    public static void main(String[] args) {
+        ArithmeticSlices as = new ArithmeticSlices();
+        System.out.println(as.numberOfArithmeticSlices(new int[]{7,7,7,7}));
+        System.out.println(as.numberOfArithmeticSlices(new int[]{1,2,3,4}));
+        System.out.println(as.numberOfArithmeticSlices(new int[]{1,2,5,3,3}));
+    }
+    public int numberOfArithmeticSlices(int[] A) {
+        int curr = 0, sum = 0;
+        for (int i=2; i<A.length; i++)
+            if (A[i]-A[i-1] == A[i-1]-A[i-2]) {
+                curr += 1;
+                sum += curr;
+            } else {
+                curr = 0;
+            }
+        return sum;
+    }
+}
