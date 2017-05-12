f91298df72165999bb98fba527d9b68ccb951585
ykliu <moneyflying_2006@hotmail.com>
2017-05-12 11:12:45 +0800

    core java ch 8

--- src/main/java/leetcode/PartitionEqualSubsetSum.java
+++ src/main/java/leetcode/PartitionEqualSubsetSum.java
@@ -13,33 +13,56 @@ public class PartitionEqualSubsetSum {
         //System.out.println(pess.canPartition(new int[]{1,2,3,4}));
     }
     public boolean canPartition(int[] nums) {
-        // check edge case
-        if (nums == null || nums.length == 0) {
-            return true;
+        if(nums.length==0) return false;
+        int sum=0;
+        for(int n:nums){
+            sum+=n;
         }
-        // preprocess
-        int volumn = 0;
-        for (int num : nums) {
-            volumn += num;
-        }
-        if (volumn % 2 != 0) {
-            return false;
-        }
-        volumn /= 2;
-        // dp def
-        boolean[] dp = new boolean[volumn + 1];
-        // dp init
-        dp[0] = true;
-        // dp transition
-        for (int i = 1; i <= nums.length; i++) {
-            for (int j = volumn; j >= nums[i-1]; j--) {
-                dp[j] = dp[j] || dp[j - nums[i-1]];
+        if(sum%2==1) return false;
+        sum=sum/2;
+        int[][] dp=new int[nums.length+1][sum+1];
+        for(int i=0;i<=nums.length;i++){
+            for(int j=0;j<=sum;j++){
+                if(i==0) //表示前0个数，所以价值均为0；
+                    dp[i][j]=0;
+                    //在装第i-1个数时，先判断剩余容量j是否大于nums[i-1]
+                else if(j<nums[i-1]) dp[i][j]=dp[i-1][j]; //小于表示空间不够，所以维持不变
+                else{
+                    //空间够，就通过比较大小来判断是否该放入第i-1个数
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-nums[i-1]]+nums[i-1]);
+                }
             }
-//            for (int k = 0; k < dp.length; k++) {
-//                System.out.print(dp[k] + ";");
-//            }
-//            System.out.println();
         }
-        return dp[volumn];
+        return dp[nums.length][sum]==sum;
     }
+//    public boolean canPartition(int[] nums) {
+//        // check edge case
+//        if (nums == null || nums.length == 0) {
+//            return true;
+//        }
+//        // preprocess
+//        int volumn = 0;
+//        for (int num : nums) {
+//            volumn += num;
+//        }
+//        if (volumn % 2 != 0) {
+//            return false;
+//        }
+//        volumn /= 2;
+//        // dp def
+//        boolean[] dp = new boolean[volumn + 1];
+//        // dp init
+//        dp[0] = true;
+//        // dp transition
+//        for (int i = 1; i <= nums.length; i++) {
+//            for (int j = volumn; j >= nums[i-1]; j--) {
+//                dp[j] = dp[j] || dp[j - nums[i-1]];
+//            }
+////            for (int k = 0; k < dp.length; k++) {
+////                System.out.print(dp[k] + ";");
+////            }
+////            System.out.println();
+//        }
+//        return dp[volumn];
+//    }
 }
