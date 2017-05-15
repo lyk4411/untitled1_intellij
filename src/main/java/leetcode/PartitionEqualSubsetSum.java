9380dae6a47d7f2a7773af06780714a1e5af4e3e
ykliu <moneyflying_2006@hotmail.com>
2017-05-15 18:04:08 +0800

    core java ch 8

--- src/main/java/leetcode/PartitionEqualSubsetSum.java
+++ src/main/java/leetcode/PartitionEqualSubsetSum.java
@@ -12,6 +12,23 @@ public class PartitionEqualSubsetSum {
         //System.out.println(pess.canPartition(new int[]{1,5,5,12}));
         //System.out.println(pess.canPartition(new int[]{1,2,3,4}));
     }
+
+//    1.解题思路
+//    此问题属于动态规划中的背包问题。
+//    背包问题：假设有n个宝石，只有一个容量为C的背包，且第i个宝石所对应的重量和价值为w[i]和v[i],求装哪些宝石可以获得最大的价值收益？
+//    思路：我们将n个宝石进行编号，0,1,2...n-1,寻找DP的状态和状态转移方程。我们用dpij表示将前i个宝石装到剩余容量为j的背包中，那么久很容易得到状态转移方程了。（宝石从0开始编号，所以dpij是在考虑第i-1个宝石装包的情况，当然我们要先初始化前0个宝石装包的情况，即dp0=0,因为不装任何宝石，所以无论如何价值都为0.）
+//
+//    dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-w[i-1]]+v[i-1])
+//    背包无法再装下第i-1个宝石-> dp[i-1][j];
+//    继续将第i-1个宝石装包->  dp[i-1][j-w[i-1]]+v[i-1]。
+//
+//    搞清楚了背包问题，这个Partition Equal Subset Sum的题目就迎刃而解了。
+//            1). 判断数组中所有数的和是否为偶数，因为奇数是不可能有解的；
+//            2). 根据背包问题，取前i个数，体积为j的情况下，
+//
+//    dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-nums[i-1]]+nums[i-1])
+//
+//            3).如果最后dpnums.length=sum/2,则返回true.
     public boolean canPartition(int[] nums) {
         if(nums.length==0) return false;
         int sum=0;
