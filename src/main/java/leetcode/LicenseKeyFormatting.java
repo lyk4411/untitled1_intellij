d122e3e68ee1b10103fe23f7cc43794dc57ecbbc
ykliu <moneyflying_2006@hotmail.com>
2017-05-17 18:21:32 +0800

    core java ch 8

---
+++ src/main/java/leetcode/LicenseKeyFormatting.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * Created by lyk on 2017/5/17.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class LicenseKeyFormatting {
+    public static void main(String[] args) {
+        LicenseKeyFormatting lkf = new LicenseKeyFormatting();
+        System.out.println(lkf.licenseKeyFormatting("2-4A0r7-4k",3));
+    }
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--)
+            if (s.charAt(i) != '-')
+                sb.append(sb.length() % (k + 1) == k ? '-' : "").append(s.charAt(i));
+        return sb.reverse().toString().toUpperCase();
+    }
+}
