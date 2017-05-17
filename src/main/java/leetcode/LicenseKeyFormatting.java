68d61be7d8868bb70a0c776f369cd5b4fa1c366d
ykliu <moneyflying_2006@hotmail.com>
2017-05-17 18:37:22 +0800

    core java ch 8

--- src/main/java/leetcode/LicenseKeyFormatting.java
+++ src/main/java/leetcode/LicenseKeyFormatting.java
@@ -9,12 +9,15 @@ public class LicenseKeyFormatting {
     public static void main(String[] args) {
         LicenseKeyFormatting lkf = new LicenseKeyFormatting();
         System.out.println(lkf.licenseKeyFormatting("2-4A0r7----------4k",3));
+        System.out.println("=================");
         System.out.println(lkf.licenseKeyFormatting("2-4A0r7-4k",3));
+        System.out.println("=================");
         System.out.println(lkf.licenseKeyFormatting("24A0r74k",3));
-
+        System.out.println("=================");
     }
     public String licenseKeyFormatting(String s, int k) {
         StringBuilder sb = new StringBuilder();
+        System.out.println(s.length());
         for (int i = s.length() - 1; i >= 0; i--)
             if (s.charAt(i) != '-')
                 sb.append(sb.length() % (k + 1) == k ? '-' : "").append(s.charAt(i));
