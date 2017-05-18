48a94de1ae0520f18c957886a382d23808b04fee
ykliu <moneyflying_2006@hotmail.com>
2017-05-18 18:05:04 +0800

    core java ch 8

--- src/main/java/leetcode/PredicttheWinner.java
+++ src/main/java/leetcode/PredicttheWinner.java
@@ -27,7 +27,9 @@ public class PredicttheWinner {
     public boolean PredictTheWinner(int[] nums) {
         int n = nums.length;
         int[][] dp = new int[n][n];
-        for (int i = 0; i < n; i++) { dp[i][i] = nums[i]; }
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
         for (int len = 1; len < n; len++) {
             for (int i = 0; i < n - len; i++) {
                 int j = i + len;
