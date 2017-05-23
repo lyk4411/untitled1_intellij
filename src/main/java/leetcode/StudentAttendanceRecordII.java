b5d053741d7618df6bf7f527fb223b46b3460a64
ykliu <moneyflying_2006@hotmail.com>
2017-05-23 19:05:34 +0800

    core java ch 8

--- src/main/java/leetcode/StudentAttendanceRecordII.java
+++ src/main/java/leetcode/StudentAttendanceRecordII.java
@@ -9,43 +9,31 @@ public class StudentAttendanceRecordII {
     public static void main(String[] args) {
         StudentAttendanceRecordII sarii = new StudentAttendanceRecordII();
         System.out.println(sarii.checkRecord(2));
-        System.out.println(sarii.checkRecord(3));
-
+        //System.out.println(sarii.checkRecord(3));
     }
-//    利用dp[n][A][L]表示长度为n，包含A个字符'A'，以L个连续的'L'结尾的字符串的个数。
-//
-//    状态转移方程：
-//
-//    dp[n][0][0] = sum(dp[n - 1][0])
-//    dp[n][0][1] = dp[n - 1][0][0]
-//    dp[n][0][2] = dp[n - 1][0][1]
-//    dp[n][1][0] = sum(dp[n - 1][0]) + sum(dp[n - 1][1])
-//    dp[n][1][1] = dp[n - 1][1][0]
-//    dp[n][1][2] = dp[n - 1][1][1]
-//
-//    初始令dp[1] = [[1, 1, 0], [1, 0, 0]]
-//
-//    由于dp[n]只和dp[n - 1]有关，因此上述转移方程可以使用滚动数组，将空间复杂度降低一维。
+
+    public int checkRecord(int n) {
         final int MOD = 1000000007;
-        public long sum(int[] nums) {
-            long ans = 0;
-            for (int n : nums) ans += n;
-            return ans % MOD;
-        }
+        int[][][] f = new int[n + 1][2][3];
 
-        public int checkRecord(int n) {
-            int dp[][] = {{1, 1, 0}, {1, 0, 0}};
-            for (int i = 2; i <= n; i++) {
-                int ndp[][] = {{0, 0, 0}, {0, 0, 0}};
-                ndp[0][0] = (int)sum(dp[0]);
-                ndp[0][1] = dp[0][0];
-                ndp[0][2] = dp[0][1];
-                ndp[1][0] = (int)((sum(dp[0]) + sum(dp[1])) % MOD);
-                ndp[1][1] = dp[1][0];
-                ndp[1][2] = dp[1][1];
-                dp = ndp;
+        f[0] = new int[][]{{1, 1, 1}, {1, 1, 1}};
+        for (int i = 0; i < f.length; i++) {
+            for (int j = 0; j < f[0].length; j++) {
+                for (int k = 0; k < f[0][0].length; k++) {
+                    System.out.print("i:" + i + " j:" + j  + " k:" + k + " f[i][j][k]:" +  f[i][j][k] + "  " );
+                }
             }
-            return (int)((sum(dp[0]) + sum(dp[1])) % MOD);
+            System.out.println();
         }
 
+        for (int i = 1; i <= n; i++)
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 3; k++) {
+                    int val = f[i - 1][j][2]; // ...P
+                    if (j > 0) val = (val + f[i - 1][j - 1][2]) % MOD; // ...A
+                    if (k > 0) val = (val + f[i - 1][j][k - 1]) % MOD; // ...L
+                    f[i][j][k] = val;
+                }
+        return f[n][1][2];
+    }
 }
