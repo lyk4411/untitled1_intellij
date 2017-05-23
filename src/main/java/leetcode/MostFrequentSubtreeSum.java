7ac6d53781df6a2b188318bb056c260ab6031ffb
ykliu <moneyflying_2006@hotmail.com>
2017-05-23 15:06:11 +0800

    core java ch 8

---
+++ src/main/java/leetcode/MostFrequentSubtreeSum.java
@@ -0,0 +1,77 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by lyk on 2017/5/23.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class MostFrequentSubtreeSum {
+    public static void main(String[] args) {
+        MostFrequentSubtreeSum mfss = new MostFrequentSubtreeSum();
+        TreeNode a1 = new TreeNode(1);
+        TreeNode a2 = new TreeNode(1);
+        TreeNode a3 = new TreeNode(2);
+        TreeNode a4 = new TreeNode(2);
+        TreeNode a5 = new TreeNode(3);
+        TreeNode a6 = new TreeNode(3);
+        TreeNode a7 = new TreeNode(4);
+        TreeNode a8 = new TreeNode(4);
+        TreeNode a9 = new TreeNode(5);
+
+        a1.left = a2;
+        a1.right = a3;
+        a3.left = a4;
+        a3.right = a5;
+        a5.left = a6;
+        a5.right = a7;
+        a7.left = a8;
+        a7.right = a9;
+
+        int[] results = mfss.findFrequentTreeSum(a1);
+        for(int res : results){
+            System.out.print(res + " = ");
+        }
+    }
+    Map<Integer, Integer> sumToCount;
+    int maxCount;
+
+    public int[] findFrequentTreeSum(TreeNode root) {
+        maxCount = 0;
+        sumToCount = new HashMap<Integer, Integer>();
+
+        postOrder(root);
+
+        List<Integer> res = new ArrayList<>();
+        for (int key : sumToCount.keySet()) {
+            if (sumToCount.get(key) == maxCount) {
+                res.add(key);
+            }
+        }
+
+        int[] result = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            result[i] = res.get(i);
+        }
+        return result;
+    }
+
+    private int postOrder(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = postOrder(root.left);
+        int right = postOrder(root.right);
+
+        int sum = left + right + root.val;
+        int count = sumToCount.getOrDefault(sum, 0) + 1;
+        sumToCount.put(sum, count);
+
+        maxCount = Math.max(maxCount, count);
+
+        return sum;
+    }
+}
