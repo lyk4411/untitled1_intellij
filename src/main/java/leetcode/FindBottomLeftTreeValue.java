6f26605bb2424d26b975982fc2b9dbbdd4b38d44
ykliu <moneyflying_2006@hotmail.com>
2017-05-23 15:55:39 +0800

    core java ch 8

---
+++ src/main/java/leetcode/FindBottomLeftTreeValue.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by lyk on 2017/5/23.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class FindBottomLeftTreeValue {
+    public static void main(String[] args) {
+        FindBottomLeftTreeValue fbtv = new FindBottomLeftTreeValue();
+        TreeNode a1 = new TreeNode(1);
+        TreeNode a2 = new TreeNode(1);
+        TreeNode a3 = new TreeNode(2);
+        TreeNode a4 = new TreeNode(2);
+        TreeNode a5 = new TreeNode(3);
+        TreeNode a6 = new TreeNode(3);
+        TreeNode a7 = new TreeNode(4);
+        TreeNode a8 = new TreeNode(4);
+        TreeNode a9 = new TreeNode(5);
+
+        a1.left = a2;
+        a1.right = a3;
+        a3.left = a4;
+        a3.right = a5;
+        a5.left = a6;
+        a5.right = a7;
+        a7.left = a8;
+        a7.right = a9;
+        System.out.println(fbtv.findLeftMostNode(a1));
+    }
+    public int findLeftMostNode(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            root = queue.poll();
+            if (root.right != null)
+                queue.add(root.right);
+            if (root.left != null)
+                queue.add(root.left);
+        }
+        return root.val;
+    }
+}
