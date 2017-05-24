781018969abbf12561f626d4d4cdf69a0eeb36be
ykliu <moneyflying_2006@hotmail.com>
2017-05-24 18:26:41 +0800

    core java ch 8

--- src/main/java/leetcode/ImplementstrStr.java
+++ src/main/java/leetcode/ImplementstrStr.java
@@ -12,85 +12,79 @@ public class ImplementstrStr {
         ImplementstrStr is = new ImplementstrStr();
         System.out.println(is.strStr("ababcaababcaabc","ababcaabc"));
     }
+
+    // http://www.ruanyifeng.com/blog/2013/05/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm.html
+
+
+    public int strStr(String haystack, String needle) {
+        if(needle.length() == 0){
+            return 0;
+        }
+        int[] next = new int[needle.length()];
+        // 得到next数组
+        getNextArr(next, needle);
+        // i是匹配串haystack的指针，j是模式串needle的指针
+        int i = 0, j = 0;
+        // 双指针开始匹配
+        while(i < haystack.length() && j < needle.length()){
+            // 如果j=-1意味着刚刚失配过，下标+1后，下一轮就可以开始匹配各自的第一个了
+            // 如果指向的字母相同，则下一轮匹配各自的下一个
+            if(j == -1 || haystack.charAt(i) == needle.charAt(j)){
+                i++;
+                j++;
+                // 如果失配，则将更新j为next[j]
+            } else {
+                j = next[j];
+            }
+        }
+        return j == needle.length() ? i - j : -1;
+    }
+
+    private void getNextArr(int[] next, String needle){
+        // k是前缀中相同部分的末尾，同时也是相同部分的长度，因为长度等于k-0。
+        // j是后缀的末尾，即后缀相同部分的末尾
+        int k = -1, j = 0;
+        next[0] = -1;
+        while(j < needle.length() - 1){
+            // 如果k=-1，说明刚刚失配了，则重新开始计算前缀和后缀相同的长度
+            // 如果两个字符相等，则在上次前缀和后缀相同的长度加1就行了
+            if (k == -1 || needle.charAt(j) == needle.charAt(k)){
+                k++;
+                j++;
+                next[j] = k;
+            } else {
+                // 否则，前缀长度缩短为next[k]
+                k = next[k];
+            }
+        }
+    }
+
+
+
 //    public int strStr(String haystack, String needle) {
 //        if(haystack==null || needle==null)
 //            return 0;
 //
-//        int h = haystack.length();
-//        int n = needle.length();
-//
-//        if (n > h)
-//            return -1;
-//        if (n == 0)
+//        if(needle.length() == 0)
 //            return 0;
 //
-//        int[] next = getNext(needle);
-//        int i = 0;
+//        for (int i = 0; i <haystack.length() ; i++) {
+//            if(i+needle.length() > haystack.length()){
+//                return  -1;
+//            }
 //
-//        while (i <= h - n) {
-//            int success = 1;
-//            for (int j = 0; j < n; j++) {
-//                if (needle.charAt(0) != haystack.charAt(i)) {
-//                    success = 0;
-//                    i++;
-//                    break;
-//                } else if (needle.charAt(j) != haystack.charAt(i + j)) {
-//                    success = 0;
-//                    i = i + j - next[j - 1];
+//            int m = i;
+//            for (int j = 0; j < needle.length(); j++) {
+//                if(needle.charAt(j)==haystack.charAt(m)){
+//                    if(j==needle.length()-1){
+//                        return i;
+//                    }
+//                    m++;
+//                } else {
 //                    break;
 //                }
 //            }
-//            if (success == 1)
-//                return i;
 //        }
-//
 //        return -1;
 //    }
-//
-//    //calculate KMP array
-//    public int[] getNext(String needle) {
-//        int[] next = new int[needle.length()];
-//        next[0] = 0;
-//
-//        for (int i = 1; i < needle.length(); i++) {
-//            int index = next[i - 1];
-//            while (index > 0 && needle.charAt(index) != needle.charAt(i)) {
-//                index = next[index - 1];
-//            }
-//
-//            if (needle.charAt(index) == needle.charAt(i)) {
-//                next[i] = next[i - 1] + 1;
-//            } else {
-//                next[i] = 0;
-//            }
-//        }
-//
-//        return next;
-//    }
-    public int strStr(String haystack, String needle) {
-        if(haystack==null || needle==null)
-            return 0;
-
-        if(needle.length() == 0)
-            return 0;
-
-        for (int i = 0; i <haystack.length() ; i++) {
-            if(i+needle.length() > haystack.length()){
-                return  -1;
-            }
-
-            int m = i;
-            for (int j = 0; j < needle.length(); j++) {
-                if(needle.charAt(j)==haystack.charAt(m)){
-                    if(j==needle.length()-1){
-                        return i;
-                    }
-                    m++;
-                } else {
-                    break;
-                }
-            }
-        }
-        return -1;
-    }
 }
