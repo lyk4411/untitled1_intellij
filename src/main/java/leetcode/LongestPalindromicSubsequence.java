b871197f83797cccb670f00982f21b7c8da44be0
ykliu <moneyflying_2006@hotmail.com>
2017-05-24 16:10:47 +0800

    core java ch 8

--- src/main/java/leetcode/LongestPalindromicSubsequence.java
+++ src/main/java/leetcode/LongestPalindromicSubsequence.java
@@ -14,9 +14,11 @@ public class LongestPalindromicSubsequence {
     }
     public int longestPalindromeSubseq(String s) {
         int[][] dp = new int[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = 1;
+        }
 
         for (int i = s.length() - 1; i >= 0; i--) {
-            dp[i][i] = 1;
             for (int j = i+1; j < s.length(); j++) {
                 if (s.charAt(i) == s.charAt(j)) {
                     dp[i][j] = dp[i+1][j-1] + 2;
