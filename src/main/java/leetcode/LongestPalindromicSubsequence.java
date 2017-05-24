e82a0da8d86d743dd26e146376694beb858a6e63
ykliu <moneyflying_2006@hotmail.com>
2017-05-24 16:17:06 +0800

    core java ch 8

--- src/main/java/leetcode/LongestPalindromicSubsequence.java
+++ src/main/java/leetcode/LongestPalindromicSubsequence.java
@@ -27,6 +27,12 @@ public class LongestPalindromicSubsequence {
                 }
             }
         }
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " : ");
+            }
+            System.out.println();
+        }
         return dp[0][s.length()-1];
     }
 }
