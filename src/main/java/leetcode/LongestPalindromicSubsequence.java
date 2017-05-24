4cdaffe0eabb9de48bf3cd804d7e910a10a46988
ykliu <moneyflying_2006@hotmail.com>
2017-05-24 16:18:23 +0800

    core java ch 8

--- src/main/java/leetcode/LongestPalindromicSubsequence.java
+++ src/main/java/leetcode/LongestPalindromicSubsequence.java
@@ -10,8 +10,13 @@ public class LongestPalindromicSubsequence {
     public static void main(String[] args) {
         LongestPalindromicSubsequence lps = new LongestPalindromicSubsequence();
         System.out.println(lps.longestPalindromeSubseq("abcdedcbaaa"));
-
     }
+
+//    dp[i][j]: the longest palindromic subsequence's length of substring(i, j)
+//    State transition:
+//    dp[i][j] = dp[i+1][j-1] + 2 if s.charAt(i) == s.charAt(j)
+//    otherwise, dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1])
+//    Initialization: dp[i][i] = 1
     public int longestPalindromeSubseq(String s) {
         int[][] dp = new int[s.length()][s.length()];
         for (int i = 0; i < s.length(); i++) {
