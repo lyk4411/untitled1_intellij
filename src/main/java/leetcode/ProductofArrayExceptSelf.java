9426ea580f0e0719efd7c8182a1271a3c6a6127a
ykliu <moneyflying_2006@hotmail.com>
2017-05-25 14:48:36 +0800

    core java ch 8

---
+++ src/main/java/leetcode/ProductofArrayExceptSelf.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * Created by lyk on 2017/5/25.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class ProductofArrayExceptSelf {
+    public static void main(String[] args) {
+        ProductofArrayExceptSelf paes = new ProductofArrayExceptSelf();
+        int[] nums = new int[]{2,3,4,5};
+        int[] results = paes.productExceptSelf(nums);
+        for (int i = 0; i < results.length; i++) {
+            System.out.print(results[i] + " :");
+        }
+        System.out.println();
+    }
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+}
