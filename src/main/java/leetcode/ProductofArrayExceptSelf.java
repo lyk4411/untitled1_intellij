3ca36f7ea46d0cb9d053ed4378143a821a199147
ykliu <moneyflying_2006@hotmail.com>
2017-05-25 15:02:56 +0800

    core java ch 8

--- src/main/java/leetcode/ProductofArrayExceptSelf.java
+++ src/main/java/leetcode/ProductofArrayExceptSelf.java
@@ -8,7 +8,7 @@ package leetcode;
 public class ProductofArrayExceptSelf {
     public static void main(String[] args) {
         ProductofArrayExceptSelf paes = new ProductofArrayExceptSelf();
-        int[] nums = new int[]{2,3,4,5};
+        int[] nums = new int[]{0,4,0};
         int[] results = paes.productExceptSelf(nums);
         for (int i = 0; i < results.length; i++) {
             System.out.print(results[i] + " :");
@@ -17,14 +17,15 @@ public class ProductofArrayExceptSelf {
     }
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int temp = 1;
-        for (int i = 0; i < n; i++) {
-            temp *= nums[i];
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
         }
-        System.out.println(temp);
-        int[] res = new int[nums.length];
+        int right = 1;
         for (int i = n - 1; i >= 0; i--) {
-            res[i] = temp / nums[i];
+            res[i] *= right;
+            right *= nums[i];
         }
         return res;
     }
