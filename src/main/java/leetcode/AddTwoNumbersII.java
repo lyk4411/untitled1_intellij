dbb7c2c45e8ebd82a31c9f4359446b6ccb3ba5b3
ykliu <moneyflying_2006@hotmail.com>
2017-05-26 18:47:38 +0800

    core java ch 8

---
+++ src/main/java/leetcode/AddTwoNumbersII.java
@@ -0,0 +1,70 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * Created by lyk on 2017/5/26.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class AddTwoNumbersII {
+    public static void main(String[] args) {
+        AddTwoNumbersII atnii = new AddTwoNumbersII();
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = l6;
+
+        ListNode ln1 = new ListNode(1);
+        ListNode ln2 = new ListNode(1);
+        ListNode ln3 = new ListNode(2);
+        ListNode ln4 = new ListNode(2);
+        ListNode ln5 = new ListNode(2);
+        ListNode ln6 = new ListNode(3);
+        ListNode ln7 = new ListNode(4);
+        ListNode ln8 = new ListNode(5);
+        ln1.next = ln2;
+        ln2.next = ln3;
+        ln3.next = ln4;
+        ln4.next = ln5;
+        ln5.next = ln6;
+        ln6.next = ln7;
+        ln7.next = ln8;
+        System.out.println(atnii.addTwoNumbers(l1,ln1));
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<Integer>();
+        Stack<Integer> s2 = new Stack<Integer>();
+
+        while(l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        };
+        while(l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int sum = 0;
+        ListNode list = new ListNode(0);
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) sum += s1.pop();
+            if (!s2.empty()) sum += s2.pop();
+            list.val = sum % 10;
+            ListNode head = new ListNode(sum / 10);
+            head.next = list;
+            list = head;
+            sum /= 10;
+        }
+
+        return list.val == 0 ? list.next : list;
+    }
+}
