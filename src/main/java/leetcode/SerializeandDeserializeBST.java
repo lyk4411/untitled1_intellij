06892e347c0e5fe33b3bc4ad86108ab0357db323
ykliu <moneyflying_2006@hotmail.com>
2017-05-27 15:51:36 +0800

    core java ch 8

---
+++ src/main/java/leetcode/SerializeandDeserializeBST.java
@@ -0,0 +1,85 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * Created by lyk on 2017/5/27.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class SerializeandDeserializeBST {
+    public static void main(String[] args) {
+        SerializeandDeserializeBST sd = new SerializeandDeserializeBST();
+        TreeNode a1 = new TreeNode(1);
+        TreeNode a2 = new TreeNode(1);
+        TreeNode a3 = new TreeNode(2);
+        TreeNode a4 = new TreeNode(2);
+        TreeNode a5 = new TreeNode(3);
+        TreeNode a6 = new TreeNode(3);
+        TreeNode a7 = new TreeNode(4);
+        TreeNode a8 = new TreeNode(4);
+        TreeNode a9 = new TreeNode(5);
+
+        a1.left = a2;
+        a1.right = a3;
+        a2.left = a4;
+        a2.right = a5;
+        a3.left = a6;
+        a3.right = a7;
+        a4.left = a8;
+        a4.right = a9;
+        String serTree = sd.serialize(a1);
+        System.out.println(serTree);
+        System.out.println(sd.deserialize(serTree));
+    }
+
+    private static final String SEP = ",";
+    private static final String NULL = "null";
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        if (root == null) return NULL;
+        //traverse it recursively if you want to, I am doing it iteratively here
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while (!st.empty()) {
+            root = st.pop();
+            sb.append(root.val).append(SEP);
+            if (root.right != null) st.push(root.right);
+            if (root.left != null) st.push(root.left);
+        }
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    // pre-order traversal
+    public TreeNode deserialize(String data) {
+        if (data.equals(NULL)) return null;
+        String[] strs = data.split(SEP);
+        Queue<Integer> q = new LinkedList<>();
+        for (String e : strs) {
+            q.offer(Integer.parseInt(e));
+        }
+        return getNode(q);
+    }
+
+    // some notes:
+    //   5
+    //  3 6
+    // 2   7
+    private TreeNode getNode(Queue<Integer> q) { //q: 5,3,2,6,7
+        if (q.isEmpty()) return null;
+        TreeNode root = new TreeNode(q.poll());//root (5)
+        Queue<Integer> samllerQueue = new LinkedList<>();
+        while (!q.isEmpty() && q.peek() < root.val) {
+            samllerQueue.offer(q.poll());
+        }
+        //smallerQueue : 3,2   storing elements smaller than 5 (root)
+        root.left = getNode(samllerQueue);
+        //q: 6,7   storing elements bigger than 5 (root)
+        root.right = getNode(q);
+        return root;
+    }
+}
