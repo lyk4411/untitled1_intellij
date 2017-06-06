31383d3ffc9f8d66b4265bbad8593eacee5e96d8
ykliu <moneyflying_2006@hotmail.com>
2017-06-06 17:36:24 +0800

    core java ch 8

---
+++ src/main/java/leetcode/DeleteNodeinaBST.java
@@ -0,0 +1,58 @@
+package leetcode;
+
+/**
+ * Created by lyk on 2017/6/5.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class DeleteNodeinaBST {
+    public static void main(String[] args) {
+        DeleteNodeinaBST dnbst = new DeleteNodeinaBST();
+        TreeNode a1 = new TreeNode(5);
+        TreeNode a2 = new TreeNode(3);
+        TreeNode a3 = new TreeNode(6);
+        TreeNode a4 = new TreeNode(2);
+        TreeNode a5 = new TreeNode(4);
+        TreeNode a6 = new TreeNode(7);
+        TreeNode a7 = new TreeNode(1);
+        TreeNode a8 = new TreeNode(8);
+
+        a1.left = a2;
+        a1.right = a3;
+        a2.left = a4;
+        a2.right = a5;
+        a3.right = a6;
+        a4.left = a7;
+        a6.right = a8;
+        System.out.println(a1);
+        System.out.println(dnbst.deleteNode(a1,6));
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null){
+            return null;
+        }
+        if(key < root.val){
+            root.left = deleteNode(root.left, key);
+        }else if(key > root.val){
+            root.right = deleteNode(root.right, key);
+        }else{
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
+
+            TreeNode minNode = findMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, root.val);
+        }
+        return root;
+    }
+
+    private TreeNode findMin(TreeNode node){
+        while(node.left != null){
+            node = node.left;
+        }
+        return node;
+    }
+}
