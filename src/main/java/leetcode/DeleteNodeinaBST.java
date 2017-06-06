d4bbb95e69839036f88d5b0550f501e30aedfefd
ykliu <moneyflying_2006@hotmail.com>
2017-06-06 18:43:19 +0800

    core java ch 8

--- src/main/java/leetcode/DeleteNodeinaBST.java
+++ src/main/java/leetcode/DeleteNodeinaBST.java
@@ -27,6 +27,50 @@ public class DeleteNodeinaBST {
         System.out.println(a1);
         System.out.println(dnbst.deleteNode(a1,6));
     }
+
+//    private TreeNode deleteRootNode(TreeNode root) {
+//        if (root == null) {
+//            return null;
+//        }
+//        if (root.left == null) {
+//            return root.right;
+//        }
+//        if (root.right == null) {
+//            return root.left;
+//        }
+//        TreeNode next = root.right;
+//        TreeNode pre = null;
+//        for(; next.left != null; pre = next, next = next.left);
+//        next.left = root.left;
+//        //if(root.right != next) {
+//        if(pre != null)  {
+//            pre.left = next.right;
+//            next.right = root.right;
+//        }
+//        return next;
+//    }
+//
+//    public TreeNode deleteNode(TreeNode root, int key) {
+//        TreeNode cur = root;
+//        TreeNode pre = null;
+//        while(cur != null && cur.val != key) {
+//            pre = cur;
+//            if (key < cur.val) {
+//                cur = cur.left;
+//            } else if (key > cur.val) {
+//                cur = cur.right;
+//            }
+//        }
+//        if (pre == null) {
+//            return deleteRootNode(cur);
+//        }
+//        if (pre.left == cur) {
+//            pre.left = deleteRootNode(cur);
+//        } else {
+//            pre.right = deleteRootNode(cur);
+//        }
+//        return root;
+//    }
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root == null){
             return null;
