8f9ede66c31c2cead42dde2f6501a3d6290edaad
ykliu <moneyflying_2006@hotmail.com>
2017-06-07 16:02:50 +0800

    core java ch 8

---
+++ src/main/java/leetcode/SubarraySumEqualsK.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by lyk on 2017/6/7.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class SubarraySumEqualsK {
+    public static void main(String[] args) {
+        SubarraySumEqualsK ssek = new SubarraySumEqualsK();
+        int[] nums = new int[]{1,2,3,4};
+        System.out.println(ssek.subarraySum(nums,3));
+    }
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0, result = 0;
+        Map<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (preSum.containsKey(sum - k)) {
+                result += preSum.get(sum - k);
+            }
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+
+        return result;
+    }
+}
