515f3400654bf0efcdde4535dc775aef5be521dd
ykliu <moneyflying_2006@hotmail.com>
2017-06-07 10:56:47 +0800

    core java ch 8

--- src/main/java/leetcode/FriendCircles.java
+++ src/main/java/leetcode/FriendCircles.java
@@ -16,6 +16,9 @@ public class FriendCircles {
         };
         System.out.println(fc.findCircleNum(matrix));
     }
+    //那么比较直接的解法就是DFS搜索，对于某个人，遍历其好友，然后再遍历其好友的好友，
+    // 那么我们就能把属于同一个朋友圈的人都遍历一遍，我们同时标记出已经遍历过的人，
+    // 然后累积朋友圈的个数，再去对于没有遍历到的人在找其朋友圈的人，这样就能求出个数。
     public void dfs(int[][] M, int[] visited, int i) {
         for (int j = 0; j < M.length; j++) {
             if (M[i][j] == 1 && visited[j] == 0) {
