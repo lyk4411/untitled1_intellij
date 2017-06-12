1190f726ce34194e22edc1affc8441508729b9c6
ykliu <moneyflying_2006@hotmail.com>
2017-06-12 19:12:14 +0800

    core java ch 8

---
+++ src/main/java/leetcode/PermutationinString.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+/**
+ * Created by lyk on 2017/6/12.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class PermutationinString {
+    public static void main(String[] args) {
+        PermutationinString ps = new PermutationinString();
+        String s1 = "ab";
+        String s2 = "ba";
+        String s3 = "eidbaooo";
+        System.out.println(ps.checkInclusion(s1,s3));
+        System.out.println(ps.checkInclusion(s1,s1));
+        System.out.println(ps.checkInclusion(s2,s3));
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        int len1 = s1.length(), len2 = s2.length();
+        if (len1 > len2) return false;
+
+        int[] count = new int[26];
+        for (int i = 0; i < len1; i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+        if (allZero(count)) return true;
+
+        for (int i = len1; i < len2; i++) {
+            count[s2.charAt(i) - 'a']--;
+            count[s2.charAt(i - len1) - 'a']++;
+            if (allZero(count)) return true;
+        }
+
+        return false;
+    }
+
+    private boolean allZero(int[] count) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
+    }
+}
