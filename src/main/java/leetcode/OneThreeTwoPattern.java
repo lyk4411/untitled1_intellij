2c607abf0227f9b0bee7392ae6e0788dcd8c0ef4
ykliu <moneyflying_2006@hotmail.com>
2017-06-13 19:25:44 +0800

    core java ch 8

--- src/main/java/leetcode/OneThreeTwoPattern.java
+++ src/main/java/leetcode/OneThreeTwoPattern.java
@@ -45,28 +45,18 @@ public class OneThreeTwoPattern {
 
 
 
-    class Pair{
-        int min, max;
-        public Pair(int min, int max){
-            this.min = min;
-            this.max = max;
-        }
-    }
-    public boolean find132pattern(int[] nums) {
-        Stack<Pair> stack = new Stack();
-        for(int n: nums){
-            if(stack.isEmpty() || n <stack.peek().min ) stack.push(new Pair(n,n));
-            else if(n > stack.peek().min){
-                Pair last = stack.pop();
-                if(n < last.max) return true;
-                else {
-                    last.max = n;
-                    while(!stack.isEmpty() && n >= stack.peek().max) stack.pop();
-                    // At this time, n < stack.peek().max (if stack not empty)
-                    if(!stack.isEmpty() && stack.peek().min < n) return true;
-                    stack.push(last);
-                }
 
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> stack = new Stack();
+        int third = Integer.MIN_VALUE;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if(nums[i] < third) return true;
+            else {
+                while (!stack.empty() && nums[i] > stack.peek()){
+                    third = stack.peek();
+                    stack.pop();
+                }
+                stack.push(nums[i]);
             }
         }
         return false;
