154b9460717e1ae4fdf775893314b0a1f9fb8077
ykliu <moneyflying_2006@hotmail.com>
2017-06-13 19:08:22 +0800

    core java ch 8

--- src/main/java/leetcode/OneThreeTwoPattern.java
+++ src/main/java/leetcode/OneThreeTwoPattern.java
@@ -13,7 +13,14 @@ public class OneThreeTwoPattern {
         System.out.println(ottp.find132pattern(nums1));
         System.out.println(ottp.find132pattern(nums2));
     }
-
+    //这道题给我们了一个数组，让我们找到132的模式，就是第一个数小于第二第三个数，
+    // 且第三个数小于第二个数。那么我们就按顺序来找这三个数，首先我们来找第一个数，
+    // 这个数需要最小，那么我们如果发现当前数字大于等于后面一个数字，我们就往下继
+    // 续遍历，直到当前数字小于下一个数字停止。然后我们找第二个数字，这个数字需要
+    // 最大，那么如果我们发现当前数字小于等于下一个数字就继续遍历，直到当前数字大
+    // 于下一个数字停止。最后就找第三个数字，我们验证这个数字是否在之前两个数字的
+    // 中间，如果没有找到，我们就从第二个数字的后面一个位置继续开始重新找这三个
+    // 数字，参见代码如下：
     public boolean find132pattern(int[] nums) {
         if(nums.length<3) return false;
         Integer low = null, high = null;
