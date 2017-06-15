e9d5b451c312eab32025873b3fed27a8140143a7
ykliu <moneyflying_2006@hotmail.com>
2017-06-15 18:39:16 +0800

    core java ch 8

--- src/main/java/leetcode/TotalHammingDistance.java
+++ src/main/java/leetcode/TotalHammingDistance.java
@@ -13,7 +13,10 @@ public class TotalHammingDistance {
         System.out.println(thd.totalHammingDistance(nums1));
         System.out.println(thd.totalHammingDistance(nums2));
     }
-
+//For each bit position 1-32 in a 32-bit integer, we count the number of
+// integers in the array which have that bit set. Then, if there are n integers
+// in the array and k of them have a particular bit set and (n-k) do not, then
+// that bit contributes k*(n-k) hamming distance to the total.
     public int totalHammingDistance(int[] nums) {
         int total = 0, n = nums.length;
         for (int j = 0; j < 32; j++) {
