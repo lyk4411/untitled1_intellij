203401e04cc2cdfd5cd48c7f434681912c405d10
ykliu <moneyflying_2006@hotmail.com>
2017-06-15 18:54:15 +0800

    core java ch 8

--- src/main/java/leetcode/OnesandZeroes.java
+++ src/main/java/leetcode/OnesandZeroes.java
@@ -12,6 +12,14 @@ public class OnesandZeroes {
         System.out.println(oz.findMaxForm(strs1,5,4));
         System.out.println(oz.findMaxForm(strs1,5,1));
     }
+    // 我们建立一个二位的DP数组，其中dp[i][j]表示有i个0和j个1时能组成的最
+    // 多字符串的个数，而对于当前遍历到的字符串，我们统计出其中0和1的个数为
+    // zeros和ones，然后dp[i - zeros][j - ones]表示当前的i和j减去zeros和ones
+    // 之前能拼成字符串的个数，那么加上当前的zeros和ones就是当前dp[i][j]可以
+    // 达到的个数，我们跟其原有数值对比取较大值即可，所以递推式如下：
+
+    // dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1);
+    // 有了递推式，我们就可以很容易的写出代码如下：
     public int findMaxForm(String[] strs, int m, int n) {
         int[][] dp = new int[m+1][n+1];
         for (String s : strs) {
