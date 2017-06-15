e1d2f08163c05c2df0c85009b461d583f80dc13c
ykliu <moneyflying_2006@hotmail.com>
2017-06-15 19:15:51 +0800

    core java ch 8

--- src/main/java/leetcode/BurstBalloons.java
+++ src/main/java/leetcode/BurstBalloons.java
@@ -13,6 +13,15 @@ public class BurstBalloons {
         System.out.println(bb.maxCoins(nums1));
         System.out.println(bb.maxCoins(nums2));
     }
+    // 我们维护一个二维动态数组dp，其中dp[i][j]表示打爆区间[i,j]中的所有气球能得到的最多金币。
+    // 题目中说明了边界情况，当气球周围没有气球的时候，旁边的数字按1算，这样我们可以在原数组
+    // 两边各填充一个1，这样方便于计算。这道题的最难点就是找递归式，如下所示：
+    // dp[i][j] = max(dp[i][j], nums[i - 1]*nums[k]*nums[j + 1] + dp[i][k - 1] + dp[k + 1][j])
+    //       ( i ≤ k ≤ j )
+    //有了递推式，我们可以写代码，我们其实只是更新了dp数组的右上三角区域，
+    // 我们最终要返回的值存在dp[1][n]中，其中n是两端添加1之前数组nums的个数。
+    // 参见代码如下：
+
     public int maxCoins(int[] iNums) {
         int[] nums = new int[iNums.length + 2];
         int n = 1;
