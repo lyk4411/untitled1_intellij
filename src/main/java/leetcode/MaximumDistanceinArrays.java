d65d7c33f2edd9b57b681f216f8ea293f3ad2b3b
ykliu <moneyflying_2006@hotmail.com>
2017-06-20 17:40:50 +0800

    core java ch 8

--- src/main/java/leetcode/MaximumDistanceinArrays.java
+++ src/main/java/leetcode/MaximumDistanceinArrays.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by lyk on 2017/6/20.
  * Package name: leetcode
@@ -8,23 +11,27 @@ package leetcode;
 public class MaximumDistanceinArrays {
     public static void main(String[] args) {
         MaximumDistanceinArrays mda = new MaximumDistanceinArrays();
-        int[][] arrays = new int[][]{
-                {1,2,3},
-                {4,5},
-                {1,2,3}
-        };
+        List<List<Integer>> arrays = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        List<Integer> list3 = new ArrayList<>();
+        list1.add(3);list1.add(2);list1.add(1);
+        list2.add(5);list2.add(4);
+        list3.add(3);list3.add(2);list3.add(1);
+        arrays.add(list1);arrays.add(list2);arrays.add(list3);
+
         System.out.println(mda.maxDistance(arrays));
     }
-    public int maxDistance(int[][] arrays) {
+    public int maxDistance(List<List<Integer>> arrays) {
         int result = Integer.MIN_VALUE;
-        int max = arrays[0][arrays[0].length - 1];
-        int min = arrays[0][0];
+        int max = arrays.get(0).get(arrays.get(0).size() - 1);
+        int min = arrays.get(0).get(0);
 
-        for (int i = 1; i < arrays.length; i++) {
-            result = Math.max(result, Math.abs(arrays[i][0] - max));
-            result = Math.max(result, Math.abs(arrays[i][arrays[i].length - 1] - min));
-            max = Math.max(max, arrays[i][arrays[i].length - 1]);
-            min = Math.min(min, arrays[i][0]);
+        for (int i = 1; i < arrays.size(); i++) {
+            result = Math.max(result, Math.abs(arrays.get(i).get(0) - max));
+            result = Math.max(result, Math.abs(arrays.get(i).get(arrays.get(i).size() - 1) - min));
+            max = Math.max(max, arrays.get(i).get(arrays.get(i).size() - 1));
+            min = Math.min(min, arrays.get(i).get(0));
         }
 
         return result;
