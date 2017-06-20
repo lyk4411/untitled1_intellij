2b82021ee3e4ec778cec34efdca9ab8c68468972
ykliu <moneyflying_2006@hotmail.com>
2017-06-20 14:44:59 +0800

    core java ch 8

--- src/main/java/leetcode/LongestIncreasingPathinaMatrix.java
+++ src/main/java/leetcode/LongestIncreasingPathinaMatrix.java
@@ -24,20 +24,20 @@ public class LongestIncreasingPathinaMatrix {
         int max = 1;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                int len = dfs(matrix, i, j, m, n, cache);
+                int len = dfs(matrix, i, j, m, n,cache);
                 max = Math.max(max, len);
             }
         }
         return max;
     }
 
-    public int dfs(int[][] matrix, int i, int j, int m, int n, int[][] cache) {
+    public int dfs(int[][] matrix, int i, int j, int m, int n,int[][] cache) {
         if(cache[i][j] != 0) return cache[i][j];
         int max = 1;
         for(int[] dir: dirs) {
             int x = i + dir[0], y = j + dir[1];
             if(x < 0 || x >= m || y < 0 || y >= n || matrix[x][y] <= matrix[i][j]) continue;
-            int len = 1 + dfs(matrix, x, y, m, n, cache);
+            int len = 1 + dfs(matrix, x, y, m, n,cache);
             max = Math.max(max, len);
         }
         cache[i][j] = max;
