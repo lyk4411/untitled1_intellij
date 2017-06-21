c5771e79742ffe66669ee72078cf70992f311863
ykliu <moneyflying_2006@hotmail.com>
2017-06-21 15:41:40 +0800

    core java ch 8

---
+++ src/main/java/leetcode/SlidingWindowMedian.java
@@ -0,0 +1,87 @@
+package leetcode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Created by lyk on 2017/6/21.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class SlidingWindowMedian {
+    public static void main(String[] args) {
+        SlidingWindowMedian swm = new SlidingWindowMedian();
+        int[] nums = new int[]{1,3,-1,-3,5,3,6,7};
+        double[] results= swm.medianSlidingWindow(nums,3);
+        for (int i = 0; i < results.length; i++) {
+            System.out.print("results[" + i + "]:" + results[i] + ";  ");
+        }
+    }
+    PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(
+            new Comparator<Integer>() {
+                public int compare(Integer i1, Integer i2) {
+                    return i2.compareTo(i1);
+                }
+            }
+    );
+
+    public double[] medianSlidingWindow(int[] nums, int k) {
+        int n = nums.length - k + 1;
+        if (n <= 0) return new double[0];
+        double[] result = new double[n];
+
+        for (int i = 0; i <= nums.length; i++) {
+            if (i >= k) {
+                result[i - k] = getMedian();
+                remove(nums[i - k]);
+            }
+            if (i < nums.length) {
+                add(nums[i]);
+            }
+        }
+
+        return result;
+    }
+
+    private void add(int num) {
+        if (num < getMedian()) {
+            maxHeap.add(num);
+        }
+        else {
+            minHeap.add(num);
+        }
+        if (maxHeap.size() > minHeap.size()) {
+            minHeap.add(maxHeap.poll());
+        }
+        if (minHeap.size() - maxHeap.size() > 1) {
+            maxHeap.add(minHeap.poll());
+        }
+    }
+
+    private void remove(int num) {
+        if (num < getMedian()) {
+            maxHeap.remove(num);
+        }
+        else {
+            minHeap.remove(num);
+        }
+        if (maxHeap.size() > minHeap.size()) {
+            minHeap.add(maxHeap.poll());
+        }
+        if (minHeap.size() - maxHeap.size() > 1) {
+            maxHeap.add(minHeap.poll());
+        }
+    }
+
+    private double getMedian() {
+        if (maxHeap.isEmpty() && minHeap.isEmpty()) return 0;
+
+        if (maxHeap.size() == minHeap.size()) {
+            return ((double)maxHeap.peek() + (double)minHeap.peek()) / 2.0;
+        }
+        else {
+            return (double)minHeap.peek();
+        }
+    }
+}
