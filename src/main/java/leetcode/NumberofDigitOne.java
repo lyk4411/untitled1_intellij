5c12a13a68415fb048d2a7d3288c64c843d77130
ykliu <moneyflying_2006@hotmail.com>
2017-06-22 18:15:56 +0800

    core java ch 8

--- src/main/java/leetcode/NumberofDigitOne.java
+++ src/main/java/leetcode/NumberofDigitOne.java
@@ -8,14 +8,23 @@ package leetcode;
 public class NumberofDigitOne {
     public static void main(String[] args) {
         NumberofDigitOne ndo = new NumberofDigitOne();
+        System.out.println(ndo.countDigitOne(7));
         System.out.println(ndo.countDigitOne(13));
         System.out.println(ndo.countDigitOne(130));
     }
 
     public int countDigitOne(int n) {
-        int ones = 0;
-        for (long m = 1; m <= n; m *= 10)
-            ones += (n/m + 8) / 10 * m + (n/m % 10 == 1 ? n%m + 1 : 0);
-        return ones;
+        if (n <= 0) return 0;
+        int q = n, x = 1, ans = 0;
+        do {
+            int digit = q % 10;
+            q /= 10;
+            ans += q * x;
+            if (digit == 1) ans += n % x + 1;
+            if (digit >  1) ans += x;
+            x *= 10;
+        } while (q > 0);
+        return ans;
+
     }
 }
