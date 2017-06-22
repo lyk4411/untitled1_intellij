0ee9a135ce14f80cb2cd8c13cf1ebd4b6185c55d
ykliu <moneyflying_2006@hotmail.com>
2017-06-22 19:01:47 +0800

    core java ch 8

--- src/main/java/leetcode/CountofSmallerNumbersAfterSelf.java
+++ src/main/java/leetcode/CountofSmallerNumbersAfterSelf.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,34 +17,30 @@ public class CountofSmallerNumbersAfterSelf {
     }
     public List<Integer> countSmaller(int[] nums) {
         Integer[] ans = new Integer[nums.length];
-        Node root = null;
+        List<Integer> sorted = new ArrayList<Integer>();
         for (int i = nums.length - 1; i >= 0; i--) {
-            root = insert(nums[i], root, ans, i, 0);
+            int index = findIndex(sorted, nums[i]);
+            ans[i] = index;
+            sorted.add(index, nums[i]);
         }
         return Arrays.asList(ans);
     }
-    private Node insert(int num, Node node, Integer[] ans, int i, int preSum) {
-        if (node == null) {
-            node = new Node(num, 0);
-            ans[i] = preSum;
-        } else if (node.val == num) {
-            node.dup++;
-            ans[i] = preSum + node.sum;
-        } else if (node.val > num) {
-            node.sum++;
-            node.left = insert(num, node.left, ans, i, preSum);
-        } else {
-            node.right = insert(num, node.right, ans, i, preSum + node.dup + node.sum);
+    private int findIndex(List<Integer> sorted, int target) {
+        if (sorted.size() == 0) return 0;
+        int start = 0;
+        int end = sorted.size() - 1;
+        if (sorted.get(end) < target) return end + 1;
+        if (sorted.get(start) >= target) return 0;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (sorted.get(mid) < target) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
         }
-        return node;
+        if (sorted.get(start) >= target) return start;
+        return end;
     }
 }
 
-class Node {
-    Node left, right;
-    int val, sum, dup = 1;
-    public Node(int v, int s) {
-        val = v;
-        sum = s;
-    }
-}
