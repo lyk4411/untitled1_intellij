26903ce747f4299e5a45f52b7cd0b14b4c47176a
ykliu <moneyflying_2006@hotmail.com>
2017-06-23 18:34:01 +0800

    core java ch 8

--- src/main/java/leetcode/RussianDollEnvelopes.java
+++ src/main/java/leetcode/RussianDollEnvelopes.java
@@ -21,27 +21,36 @@ public class RussianDollEnvelopes {
     }
 
     public int maxEnvelopes(int[][] envelopes) {
-        if(envelopes == null || envelopes.length == 0
-                || envelopes[0] == null || envelopes[0].length != 2)
+        if (   envelopes           == null
+                || envelopes.length    == 0
+                || envelopes[0]        == null
+                || envelopes[0].length == 0){
             return 0;
+        }
+
         Arrays.sort(envelopes, new Comparator<int[]>(){
-            public int compare(int[] arr1, int[] arr2){
-                if(arr1[0] == arr2[0])
-                    return arr2[1] - arr1[1];
-                else
-                    return arr1[0] - arr2[0];
+            @Override
+            public int compare(int[] e1, int[] e2){
+                return Integer.compare(e1[0], e2[0]);
             }
         });
-        int dp[] = new int[envelopes.length];
-        int len = 0;
-        for(int[] envelope : envelopes){
-            int index = Arrays.binarySearch(dp, 0, len, envelope[1]);
-            if(index < 0)
-                index = -(index + 1);
-            dp[index] = envelope[1];
-            if(index == len)
-                len++;
+
+        int   n  = envelopes.length;
+        int[] dp = new int[n];
+
+        int ret = 0;
+        for (int i = 0; i < n; i++){
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++){
+                if (   envelopes[i][0] > envelopes[j][0]
+                        && envelopes[i][1] > envelopes[j][1]){
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                }
+            }
+
+            ret = Math.max(ret, dp[i]);
         }
-        return len;
+        return ret;
     }
 }
