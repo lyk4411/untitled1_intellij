6b69c33ae72a4a7faf2ce0fe96b74d9940d2fd02
ykliu <moneyflying_2006@hotmail.com>
2017-06-26 18:16:02 +0800

    core java ch 8

--- src/main/java/leetcode/KInversePairsArray.java
+++ src/main/java/leetcode/KInversePairsArray.java
@@ -8,23 +8,29 @@ package leetcode;
 public class KInversePairsArray {
     public static void main(String[] args) {
         KInversePairsArray kipa = new KInversePairsArray();
-        System.out.println(kipa.kInversePairs(3,1));
+        System.out.println(kipa.kInversePairs(10,4));
     }
-    public static int kInversePairs(int n, int k) {
-        int mod = 1000000007;
-        if (k > n*(n-1)/2 || k < 0) return 0;
-        if (k == 0 || k == n*(n-1)/2) return 1;
+    public int kInversePairs(int n, int k) {
         long[][] dp = new long[n+1][k+1];
-        dp[2][0] = 1;
-        dp[2][1] = 1;
-        for (int i = 3; i <= n; i++) {
+
+        for(int i=0; i<=n; i++)
             dp[i][0] = 1;
-            for (int j = 1; j <= Math.min(k, i*(i-1)/2); j++) {
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
-                if (j >= i) dp[i][j] -= dp[i-1][j-i];
-                dp[i][j] = (dp[i][j]+mod) % mod;
+
+        for(int i=1; i<=n; i++)
+            for(int j=1; j<=k; j++) {
+                dp[i][j] = dp[i][j-1]+dp[i-1][j];
+                if(j-i >= 0) dp[i][j] -= dp[i-1][j-i];
+                dp[i][j] += 1000000007;
+                dp[i][j] %= 1000000007;
             }
-        }
+
+//        for (int i = 0; i < dp.length; i++) {
+//            for (int j = 0; j < dp[0].length; j++) {
+//                System.out.print(dp[i][j] + "  :");
+//            }
+//            System.out.println();
+//        }
+
         return (int) dp[n][k];
     }
 }
