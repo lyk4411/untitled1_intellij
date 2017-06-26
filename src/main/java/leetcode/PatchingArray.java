2ee8d2c5877f51309cad4711e5759e78e2b38c45
ykliu <moneyflying_2006@hotmail.com>
2017-06-26 14:10:01 +0800

    core java ch 8

--- src/main/java/leetcode/PatchingArray.java
+++ src/main/java/leetcode/PatchingArray.java
@@ -8,7 +8,7 @@ package leetcode;
 public class PatchingArray {
     public static void main(String[] args) {
         PatchingArray pa = new PatchingArray();
-        int[] nums1 = new int[]{1,3};
+        int[] nums1 = new int[]{5};
         int[] nums2 = new int[]{1,5,10};
         System.out.println(pa.minPatches(nums1,6));
         System.out.println(pa.minPatches(nums2,20));
