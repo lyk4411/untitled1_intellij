b0f9e6696180c6b359b53de69901b2e040014f68
ykliu <moneyflying_2006@hotmail.com>
2017-06-27 16:14:56 +0800

    core java ch 8

--- src/main/java/leetcode/SplitArrayLargestSum.java
+++ src/main/java/leetcode/SplitArrayLargestSum.java
@@ -12,40 +12,53 @@ public class SplitArrayLargestSum {
         System.out.println(sals.splitArray(nums,2));
     }
 
+    /**
+     *  这个数肯定介于最大的那一个单值和所有元素只和的中间
+     * */
     public int splitArray(int[] nums, int m) {
-        int max = 0; long sum = 0;
-        for (int num : nums) {
-            max = Math.max(num, max);
+        long sum = 0;
+        int max = 0;
+        for(int num: nums){
+            max = Math.max(max, num);
             sum += num;
         }
-        if (m == 1) return (int)sum;
-        //binary search
-        long l = max; long r = sum;
-        while (l <= r) {
-            long mid = (l + r)/ 2;
-            if (valid(mid, nums, m)) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+        return (int)binarySearch(nums, m, sum, max);
+    }
+    //二分查找
+    private long binarySearch(int[] nums, int m, long high, long low){
+        long mid = 0;
+        while(low < high){
+            mid = (high + low)/2;
+            //验证是否满足,也就是这么大的值有可能出现么
+            if(valid(nums, m, mid)){
+                high = mid;
+            }else{
+                low = mid + 1;
             }
         }
-        return (int)l;
+        return high;
     }
-    public boolean valid(long target, int[] nums, int m) {
+
+    /**
+     * 验证这个值是否合法
+     * */
+    private boolean valid(int[] nums, int m, long max){
+        int cur = 0;
         int count = 1;
-        long total = 0;
-        for(int num : nums) {
-            total += num;
-            if (total > target) {
-                total = num;
+        //是否有多余m个片段or区间，大于给定值的max的，如果有了，那么就不合法了，因为这样划分就不止m个，及max太小
+        for(int num: nums){
+            cur += num;
+            if(cur > max){
+                cur = num;
                 count++;
-                if (count > m) {
+                if(count > m){
                     return false;
                 }
             }
         }
         return true;
     }
+
 
 
 //    public int splitArray(int[] nums, int m)
