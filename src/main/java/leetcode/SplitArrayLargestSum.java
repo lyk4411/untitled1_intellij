5c53188302f3631c697d6054215274fe5ffbda40
ykliu <moneyflying_2006@hotmail.com>
2017-06-27 14:32:36 +0800

    core java ch 8

--- src/main/java/leetcode/SplitArrayLargestSum.java
+++ src/main/java/leetcode/SplitArrayLargestSum.java
@@ -12,38 +12,94 @@ public class SplitArrayLargestSum {
         System.out.println(sals.splitArray(nums,2));
     }
 
-    public int splitArray(int[] nums, int m) {
-        int max = 0; long sum = 0;
-        for (int num : nums) {
-            max = Math.max(num, max);
-            sum += num;
+//    public int splitArray(int[] nums, int m) {
+//        int max = 0; long sum = 0;
+//        for (int num : nums) {
+//            max = Math.max(num, max);
+//            sum += num;
+//        }
+//        if (m == 1) return (int)sum;
+//        //binary search
+//        long l = max; long r = sum;
+//        while (l <= r) {
+//            long mid = (l + r)/ 2;
+//            if (valid(mid, nums, m)) {
+//                r = mid - 1;
+//            } else {
+//                l = mid + 1;
+//            }
+//        }
+//        return (int)l;
+//    }
+//    public boolean valid(long target, int[] nums, int m) {
+//        int count = 1;
+//        long total = 0;
+//        for(int num : nums) {
+//            total += num;
+//            if (total > target) {
+//                total = num;
+//                count++;
+//                if (count > m) {
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+//    }
+
+
+    public int splitArray(int[] nums, int m)
+    {
+        int n = nums.length;
+        int[] sums = new int[n + 1];
+        int dp[][] = new int[m + 1][n + 1];
+//        for (int i = 0; i < dp.length; i++) {
+//            for (int j = 0; j < dp[0].length; j++) {
+//                System.out.print("dp[" + i + "][" +j + "]:" + dp[i][j] + "  :");
+//            }
+//            System.out.println();
+//        }
+
+        for (int i = 1; i <= n; i++) {
+            sums[i] = sums[i - 1] + nums[i - 1];
         }
-        if (m == 1) return (int)sum;
-        //binary search
-        long l = max; long r = sum;
-        while (l <= r) {
-            long mid = (l + r)/ 2;
-            if (valid(mid, nums, m)) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
             }
         }
-        return (int)l;
-    }
-    public boolean valid(long target, int[] nums, int m) {
-        int count = 1;
-        long total = 0;
-        for(int num : nums) {
-            total += num;
-            if (total > target) {
-                total = num;
-                count++;
-                if (count > m) {
-                    return false;
+
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = sums[j];
+            dp[1][j] = sums[j];
+        }
+
+//        for (int i = 0; i < dp.length; i++) {
+//            for (int j = 0; j < dp[0].length; j++) {
+//                System.out.print("dp[" + i + "][" +j + "]:" + dp[i][j] + "  :");
+//            }
+//            System.out.println();
+//        }
+
+//        for (int i = 0; i < sums.length; i++) {
+//            System.out.print("sums[" + i + "]" + sums[i] + "  :");        }
+//        System.out.println();
+
+        for (int i = 2; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                for (int k = i - 1; k < j; ++k) {
+                    int val = Math.max(dp[i - 1][k], sums[j] - sums[k]);
+                    dp[i][j] = Math.min(dp[i][j], val);
                 }
             }
         }
-        return true;
+
+//        for (int i = 0; i < dp.length; i++) {
+//            for (int j = 0; j < dp[0].length; j++) {
+//                System.out.print("dp[" + i + "][" +j + "]:" + dp[i][j] + "  :");
+//            }
+//            System.out.println();
+//        }
+        return dp[m][n];
     }
 }
