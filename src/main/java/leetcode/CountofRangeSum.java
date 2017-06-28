d25f0199d9b5596770bf97af2f4147b1b5f0af17
ykliu <moneyflying_2006@hotmail.com>
2017-06-28 16:50:08 +0800

    core java ch 8

--- src/main/java/leetcode/CountofRangeSum.java
+++ src/main/java/leetcode/CountofRangeSum.java
@@ -12,29 +12,53 @@ public class CountofRangeSum {
 
         System.out.println(crs.countRangeSum(nums1,-2,2));
     }
+
     public int countRangeSum(int[] nums, int lower, int upper) {
         int n = nums.length;
-        long[] sums = new long[n + 1];
-        for (int i = 0; i < n; ++i)
-            sums[i + 1] = sums[i] + nums[i];
-        return countWhileMergeSort(sums, 0, n + 1, lower, upper);
+        if(n == 0) return 0;
+        // binary search tree
+        Node root = new Node(0);
+        int res = 0;
+        long prefixSum = 0;
+        for(int i = 0; i < n; i++) {
+            prefixSum += nums[i];
+            res += findNumInBound(root, lower, upper, prefixSum);
+            insert(root, prefixSum);
+        }
+        return res;
     }
 
-    private int countWhileMergeSort(long[] sums, int start, int end, int lower, int upper) {
-        if (end - start <= 1) return 0;
-        int mid = (start + end) / 2;
-        int count = countWhileMergeSort(sums, start, mid, lower, upper)
-                + countWhileMergeSort(sums, mid, end, lower, upper);
-        int j = mid, k = mid, t = mid;
-        long[] cache = new long[end - start];
-        for (int i = start, r = 0; i < mid; ++i, ++r) {
-            while (k < end && sums[k] - sums[i] < lower) k++;
-            while (j < end && sums[j] - sums[i] <= upper) j++;
-            while (t < end && sums[t] < sums[i]) cache[r++] = sums[t++];
-            cache[r] = sums[i];
-            count += j - k;
+    private int findNumInBound(Node node, long low, long up, long sum) {
+        // base case
+        if(node == null) return 0;
+        // range: sum - up <= node.val <= sum - low
+        if(node.val < sum - up) return findNumInBound(node.right, low, up, sum);
+        else if(node.val > sum - low) return findNumInBound(node.left, low, up, sum);
+        else return 1 + findNumInBound(node.left, low, up, sum) + findNumInBound(node.right, low, up, sum);
+    }
+
+    private void insert(Node node, long value) {
+        while(node != null) {
+            if(node.val > value) {
+                if(node.left == null) {
+                    node.left = new Node(value);
+                    break;
+                }
+                node = node.left;
+            }
+            else {
+                if(node.right == null) {
+                    node.right = new Node(value);
+                    break;
+                }
+                node = node.right;
+            }
         }
-        System.arraycopy(cache, 0, sums, start, t - start);
-        return count;
+    }
+    class Node {
+        long val;
+        Node left;
+        Node right;
+        Node(long val) { this.val = val; }
     }
 }
