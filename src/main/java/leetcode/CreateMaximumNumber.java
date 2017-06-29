a032ee64b49fb7276da91f6aa1a0e3a77b406ce6
ykliu <moneyflying_2006@hotmail.com>
2017-06-29 15:16:55 +0800

    core java ch 8

--- src/main/java/leetcode/CreateMaximumNumber.java
+++ src/main/java/leetcode/CreateMaximumNumber.java
@@ -1,8 +1,5 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by lyk on 2017/6/28.
  * Package name: leetcode
@@ -51,32 +48,32 @@ public class CreateMaximumNumber {
     public int[] maxArray(int[] nums, int k) {
         int n = nums.length;
         int[] ans = new int[k];
-        List<Integer> temp = new ArrayList<Integer>();
-        for (int i = 0; i < k && i < nums.length; i++) {
-            temp.add(nums[i]);
-        }
+//        List<Integer> temp = new ArrayList<Integer>();
+//        for (int i = 0; i < k && i < nums.length; i++) {
+//            temp.add(nums[i]);
+//        }
         //System.out.println("temp: " + temp);
 
-        if(nums.length > k) {
-            for (int i = k; i < nums.length; i++){
-                for (int j = 0; j < k; j++) {
-                    if(nums[i] > temp.get(j)){
-                        temp.remove(j);
-                        //System.out.println("temp: " + temp + "   j:" + j);
-                        temp.add(nums[i]);
-                        //System.out.println("temp: " + temp + "   num[i]:" + nums[i]);
-                        break;
-                    }
-                }
-            }
-        }
-        for(int i = 0;i<temp.size();i++){
-            ans[i] = temp.get(i);
-        }
-//        for (int i = 0, j = 0; i < n; ++i) {
-//            while (n - i + j > k && j > 0 && ans[j - 1] < nums[i]) j--;
-//            if (j < k) ans[j++] = nums[i];
+//        if(nums.length > k) {
+//            for (int i = k; i < nums.length; i++){
+//                for (int j = 0; j < k; j++) {
+//                    if(nums[i] > temp.get(j)){
+//                        temp.remove(j);
+//                        //System.out.println("temp: " + temp + "   j:" + j);
+//                        temp.add(nums[i]);
+//                        //System.out.println("temp: " + temp + "   num[i]:" + nums[i]);
+//                        break;
+//                    }
+//                }
+//            }
 //        }
+//        for(int i = 0;i<temp.size();i++){
+//            ans[i] = temp.get(i);
+//        }
+        for (int i = 0, j = 0; i < n; ++i) {
+            while (n - i + j > k && j > 0 && ans[j - 1] < nums[i]) j--;
+            if (j < k) ans[j++] = nums[i];
+        }
         return ans;
     }
 }
