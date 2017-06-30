a59438f3f6b03378115c9201b31e9ea781a5816f
ykliu <moneyflying_2006@hotmail.com>
2017-06-30 16:17:34 +0800

    core java ch 8

---
+++ src/main/java/leetcode/RegularExpressionMatching.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+/**
+ * Created by lyk on 2017/6/30.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class RegularExpressionMatching {
+    public static void main(String[] args) {
+        RegularExpressionMatching rem = new RegularExpressionMatching();
+        System.out.println(rem.isMatch("aa","aaa"));
+        System.out.println(rem.isMatch("aa","aa"));
+        System.out.println(rem.isMatch("aa","ca*b"));
+        System.out.println(rem.isMatch("aa","ca."));
+        System.out.println(rem.isMatch("aa",".."));
+        System.out.println(rem.isMatch("aa",".*"));
+
+    }
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i-1]) {
+                dp[0][i+1] = true;
+            }
+        }
+        for (int i = 0 ; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.') {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == s.charAt(i)) {
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
+                        dp[i+1][j+1] = dp[i+1][j-1];
+                    } else {
+                        dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}
