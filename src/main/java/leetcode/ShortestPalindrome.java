558cb3eae00bb4de15081ee6270db93cc865f84a
ykliu <moneyflying_2006@hotmail.com>
2017-07-03 17:46:43 +0800

    core java ch 8

--- src/main/java/leetcode/ShortestPalindrome.java
+++ src/main/java/leetcode/ShortestPalindrome.java
@@ -13,26 +13,50 @@ public class ShortestPalindrome {
         System.out.println(sp.shortestPalindrome("aabaab"));
     }
     public String shortestPalindrome(String s) {
-        StringBuilder rev = new StringBuilder(s).reverse();
-        String d = s + "#" + rev.toString();
-        int n = d.length();
-        int[] prefix = new int[n];
-        // i for rev, j for s
-        int i = 1, j = 0;
-        while(i < n) {
-            // match
-            if(d.charAt(j) == d.charAt(i)) {
-                prefix[i] = j + 1;
-                i++;  j++;
+        String temp = s + "#" + new StringBuilder(s).reverse().toString();
+        int[] table = getTable(temp);
+
+        //get the maximum palin part in s starts from 0
+        return new StringBuilder(s.substring(table[table.length - 1])).reverse().toString() + s;
+    }
+
+    public int[] getTable(String s){
+        //get lookup table
+        int[] table = new int[s.length()];
+
+        //pointer that points to matched char in prefix part
+
+        int index = 0;
+        //skip index 0, we will not match a string with itself
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(index) == s.charAt(i)){
+                //we can extend match in prefix and postfix
+                table[i] = table[i-1] + 1;
+                index ++;
+            }else{
+                //match failed, we try to match a shorter substring
+
+                //by assigning index to table[i-1], we will shorten the match string length, and jump to the
+                //prefix part that we used to match postfix ended at i - 1
+                index = table[i-1];
+
+                while(index > 0 && s.charAt(index) != s.charAt(i)){
+                    //we will try to shorten the match string length until we revert to the beginning of match (index 1)
+                    index = table[index-1];
+                }
+
+                //when we are here may either found a match char or we reach the boundary and still no luck
+                //so we need check char match
+                if(s.charAt(index) == s.charAt(i)){
+                    //if match, then extend one char
+                    index ++ ;
+                }
+
+                table[i] = index;
             }
-            else {
-                if(j == 0) i++;
-                else j = prefix[j-1];
-            }
+
         }
 
-        StringBuilder sb = new StringBuilder();
-        sb.append(s.substring(prefix[n-1]));
-        return sb.reverse().append(s).toString();
+        return table;
     }
 }
