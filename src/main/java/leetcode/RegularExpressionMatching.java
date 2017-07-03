fd78a89ce5da595211321b336ab373774efe691a
ykliu <moneyflying_2006@hotmail.com>
2017-07-03 10:48:15 +0800

    core java ch 8

--- src/main/java/leetcode/RegularExpressionMatching.java
+++ src/main/java/leetcode/RegularExpressionMatching.java
@@ -24,7 +24,7 @@ public class RegularExpressionMatching {
         }
         boolean[][] dp = new boolean[s.length()+1][p.length()+1];
         dp[0][0] = true;
-        for (int i = 0; i < p.length(); i++) {
+        for (int i = 1; i < p.length(); i++) {
             if (p.charAt(i) == '*' && dp[0][i-1]) {
                 dp[0][i+1] = true;
             }
