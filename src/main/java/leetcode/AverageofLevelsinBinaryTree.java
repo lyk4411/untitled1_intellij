887dfad5de6a6841626010d4e02dd2eebd78e029
ykliu <moneyflying_2006@hotmail.com>
2017-07-11 16:34:55 +0800

    core java ch 8

---
+++ src/main/java/leetcode/AverageofLevelsinBinaryTree.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by lyk on 2017/7/11.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class AverageofLevelsinBinaryTree {
+    public static void main(String[] args) {
+        AverageofLevelsinBinaryTree albt = new AverageofLevelsinBinaryTree();
+        TreeNode a1 = new TreeNode(1);
+        TreeNode a2 = new TreeNode(1);
+        TreeNode a3 = new TreeNode(2);
+        TreeNode a4 = new TreeNode(2);
+        TreeNode a5 = new TreeNode(3);
+        TreeNode a6 = new TreeNode(3);
+        TreeNode a7 = new TreeNode(4);
+        TreeNode a8 = new TreeNode(4);
+        TreeNode a9 = new TreeNode(5);
+
+        a1.left = a2;
+        a1.right = a3;
+        a3.left = a4;
+        a3.right = a5;
+        a5.left = a6;
+        a5.right = a7;
+        a7.left = a8;
+        a7.right = a9;
+        System.out.println(albt.averageOfLevels(a1));
+    }
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+
+        if(root == null) return result;
+        q.add(root);
+        while(!q.isEmpty()) {
+            int n = q.size();
+            double sum = 0.0;
+            for(int i = 0; i < n; i++) {
+                TreeNode node = q.poll();
+                sum += node.val;
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+            }
+            result.add(sum / n);
+        }
+        return result;
+    }
+}
