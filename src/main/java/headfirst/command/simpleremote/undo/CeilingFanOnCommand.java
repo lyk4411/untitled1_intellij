9f5be6604255c9d4892936ff6875b7ddaefd754b
ykliu <moneyflying_2006@hotmail.com>
2017-09-14 11:25:12 +0800

    core java ch 8

---
+++ src/main/java/headfirst/command/simpleremote/undo/CeilingFanOnCommand.java
@@ -0,0 +1,15 @@
+package headfirst.command.simpleremote.undo;
+
+public class CeilingFanOnCommand implements Command {
+	CeilingFan ceilingFan;
+
+	public CeilingFanOnCommand(CeilingFan ceilingFan) {
+		this.ceilingFan = ceilingFan;
+	}
+	public void execute() {
+		ceilingFan.high();
+	}
+	public void undo() {
+		ceilingFan.off();
+	}
+}
