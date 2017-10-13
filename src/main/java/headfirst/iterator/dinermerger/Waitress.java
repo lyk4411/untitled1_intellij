e036c65cb6fe711d5a6200bcdeba25aef245d0ec
ykliu <moneyflying_2006@hotmail.com>
2017-10-13 16:52:33 +0800

    core java ch 8

--- src/main/java/headfirst/iterator/dinermerger/Waitress.java
+++ src/main/java/headfirst/iterator/dinermerger/Waitress.java
@@ -28,44 +28,5 @@ public class Waitress {
 		}
 	}
  
-	public void printVegetarianMenu() {
-		printVegetarianMenu(pancakeHouseMenu.createIterator());
-		printVegetarianMenu(dinerMenu.createIterator());
-	}
- 
-	public boolean isItemVegetarian(String name) {
-		Iterator breakfastIterator = pancakeHouseMenu.createIterator();
-		if (isVegetarian(name, breakfastIterator)) {
-			return true;
-		}
-		Iterator dinnerIterator = dinerMenu.createIterator();
-		if (isVegetarian(name, dinnerIterator)) {
-			return true;
-		}
-		return false;
-	}
 
-
-	private void printVegetarianMenu(Iterator iterator) {
-		while (iterator.hasNext()) {
-			MenuItem menuItem = (MenuItem)iterator.next();
-			if (menuItem.isVegetarian()) {
-				System.out.print(menuItem.getName());
-				System.out.println("\t\t" + menuItem.getPrice());
-				System.out.println("\t" + menuItem.getDescription());
-			}
-		}
-	}
-
-	private boolean isVegetarian(String name, Iterator iterator) {
-		while (iterator.hasNext()) {
-			MenuItem menuItem = (MenuItem)iterator.next();
-			if (menuItem.getName().equals(name)) {
-				if (menuItem.isVegetarian()) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
 }
