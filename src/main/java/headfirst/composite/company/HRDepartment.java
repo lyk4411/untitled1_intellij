248d69e7758d03680320c66199e499edae410d1d
ykliu <moneyflying_2006@hotmail.com>
2017-11-14 13:56:53 +0800

    core java ch 8

--- src/main/java/headfirst/composite/company/HRDepartment.java
+++ src/main/java/headfirst/composite/company/HRDepartment.java
@@ -6,7 +6,6 @@ package headfirst.composite.company;
  * Porject name: untitled1
  */
 //这两个类，其实也是公司的子类，然而，其没有存放公司类的数组，故，注定是一个叶子节点，下面不得有子节点
-//也就是断子绝孙的结点
 class HRDepartment extends Company {
     public HRDepartment(String name) {
         super(name);
