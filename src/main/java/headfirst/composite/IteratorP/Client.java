217da70c9ffa02cddbfd1c9455f900628362aa49
ykliu <moneyflying_2006@hotmail.com>
2017-11-20 15:47:35 +0800

    core java ch 8

--- src/main/java/headfirst/composite/IteratorP/Client.java
+++ src/main/java/headfirst/composite/IteratorP/Client.java
@@ -5,5 +5,18 @@ package headfirst.composite.IteratorP;
  * Package name: headfirst.composite.IteratorP
  * Porject name: untitled1
  */
+
+
 public class Client {
+    public static void main(String[] args){
+        Aggregate ag = new ConcreteAggregate();
+        ag.add("小明");
+        ag.add("小红");
+        ag.add("小刚");
+        Iterator it = ag.iterator();
+        while(it.hasNext()){
+            String str = (String)it.next();
+            System.out.println(str);
+        }
+    }
 }
