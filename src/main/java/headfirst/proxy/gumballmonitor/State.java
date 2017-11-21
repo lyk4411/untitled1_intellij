0e611fa21b64a3bf0a817cb753c31c3e3c231d2d
ykliu <moneyflying_2006@hotmail.com>
2017-11-21 18:08:31 +0800

    core java ch 8

---
+++ src/main/java/headfirst/proxy/gumballmonitor/State.java
@@ -0,0 +1,11 @@
+package headfirst.proxy.gumballmonitor;
+
+import java.io.Serializable;
+
+public interface State extends Serializable {
+ 
+	 void insertQuarter();
+	 void ejectQuarter();
+	 void turnCrank();
+	 void dispense();
+}
