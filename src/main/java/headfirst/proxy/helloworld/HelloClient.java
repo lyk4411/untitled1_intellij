c78c989bc3135d5560fa2d320b2a2ba181802bbe
ykliu <moneyflying_2006@hotmail.com>
2017-11-22 18:18:24 +0800

    core java ch 8

---
+++ src/main/java/headfirst/proxy/helloworld/HelloClient.java
@@ -0,0 +1,31 @@
+package headfirst.proxy.helloworld;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+/**
+ * Created by lyk on 2017-11-22.
+ * Package name: headfirst.proxy.helloworld
+ * Porject name: untitled1
+ */
+
+
+
+public class HelloClient {
+    public static void main(String args[]){
+        try {
+            //在RMI服务注册表中查找名称为RHello的对象，并调用其上的方法
+            IHello rhello =(IHello) Naming.lookup("rmi://localhost:8888/RHello");
+            System.out.println(rhello.helloWorld());
+            System.out.println(rhello.sayHelloToSomeBody("熔岩"));
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+}
