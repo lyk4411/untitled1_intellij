5ec56b6d10a1e3ba1010b54b94a8c34f761807d5
ykliu <moneyflying_2006@hotmail.com>
2018-01-08 15:11:38 +0800

    core java ch 8

---
+++ src/main/java/JUnitLearn/ch02/Largest.java
@@ -0,0 +1,18 @@
+package JUnitLearn.ch02;
+
+/**
+ * Created by lyk on 2018-1-8.
+ * Package name: JUnitLearn.ch02
+ * Porject name: untitled1
+ */
+public class Largest {
+    public  static int largest(int[] list){
+        int index,max=Integer.MIN_VALUE;
+        for(index = 0; index <= list.length - 1; index++){
+            if (list[index] > max){
+                max = list[index];
+            }
+        }
+        return max;
+    }
+}
---
+++ src/main/java/JUnitLearn/ch02/TestLargest.java
@@ -0,0 +1,21 @@
+package JUnitLearn.ch02;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by lyk on 2018-1-8.
+ * Package name: JUnitLearn.ch02
+ * Porject name: untitled1
+ */
+public class TestLargest extends TestCase {
+    public TestLargest(String name){
+        super(name);
+    }
+    public void testSimple(){
+        assertEquals(9,Largest.largest(new int[]{7,8,9}));
+        assertEquals(9,Largest.largest(new int[]{9,8,7}));
+        assertEquals(9,Largest.largest(new int[]{7,9,8}));
+        assertEquals(-7,Largest.largest(new int[]{-7,-8,-9}));
+
+    }
+}
