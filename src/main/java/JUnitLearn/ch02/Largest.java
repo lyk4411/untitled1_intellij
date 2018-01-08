75e092a775d4f09b84f3d6779982c791afc26929
ykliu <moneyflying_2006@hotmail.com>
2018-01-08 15:22:13 +0800

    core java ch 8

--- src/main/java/JUnitLearn/ch02/Largest.java
+++ src/main/java/JUnitLearn/ch02/Largest.java
@@ -8,6 +8,9 @@ package JUnitLearn.ch02;
 public class Largest {
     public  static int largest(int[] list){
         int index,max=Integer.MIN_VALUE;
+        if(list.length == 0){
+            throw new RuntimeException("Empty list.");
+        }
         for(index = 0; index <= list.length - 1; index++){
             if (list[index] > max){
                 max = list[index];
