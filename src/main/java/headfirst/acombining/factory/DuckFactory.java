df908c9d58e05f77128b923a5fa95c0a64b9dffd
ykliu <moneyflying_2006@hotmail.com>
2018-01-18 15:48:43 +0800

    core java ch 8

---
+++ src/main/java/headfirst/acombining/factory/DuckFactory.java
@@ -0,0 +1,23 @@
+package headfirst.acombining.factory;
+
+import headfirst.acombining.*;
+
+
+public class DuckFactory extends AbstractDuckFactory {
+  
+	public Quackable createMallardDuck() {
+		return new MallardDuck();
+	}
+  
+	public Quackable createRedheadDuck() {
+		return new RedheadDuck();
+	}
+  
+	public Quackable createDuckCall() {
+		return new DuckCall();
+	}
+   
+	public Quackable createRubberDuck() {
+		return new RubberDuck();
+	}
+}
