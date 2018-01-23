bb6d07fdd40c2784a81912df740ca51c914b8ba9
ykliu <moneyflying_2006@hotmail.com>
2018-01-23 14:58:01 +0800

    core java ch 8

--- src/main/java/headfirst/flyweight/Circle.java
+++ src/main/java/headfirst/flyweight/Circle.java
@@ -31,6 +31,6 @@ public class Circle implements Shape {
     @Override
     public void draw() {
         System.out.println("Circle: Draw() [Color : " + color
-                +", x : " + x +", y :" + y +", radius :" + radius);
+                +", x : " + x +", y :" + y +", radius :" + radius + "]");
     }
 }
