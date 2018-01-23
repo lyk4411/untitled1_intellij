8d6c276aabb4bf84b00f76cdb0667acc6c612c18
ykliu <moneyflying_2006@hotmail.com>
2018-01-23 14:24:38 +0800

    core java ch 8

---
+++ src/main/java/headfirst/handler1/Handler.java
@@ -0,0 +1,32 @@
+package headfirst.handler1;
+
+/**
+ * Created by lyk on 2018-1-23.
+ * Package name: headfirst.handler1
+ * Porject name: untitled1
+ */
+public abstract class Handler {
+    /**
+     * 持有下一个处理请求的对象
+     */
+    protected Handler successor = null;
+    /**
+     * 取值方法
+     */
+    public Handler getSuccessor() {
+        return successor;
+    }
+    /**
+     * 设置下一个处理请求的对象
+     */
+    public void setSuccessor(Handler successor) {
+        this.successor = successor;
+    }
+    /**
+     * 处理聚餐费用的申请
+     * @param user    申请人
+     * @param fee    申请的钱数
+     * @return        成功或失败的具体通知
+     */
+    public abstract String handleFeeRequest(String user , double fee);
+}
