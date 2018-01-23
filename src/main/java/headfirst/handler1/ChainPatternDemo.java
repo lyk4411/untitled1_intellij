c2bf85be80f98e227442953accb35d8e4ce64c90
ykliu <moneyflying_2006@hotmail.com>
2018-01-23 14:46:44 +0800

    core java ch 8

--- src/main/java/headfirst/handler1/ChainPatternDemo.java
+++ src/main/java/headfirst/handler1/ChainPatternDemo.java
@@ -24,11 +24,17 @@ public class ChainPatternDemo {
 
         loggerChain.logMessage(AbstractLogger.INFO,
                 "This is an information.");
-
+        System.out.println("=======================================");
         loggerChain.logMessage(AbstractLogger.DEBUG,
                 "This is an debug level information.");
+        System.out.println("=======================================");
 
         loggerChain.logMessage(AbstractLogger.ERROR,
                 "This is an error information.");
+        System.out.println("=======================================");
+        loggerChain.logMessage(AbstractLogger.DEBUG,
+                "This is an error information.");
+        System.out.println("=======================================");
+
     }
 }
