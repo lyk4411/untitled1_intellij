b9860c9b001ff0e4069c821d1e92d9b4e03371df
ykliu <moneyflying_2006@hotmail.com>
2018-02-08 16:55:41 +0800

    core java ch 8

--- src/main/java/leetcode/StringCompression.java
+++ src/main/java/leetcode/StringCompression.java
@@ -10,6 +10,7 @@ public class StringCompression {
         char[] c1 = new char[]{'a','a','b','b','c','c','c'};
         char[] c2 = new char[]{'a'};
         char[] c3 = new char[]{'a','b','b','b','b','b','b','b','b','b','b','b','b'};
+        char[] c4 = new char[]{'a','b','c'};
         StringCompression sc = new StringCompression();
         System.out.println(c1);
         System.out.println(sc.compress(c1));
@@ -22,20 +23,42 @@ public class StringCompression {
         System.out.println(c3);
         System.out.println(sc.compress(c3));
         System.out.println(c3);
+        System.out.println("============================================");
+        System.out.println(c4);
+        System.out.println(sc.compress(c4));
+        System.out.println(c4);
     }
     public int compress(char[] chars) {
-        int anchor = 0, write = 0;
-        for (int read = 0; read < chars.length; read++) {
-            if (read + 1 == chars.length || chars[read + 1] != chars[read]) {
-                chars[write++] = chars[anchor];
-                if (read > anchor) {
-                    for (char c: ("" + (read - anchor + 1)).toCharArray()) {
-                        chars[write++] = c;
+        int len = chars.length;
+        if (len < 2) {
+            return len;
+        }
+        int res = 0;
+        char c = chars[0];
+        int num = 1;
+        for (int i = 1; i < len; i++) {
+            if (chars[i] == chars[i - 1]) {
+                num++;
+            }
+            if (chars[i] != chars[i - 1]) {
+                chars[res++] = c;
+                if (num > 1) {
+                    for (char cc : ("" + num).toCharArray()) {
+                        chars[res++] = cc;
                     }
                 }
-                anchor = read + 1;
+                num = 1;
+                c = chars[i];
+            }
+            if(i == len - 1){
+                chars[res++] = c;
+                if (num > 1) {
+                    for (char cc : ("" + num).toCharArray()) {
+                        chars[res++] = cc;
+                    }
+                }
             }
         }
-        return write;
+        return res;
     }
 }
