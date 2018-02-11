d420016b31a516946fb1724379f751265862678e
ykliu <moneyflying_2006@hotmail.com>
2018-02-11 09:52:28 +0800

    core java ch 8

--- src/main/java/leetcode/LongestContinuousIncreasingSubsequence.java
+++ src/main/java/leetcode/LongestContinuousIncreasingSubsequence.java
@@ -18,7 +18,9 @@ public class LongestContinuousIncreasingSubsequence {
     public int findLengthOfLCIS(int[] nums) {
         int ans = 0, anchor = 0;
         for (int i = 0; i < nums.length; ++i) {
-            if (i > 0 && nums[i-1] >= nums[i]) anchor = i;
+            if (i > 0 && nums[i-1] >= nums[i]) {
+                anchor = i;
+            }
             ans = Math.max(ans, i - anchor + 1);
         }
         return ans;
