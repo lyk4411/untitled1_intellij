bc82ddc404821b382056a9e5152f4441be60b167
ykliu <moneyflying_2006@hotmail.com>
2018-04-16 18:30:39 +0800

    core java ch 8

--- src/main/java/leetcode/SetMismatch.java
+++ src/main/java/leetcode/SetMismatch.java
@@ -8,6 +8,20 @@ import java.util.stream.IntStream;
  * Porject name: untitled1
  */
 public class SetMismatch {
+
+
+//    public int[]  findErrorNums(int[] nums){
+//        Set<Integer> set = new HashSet<>();
+//        int duplicate = 0,n = nums.length;
+//        long sum = (n * (n+1)) / 2;
+//        for(int i : nums) {
+//            if(set.contains(i))
+//                duplicate = i;
+//            sum -= i;
+//            set.add(i);
+//        } return new int[]{duplicate,(int)sum + duplicate};
+//    }
+
     public int[] findErrorNums(int[] nums) {
         int[] res = new int[2];
         for (int i : nums) {
