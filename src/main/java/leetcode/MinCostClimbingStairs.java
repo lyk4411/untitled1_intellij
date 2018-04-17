cc60c30691ad091209775d91022c4e59b4e1aa92
ykliu <moneyflying_2006@hotmail.com>
2018-04-18 07:58:00 +0800

    core java ch 8

--- src/main/java/leetcode/MinCostClimbingStairs.java
+++ src/main/java/leetcode/MinCostClimbingStairs.java
@@ -7,13 +7,14 @@ package leetcode;
  */
 public class MinCostClimbingStairs {
     public int minCostClimbingStairs(int[] cost) {
-        int f1 = 0, f2 = 0;
-        for (int i = cost.length - 1; i >= 0; --i) {
-            int f0 = cost[i] + Math.min(f1, f2);
-            f2 = f1;
-            f1 = f0;
+        int length = cost.length;
+        int[] dp = new int[length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i < length + 1; i++) {
+            dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
         }
-        return Math.min(f1, f2);
+        return dp[length];
     }
 
     public static void main(String[] args) {
