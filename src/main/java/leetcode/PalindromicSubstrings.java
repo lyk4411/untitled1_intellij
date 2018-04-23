2d9189f8c8e4779c0c0333734bfbf98b53cf4445
ykliu <moneyflying_2006@hotmail.com>
2018-04-23 18:31:56 +0800

    core java ch 8

--- src/main/java/leetcode/PalindromicSubstrings.java
+++ src/main/java/leetcode/PalindromicSubstrings.java
@@ -5,6 +5,13 @@ package leetcode;
  * Package name: leetcode
  * Porject name: untitled1
  */
+//      其实这道题也可以用递归来做，而且思路非常的简单粗暴。就是以字符串中的每一个
+//      字符都当作回文串中间的位置，然后向两边扩散，每当成功匹配两个左右两个字符，
+//      结果res自增1，然后再比较下一对。注意回文字符串有奇数和偶数两种形式，如果是
+//      奇数长度，那么i位置就是中间那个字符的位置，所以我们左右两遍都从i开始遍历；
+//      如果是偶数长度的，那么i是最中间两个字符的左边那个，右边那个就是i+1，这样就
+//      能cover所有的情况啦，而且都是不同的回文子字符串，参见代码如下：
+
 public class PalindromicSubstrings {
     public int countSubstrings(String S) {
         int N = S.length(), ans = 0;
