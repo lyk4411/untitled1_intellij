36082577079a96b2f56c1a71b7607fcff5407079
ykliu <moneyflying_2006@hotmail.com>
2018-04-24 16:52:47 +0800

    core java ch 8

---
+++ src/main/java/example/test112.java
@@ -0,0 +1,21 @@
+package example;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by lyk on 2018-4-24.
+ * Package name: example
+ * Porject name: untitled1
+ */
+public class test112 {
+    public static void main(String[] s) {
+        Map<String, Boolean> whoLetDogsOut = new ConcurrentHashMap<>();
+        whoLetDogsOut.computeIfAbsent("snoop", k -> f(k));
+        whoLetDogsOut.computeIfAbsent("snoop", k -> f(k));
+    }
+    static boolean f(String s) {
+        System.out.println("creating a value for \""+s+'"');
+        return s.isEmpty();
+    }
+}
