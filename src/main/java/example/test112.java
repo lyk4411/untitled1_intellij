f59d7bc695201f2c37e91fd0ccc5bd248f808463
ykliu <moneyflying_2006@hotmail.com>
2018-04-24 18:57:13 +0800

    core java ch 8

--- src/main/java/example/test112.java
+++ src/main/java/example/test112.java
@@ -13,6 +13,7 @@ public class test112 {
         Map<String, Boolean> whoLetDogsOut = new ConcurrentHashMap<>();
         whoLetDogsOut.computeIfAbsent("snoop", k -> f(k));
         whoLetDogsOut.computeIfAbsent("snoop", k -> f(k));
+        System.out.println(1<<2);
     }
     static boolean f(String s) {
         System.out.println("creating a value for \""+s+'"');
