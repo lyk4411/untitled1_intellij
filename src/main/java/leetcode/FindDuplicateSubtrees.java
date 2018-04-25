183be146d1d50e4e34d644a1dce75edbe46b23bb
ykliu <moneyflying_2006@hotmail.com>
2018-04-25 18:24:21 +0800

    core java ch 8

--- src/main/java/leetcode/FindDuplicateSubtrees.java
+++ src/main/java/leetcode/FindDuplicateSubtrees.java
@@ -11,48 +11,53 @@ import java.util.Map;
  * Porject name: untitled1
  */
 public class FindDuplicateSubtrees {
-//    int t;
-//    Map<String, Integer> trees;
+    int t;
+    Map<String, Integer> trees;
+    Map<Integer, Integer> count;
+    List<TreeNode> ans;
+//    int i = 0;
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        t = 1;
+        trees = new HashMap();
+        count = new HashMap();
+        ans = new ArrayList();
+        lookup(root);
+        return ans;
+    }
+
+    public int lookup(TreeNode node) {
+//        System.out.println(i++);
+        if (node == null) return 0;
+        String serial = node.val + "," + lookup(node.left) + "," + lookup(node.right);
+        int uid = trees.computeIfAbsent(serial, x-> t++);
+        count.put(uid, count.getOrDefault(uid, 0) + 1);
+        if (count.get(uid) == 2)
+            ans.add(node);
+        return uid;
+    }
+
+
 //    Map<String, Integer> count;
 //    List<TreeNode> ans;
+//    int i = 0;
 //
 //    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-//        t = 1;
-//        trees = new HashMap();
 //        count = new HashMap();
 //        ans = new ArrayList();
-//        lookup(root);
+//        collect(root);
 //        return ans;
 //    }
 //
-//    public String lookup(TreeNode node) {
-//        if (node == null) return "";
-//        String serial = node.val + "," + lookup(node.left) + "," + lookup(node.right);
-//        trees.computeIfAbsent(serial, x-> t++);
+//    public String collect(TreeNode node) {
+//        System.out.println(i++);
+//        if (node == null) return "#";
+//        String serial = node.val + "," + collect(node.left) + "," + collect(node.right);
 //        count.put(serial, count.getOrDefault(serial, 0) + 1);
 //        if (count.get(serial) == 2)
 //            ans.add(node);
 //        return serial;
 //    }
-
-
-    Map<String, Integer> count;
-    List<TreeNode> ans;
-    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        count = new HashMap();
-        ans = new ArrayList();
-        collect(root);
-        return ans;
-    }
-
-    public String collect(TreeNode node) {
-        if (node == null) return "#";
-        String serial = node.val + "," + collect(node.left) + "," + collect(node.right);
-        count.put(serial, count.getOrDefault(serial, 0) + 1);
-        if (count.get(serial) == 2)
-            ans.add(node);
-        return serial;
-    }
 
     public static void main(String[] args) {
         FindDuplicateSubtrees fds = new FindDuplicateSubtrees();
