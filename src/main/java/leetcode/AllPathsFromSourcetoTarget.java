519907856fd8707bdb5c4c1a87426df3ffcfc80d
ykliu <moneyflying_2006@hotmail.com>
2018-06-07 08:04:45 +0800

    core java ch 8

---
+++ src/main/java/leetcode/AllPathsFromSourcetoTarget.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lyk on 2018-6-7.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class AllPathsFromSourcetoTarget {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        return solve(graph, 0);
+    }
+
+    public List<List<Integer>> solve(int[][] graph, int node) {
+        int N = graph.length;
+        List<List<Integer>> ans = new ArrayList();
+        if (node == N - 1) {
+            List<Integer> path = new ArrayList();
+            path.add(N-1);
+            ans.add(path);
+            return ans;
+        }
+
+        for (int nei: graph[node]) {
+            for (List<Integer> path: solve(graph, nei)) {
+                path.add(0, node);
+                ans.add(path);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        AllPathsFromSourcetoTarget apfst = new AllPathsFromSourcetoTarget();
+        int[][] g1 = new int[][]{
+                {1,2}, {3}, {3}, {}
+        };
+        System.out.println(apfst.allPathsSourceTarget(g1));
+    }
+}
