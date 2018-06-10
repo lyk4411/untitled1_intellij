2f2c4350755bc0862d4cb95956efa1ba4257098c
ykliu <moneyflying_2006@hotmail.com>
2018-06-11 07:56:34 +0800

    core java ch 8

--- src/main/java/leetcode/SplitLinkedListinParts.java
+++ src/main/java/leetcode/SplitLinkedListinParts.java
@@ -42,5 +42,28 @@ public class SplitLinkedListinParts {
         for(ListNode t : lns){
             System.out.println(t + " #");
         }
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        ListNode l7 = new ListNode(7);
+        ListNode l8 = new ListNode(8);
+        ListNode l9 = new ListNode(9);
+        ListNode l10 = new ListNode(10);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = l6;
+        l6.next = l7;
+        l7.next = l8;
+        l8.next = l9;
+        l9.next = l10;
+        ListNode[] lns2 = sllp.splitListToParts(l1,3);
+        for(ListNode t : lns2){
+            System.out.println(t + " #");
+        }
     }
 }
