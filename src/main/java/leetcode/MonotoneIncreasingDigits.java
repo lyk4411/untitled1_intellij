cc417eb4ad225ca9506036dc81142187db68ec52
ykliu <moneyflying_2006@hotmail.com>
2018-06-12 08:09:41 +0800

    core java ch 8

--- src/main/java/leetcode/MonotoneIncreasingDigits.java
+++ src/main/java/leetcode/MonotoneIncreasingDigits.java
@@ -9,10 +9,15 @@ public class MonotoneIncreasingDigits {
     public int monotoneIncreasingDigits(int N) {
         char[] S = String.valueOf(N).toCharArray();
         int i = 1;
-        while (i < S.length && S[i-1] <= S[i]) i++;
-        while (0 < i && i < S.length && S[i-1] > S[i]) S[--i]--;
-        for (int j = i+1; j < S.length; ++j) S[j] = '9';
-
+        while (i < S.length && S[i - 1] <= S[i]){
+            i++;
+        }
+        while (0 < i && i < S.length && S[i - 1] > S[i]) {
+            S[--i]--;
+        }
+        for(int j = i+1; j < S.length; ++j){
+            S[j] = '9';
+        }
         return Integer.parseInt(String.valueOf(S));
     }
 
@@ -20,5 +25,8 @@ public class MonotoneIncreasingDigits {
         MonotoneIncreasingDigits mid = new MonotoneIncreasingDigits();
         System.out.println(mid.monotoneIncreasingDigits(1234));
         System.out.println(mid.monotoneIncreasingDigits(4321));
+        System.out.println(mid.monotoneIncreasingDigits(9321));
+        System.out.println(mid.monotoneIncreasingDigits(120));
+        System.out.println(mid.monotoneIncreasingDigits(332));
     }
 }
