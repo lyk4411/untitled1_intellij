589cd2accd44b5271563621b68343e431c2bd9a5
ykliu <moneyflying_2006@hotmail.com>
2018-06-12 16:21:56 +0800

    core java ch 8

--- src/main/java/leetcode/ParseLispExpression.java
+++ src/main/java/leetcode/ParseLispExpression.java
@@ -73,12 +73,12 @@ public class ParseLispExpression {
     public static void main(String[] args) {
         ParseLispExpression ple = new ParseLispExpression();
         System.out.println(ple.evaluate("(add 1 2)"));
-        System.out.println(ple.evaluate("(mult 3 (add 2 3))"));
-        System.out.println(ple.evaluate("(let x 2 (mult x 5))"));
-        System.out.println(ple.evaluate("(let x 2 (mult x (let x 3 y 4 (add x y))))"));
-        System.out.println(ple.evaluate("(let x 3 x 2 x)"));
-        System.out.println(ple.evaluate("(let x 1 y 2 x (add x y) (add x y))"));
-        System.out.println(ple.evaluate("(let x 2 (add (let x 3 (let x 4 x)) x))"));
-        System.out.println(ple.evaluate("(let a1 3 b2 (add a1 1) b2)"));
+//        System.out.println(ple.evaluate("(mult 3 (add 2 3))"));
+//        System.out.println(ple.evaluate("(let x 2 (mult x 5))"));
+//        System.out.println(ple.evaluate("(let x 2 (mult x (let x 3 y 4 (add x y))))"));
+//        System.out.println(ple.evaluate("(let x 3 x 2 x)"));
+//        System.out.println(ple.evaluate("(let x 1 y 2 x (add x y) (add x y))"));
+//        System.out.println(ple.evaluate("(let x 2 (add (let x 3 (let x 4 x)) x))"));
+//        System.out.println(ple.evaluate("(let a1 3 b2 (add a1 1) b2)"));
     }
 }
