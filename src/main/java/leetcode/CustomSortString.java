f063a8fd9518d4e421e26f377d408e447769520d
ykliu <moneyflying_2006@hotmail.com>
2018-06-20 08:58:51 +0800

    core java ch 8

--- src/main/java/leetcode/CustomSortString.java
+++ src/main/java/leetcode/CustomSortString.java
@@ -1,7 +1,6 @@
 package leetcode;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 /**
  * Created by lyk on 2018-6-20.
@@ -14,12 +13,14 @@ public class CustomSortString {
         for (int i = 0; i < T.length(); ++i) {
             cs[i] = T.charAt(i);
         }
-        Arrays.sort(cs, new Comparator<Character>() {
-            @Override
-            public int compare(Character o1, Character o2) {
-                return S.indexOf(o1) - S.indexOf(o2);
-            }
-        });
+        Arrays.sort(cs, (o1, o2) -> S.indexOf(o1) - S.indexOf(o2));
+
+//        Arrays.sort(cs, new Comparator<Character>() {
+//            @Override
+//            public int compare(Character o1, Character o2) {
+//                return S.indexOf(o1) - S.indexOf(o2);
+//            }
+//        });
 
         StringBuilder sb = new StringBuilder();
         for (char c : cs) sb.append(c);
@@ -29,5 +30,6 @@ public class CustomSortString {
     public static void main(String[] args) {
         CustomSortString css = new CustomSortString();
         System.out.println(css.customSortString("cba", "abcd"));
+        System.out.println(css.customSortString("cba", "aabbccdd"));
     }
 }
