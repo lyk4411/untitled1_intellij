4ef443e174cadd760dc652f964690fbd5e8baff9
ykliu <moneyflying_2006@hotmail.com>
2018-07-03 08:05:02 +0800

    core java ch 8

--- src/main/java/leetcode/LargestSumofAverages.java
+++ src/main/java/leetcode/LargestSumofAverages.java
@@ -6,6 +6,23 @@ package leetcode;
  * Porject name: untitled1
  */
 public class LargestSumofAverages {
+//    https://www.jianshu.com/p/950a25796be3
+//    思路
+//
+//    当看到Note里面A的lenth最大值是100的时候,这道题如果用dfs的话可能会超时(没有去试),用dp的话目测应
+//    该需要三重循环,因此此时大概就会想到用dp[k][i]表示前i+1个元素(0~i)最多分k个组是平均数和最大,然后
+//    就是自己手动写一个表可以简单推导一下.表格如下:
+//
+//            k\i   0 	    1 	    2 	    3 	    4
+//            0 	9.00 	5.00 	4.00 	3.75 	4.80
+//            1 	9.00 	10.00 	10.50 	11.00 	12.75
+//            2 	9.00 	10.00 	12.00 	13.50 	20.00
+//
+//    自己认认真真打完表后很容易就可以dp的方程:
+//    dp[k][i] = Math.max(dp[k - 1][i], dp[k - 1][j] + (sum[j + 1, i] / (i - j)); (k > = 1, sum[j + 1, i]
+//    表示区间j+1到i中间所有数的和)
+//    当然求区间和的话我们可以做一下预处理保证sum[j+1,i]是O(1)时间就可以.
+//
     public double largestSumOfAverages(int[] A, int K) {
         int len = A.length;
         double[] pre = new double[len + 1];
