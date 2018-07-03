d32d2540f59bb4269552eab6cc35078888ec8b26
ykliu <moneyflying_2006@hotmail.com>
2018-07-03 11:19:05 +0800

    core java ch 8

--- src/main/java/leetcode/NumberofSubarrayswithBoundedMaximum.java
+++ src/main/java/leetcode/NumberofSubarrayswithBoundedMaximum.java
@@ -6,6 +6,15 @@ package leetcode;
  * Porject name: untitled1
  */
 public class NumberofSubarrayswithBoundedMaximum {
+//    思路：
+//    我们定义[left, right]表示符合条件的合法区间，并且遍历数组A，根据A[right]的大小，可以分为三种情况进行处理：
+//            1）A[right]在[L, R]区间内，所以此时我们拥有从[left, right]...到[right, right]一共(right - left + 1)个合法
+//    的区间，所以我们更新res += (right - left + 1)，并且更新count的值（后面会有用）；
+//    2）A[right]小于L，此时虽然A[right]不在[L, R]区间内，但是对于前面的count个合法区间，每个加上A[right]，仍然可以形成
+//    合法的区间，所以res += count；
+//    3）A[right] > R，此时所有包含A[right]的区间都将不再合法，所以我们更新左边界left = right + 1，并更新count = 0。
+//    算法的空间复杂度为O(1)，时间复杂度为O(n)，应该是最优的空间和时间复杂度了。
+//
     public int numSubarrayBoundedMax(int[] A, int L, int R) {
         int left = 0, count = 0, res = 0;
         for (int right = 0; right < A.length; ++right) {
