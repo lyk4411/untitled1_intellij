6692c75ebcb2518fca0eff5b527a1dbd45d4942b
ykliu <moneyflying_2006@hotmail.com>
2018-07-04 10:46:30 +0800

    core java ch 8

---
+++ src/main/java/leetcode/MaxIncreasetoKeepCitySkyline.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+/**
+ * Created by lyk on 2018-7-4.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class MaxIncreasetoKeepCitySkyline {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int N = grid.length;
+        int[] rowMaxes = new int[N];
+        int[] colMaxes = new int[N];
+
+        for (int r = 0; r < N; ++r)
+            for (int c = 0; c < N; ++c) {
+                rowMaxes[r] = Math.max(rowMaxes[r], grid[r][c]);
+                colMaxes[c] = Math.max(colMaxes[c], grid[r][c]);
+            }
+
+        int ans = 0;
+        for (int r = 0; r < N; ++r)
+            for (int c = 0; c < N; ++c)
+                ans += Math.min(rowMaxes[r], colMaxes[c]) - grid[r][c];
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        MaxIncreasetoKeepCitySkyline mikcs = new MaxIncreasetoKeepCitySkyline();
+        System.out.println(mikcs.maxIncreaseKeepingSkyline(new int[][]{
+                {3, 0, 8, 4},
+                {2, 4, 5, 7},
+                {9, 2, 6, 3},
+                {0, 3, 1, 0}
+        }));
+    }
+}
