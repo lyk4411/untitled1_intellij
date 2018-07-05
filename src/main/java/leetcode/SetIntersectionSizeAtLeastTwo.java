57e95d93afba3be864eb4b7d84f8a755fcbdb147
ykliu <moneyflying_2006@hotmail.com>
2018-07-05 19:45:29 +0800

    core java ch 8

--- src/main/java/leetcode/SetIntersectionSizeAtLeastTwo.java
+++ src/main/java/leetcode/SetIntersectionSizeAtLeastTwo.java
@@ -46,6 +46,9 @@ public class SetIntersectionSizeAtLeastTwo {
                 {1, 3}, {5, 6}
         }));
         System.out.println(sisalt.intersectionSizeTwo(new int[][]{
+                {1, 3}, {1, 2}, {0, 1}
+        }));
+        System.out.println(sisalt.intersectionSizeTwo(new int[][]{
                 {16,18},{11,18},{15,23},{1,16},{10,16},{6,19},{18,20},
                 {7,19},{10,11},{11,23},{6,7},{23,25},{1,3},{7,12},{1,13},
                 {23,25},{10,22},{23,25},{0,19},{0,13},{7,12},{14,19},
