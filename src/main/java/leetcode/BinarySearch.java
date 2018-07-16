6da088c2d76180d3eaaa79203b8fc0afefe0722b
ykliu <moneyflying_2006@hotmail.com>
2018-07-16 14:11:35 +0800

    core java ch 8

--- src/main/java/leetcode/BinarySearch.java
+++ src/main/java/leetcode/BinarySearch.java
@@ -8,7 +8,7 @@ package leetcode;
 public class BinarySearch {
     public int search(int[] nums, int target) {
         int low = 0;
-        int high = nums.length;
+        int high = nums.length - 1;
         while (low <= high) {
             int mid = (low + high) / 2;
             if (target == nums[mid]) {
