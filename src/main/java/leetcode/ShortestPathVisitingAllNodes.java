7f70cc77a6785299d8b44f29ae585a120008be6b
ykliu <moneyflying_2006@hotmail.com>
2018-07-17 18:56:40 +0800

    core java ch 8

--- src/main/java/leetcode/ShortestPathVisitingAllNodes.java
+++ src/main/java/leetcode/ShortestPathVisitingAllNodes.java
@@ -10,7 +10,7 @@ import java.util.Queue;
  * Porject name: untitled1
  */
 public class ShortestPathVisitingAllNodes {
-    public int shortestPathLength(int[][] graph) {
+        public int shortestPathLength(int[][] graph) {
         int N = graph.length;
         Queue<State> queue = new LinkedList();
         int[][] dist = new int[1<<N][N];
@@ -46,14 +46,42 @@ public class ShortestPathVisitingAllNodes {
             head = h;
         }
     }
+//    public int shortestPathLength(int[][] graph) {
+//        int N = graph.length;
+//        int dist[][] = new int[1 << N][N];
+//        for (int[] row : dist) Arrays.fill(row, N * N);
+//        for (int x = 0; x < N; ++x) dist[1 << x][x] = 0;
+//
+//        for (int cover = 0; cover < 1 << N; ++cover) {
+//            boolean repeat = true;
+//            while (repeat) {
+//                repeat = false;
+//                for (int head = 0; head < N; ++head) {
+//                    int d = dist[cover][head];
+//                    for (int next : graph[head]) {
+//                        int cover2 = cover | (1 << next);
+//                        if (d + 1 < dist[cover2][next]) {
+//                            dist[cover2][next] = d + 1;
+//                            if (cover == cover2) repeat = true;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        int ans = N * N;
+//        for (int cand : dist[(1 << N) - 1])
+//            ans = Math.min(cand, ans);
+//        return ans;
+//    }
 
     public static void main(String[] args) {
         ShortestPathVisitingAllNodes spvan = new ShortestPathVisitingAllNodes();
         int[][] p1 = new int[][]{
-                {1,2,3},{0},{0},{0}
+                {1, 2, 3}, {0}, {0}, {0}
         };
         int[][] p2 = new int[][]{
-                {1},{0,2,4},{1,3,4},{2},{1,2}
+                {1}, {0, 2, 4}, {1, 3, 4}, {2}, {1, 2}
         };
         System.out.println(spvan.shortestPathLength(p1));
         System.out.println(spvan.shortestPathLength(p2));
