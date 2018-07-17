2faf1a69b8bd0a160c6a9ac5a69d1d0ef23918e3
ykliu <moneyflying_2006@hotmail.com>
2018-07-17 15:34:27 +0800

    core java ch 8

---
+++ src/main/java/leetcode/MyCircularQueue.java
@@ -0,0 +1,96 @@
+package leetcode;
+
+/**
+ * Created by lyk on 2018-7-17.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class MyCircularQueue {
+    private int[] data;
+
+    private int cur = -1;
+    private int tail = 0;
+
+
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    public MyCircularQueue(int k) {
+        if(k <= 0){
+            throw new IllegalArgumentException("size is not valid");
+        }
+        data = new int[k];
+
+    }
+
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    public boolean enQueue(int value) {
+        if(tail == cur){
+            return false;
+        }
+        data[tail] = value;
+        if(cur == -1){
+            cur = tail;
+        }
+        tail++;
+        if(tail == data.length){
+            tail = 0;
+        }
+        return true;
+    }
+
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+        if(cur<0){
+            return false;
+        }
+        data[cur] = 0;
+        cur++;
+        if(cur == data.length){
+            cur = 0;
+        }
+        //empty
+        if(cur == tail){
+            cur = -1;
+        }
+
+        return true;
+    }
+
+    /** Get the front item from the queue. */
+    public int Front() {
+        if(cur>-1){
+            return data[cur];
+        }
+        return -1;
+    }
+
+    /** Get the last item from the queue. */
+    public int Rear() {
+        if(cur>-1){
+            return data[tail==0?data.length-1:tail-1];
+        }
+        return -1;
+    }
+
+    /** Checks whether the circular queue is empty or not. */
+    public boolean isEmpty() {
+        return cur < 0;
+    }
+
+    /** Checks whether the circular queue is full or not. */
+    public boolean isFull() {
+        return cur == tail;
+    }
+
+    public static void main(String[] args) {
+        MyCircularQueue circularQueue = new MyCircularQueue(10);
+        circularQueue.enQueue(1);  // return true
+        circularQueue.enQueue(2);  // return true
+        circularQueue.enQueue(3);  // return true
+        circularQueue.enQueue(4);  // return false, the queue is full
+        System.out.println(circularQueue.Rear());  // return 3
+        circularQueue.isFull();  // return true
+        circularQueue.deQueue();  // return true
+        circularQueue.enQueue(4);  // return true
+        System.out.println(circularQueue.Rear());  // return 4
+    }
+}
