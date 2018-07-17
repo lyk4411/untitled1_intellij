5734b5e02a1b6589328d1582827bd8efb2d9493a
ykliu <moneyflying_2006@hotmail.com>
2018-07-17 16:25:53 +0800

    core java ch 8

---
+++ src/main/java/leetcode/ShortestPathVisitingAllNodes.java
@@ -0,0 +1,62 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by lyk on 2018-7-17.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class ShortestPathVisitingAllNodes {
+    public int shortestPathLength(int[][] graph) {
+        int N = graph.length;
+        Queue<State> queue = new LinkedList();
+        int[][] dist = new int[1<<N][N];
+        for (int[] row: dist) Arrays.fill(row, N*N);
+
+        for (int x = 0; x < N; ++x) {
+            queue.offer(new State(1<<x, x));
+            dist[1 << x][x] = 0;
+        }
+
+        while (!queue.isEmpty()) {
+            State node = queue.poll();
+            int d = dist[node.cover][node.head];
+            if (node.cover == (1<<N) - 1) return d;
+
+            for (int child: graph[node.head]) {
+                int cover2 = node.cover | (1 << child);
+                if (d + 1 < dist[cover2][child]) {
+                    dist[cover2][child] = d + 1;
+                    queue.offer(new State(cover2, child));
+
+                }
+            }
+        }
+
+        throw null;
+    }
+
+    class State {
+        int cover, head;
+        State(int c, int h) {
+            cover = c;
+            head = h;
+        }
+    }
+
+    public static void main(String[] args) {
+        ShortestPathVisitingAllNodes spvan = new ShortestPathVisitingAllNodes();
+        int[][] p1 = new int[][]{
+                {1,2,3},{0},{0},{0}
+        };
+        int[][] p2 = new int[][]{
+                {1},{0,2,4},{1,3,4},{2},{1,2}
+        };
+        System.out.println(spvan.shortestPathLength(p1));
+        System.out.println(spvan.shortestPathLength(p2));
+    }
+
+}
