e363ca441e731e9c4a939e1ca94347bca99edc11
ykliu <moneyflying_2006@hotmail.com>
2018-07-24 13:52:28 +0800

    core java ch 8

---
+++ src/main/java/leetcode/StickerstoSpellWord.java
@@ -0,0 +1,94 @@
+package leetcode;
+
+import java.util.HashMap;
+
+/**
+ * Created by lyk on 2018-7-24.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class StickerstoSpellWord {
+    private HashMap<String, Integer> dmap;
+    private HashMap<String, int[]> scnts;
+
+    public int[] str2Cnt(String str) {
+        int[] cnt = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            cnt[str.charAt(i) - 'a']++;
+        }
+        return cnt;
+    }
+
+    public String cnt2Str(int[] cnt) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < cnt[i]; j++) {
+                sb.append('a' + i);
+            }
+        }
+        return sb.toString();
+    }
+
+    public int minStickers(String[] stickers, String target) {
+        dmap = new HashMap<>();
+        scnts = new HashMap<>();
+        int[] tcnt = str2Cnt(target);
+        for (int i = 0; i < stickers.length; i++) {
+            StringBuffer sb = new StringBuffer();
+            for (int j = 0; j < stickers[i].length(); j++) {
+                char ch = stickers[i].charAt(j);
+                if (tcnt[ch - 'a'] > 0) sb.append(ch);
+            }
+            String str = sb.toString();
+            if (str.length() > 0 && !scnts.containsKey(str)) {
+                scnts.put(str, str2Cnt(str));
+            }
+//            System.out.println(scnts);
+        }
+        return search(tcnt);
+    }
+
+    public int search(int[] kcnt) {
+        String key = cnt2Str(kcnt);
+        if (key.length() == 0) {
+            return 0;
+        }
+        if (scnts.keySet().contains(key)) {
+            return 1;
+        }
+        if (dmap.containsKey(key)) {
+            return dmap.get(key);
+        }
+        int ans = -1;
+        for (int[] scnt : scnts.values()) {
+            int[] nkcnt = kcnt.clone();
+            boolean stopFlag = true;
+            for (int i = 0; i < 26; i++) {
+                int nval = Math.max(0, nkcnt[i] - scnt[i]);
+                if (nval != nkcnt[i]) stopFlag = false;
+                nkcnt[i] = nval;
+            }
+            if (stopFlag) continue;
+            int val = search(nkcnt);
+            if (val < 0) continue;
+            if (ans < 0 || val + 1 < ans) ans = val + 1;
+        }
+        dmap.put(key, ans);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        StickerstoSpellWord ssw = new StickerstoSpellWord();
+        String[] stickers1 = new String[]{
+                "with", "eexaample", "science"
+        };
+        String[] stickers2 = new String[]{
+                "notice", "possible","ss","s"
+        };
+
+        System.out.println(ssw.minStickers(stickers1, "thehat"));
+        System.out.println(ssw.minStickers(stickers2, "basicbasic"));
+
+    }
+
+}
