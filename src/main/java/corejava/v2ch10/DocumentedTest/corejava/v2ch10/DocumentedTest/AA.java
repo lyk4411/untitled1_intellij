f17bccdb06851444a14b261e8f034d78c7f1e9e6
ykliu <moneyflying_2006@hotmail.com>
2018-08-20 15:31:51 +0800

    core java ch 8

---
+++ src/main/java/corejava/v2ch10/DocumentedTest/corejava/v2ch10/DocumentedTest/AA.java
@@ -0,0 +1,65 @@
+package corejava.v2ch10.DocumentedTest.corejava.v2ch10.DocumentedTest;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by lyk on 2018-8-20.
+ * Package name: corejava.v2ch10.DocumentedTest.corejava.v2ch10.DocumentedTest
+ * Porject name: untitled1
+ */
+
+@Target({ElementType.TYPE,ElementType.FIELD,ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited //添加可继承元注解
+@Repeatable(FilterPaths.class)
+@interface FilterPath {
+    String  value();
+}
+
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited //添加可继承元注解
+@interface FilterPaths {
+    FilterPath[] value();
+}
+
+@FilterPath("/web/list")
+@FilterPath("/web/getList")
+class CC { }
+
+//AA上不使用@FilterPath注解,getAnnotationsByType将会从父类查询
+class AA extends CC{
+    public static void main(String[] args) {
+
+        Class<?> clazz = AA.class;
+        //通过getAnnotationsByType方法获取所有重复注解
+        FilterPath[] annotationsByType = clazz.getAnnotationsByType(FilterPath.class);
+        FilterPath[] annotationsByType2 = clazz.getDeclaredAnnotationsByType(FilterPath.class);
+        if (annotationsByType != null) {
+            for (FilterPath filter : annotationsByType) {
+                System.out.println("1:"+filter.value());
+            }
+        }
+
+        System.out.println("-----------------");
+
+        if (annotationsByType2 != null) {
+            for (FilterPath filter : annotationsByType2) {
+                System.out.println("2:"+filter.value());
+            }
+        }
+
+
+        System.out.println("使用getAnnotation的结果:"+clazz.getAnnotation(FilterPath.class));
+
+
+        /**
+         * 执行结果(当前类没有@FilterPath,getAnnotationsByType方法从CC父类寻找)
+         1:/web/list
+         1:/web/getList
+         -----------------
+         使用getAnnotation的结果:null
+         */
+    }
+}
