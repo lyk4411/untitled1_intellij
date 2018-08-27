5dd0d7709628ff47cf6a802537fc8fcdbc489d9a
ykliu <moneyflying_2006@hotmail.com>
2018-08-27 18:18:20 +0800

    core java ch 8

---
+++ src/main/java/leetcode/FairCandySwap.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by lyk on 2018-8-27.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class FairCandySwap {
+    public int[] fairCandySwap(int[] A, int[] B) {
+        int sa = 0, sb = 0;  // sum of A, B respectively
+        for (int x: A) sa += x;
+        for (int x: B) sb += x;
+        int delta = (sb - sa) / 2;
+        // If Alice gives x, she expects to receive x + delta
+
+        Set<Integer> setB = new HashSet();
+        for (int x: B) setB.add(x);
+
+        for (int x: A)
+            if (setB.contains(x + delta))
+                return new int[]{x, x + delta};
+
+        throw null;
+    }
+
+    public static void main(String[] args) {
+        FairCandySwap fcs = new FairCandySwap();
+        System.out.println(fcs.fairCandySwap(new int[]{1, 2}, new int[]{2, 2}));
+        System.out.println(fcs.fairCandySwap(new int[]{1, 2}, new int[]{2, 3}));
+        System.out.println(fcs.fairCandySwap(new int[]{2}, new int[]{1, 3}));
+        System.out.println(fcs.fairCandySwap(new int[]{1, 2, 5}, new int[]{2, 4}));
+    }
+}
