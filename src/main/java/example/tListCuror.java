18d23a92de9e1e683aff70fb25fd0c6cd2f265c7
ykliu <moneyflying_2006@hotmail.com>
2018-10-11 11:28:53 +0800

    core java ch 8

--- src/main/java/example/tListCuror.java
+++ src/main/java/example/tListCuror.java
@@ -32,5 +32,28 @@ public class tListCuror {
     public static void main(String[] args) {
         tListCuror tlc = new tListCuror();
         tlc.testListItr();
+
+
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            list.add("sh" + i);
+        }
+//        其实这里会抛出角标越界异常，为什么呢，因为 for 循环的条件
+//        list.iterator().hasNext()，我们知道 list.iterator() 将会new 一个新的
+//        iterator 对象，而在 new 的过程中我们将 每次 list.remove 后的 modCount
+//        赋值给了新的 iterator 的 expectedModCount，所以不会抛出
+//        ConcurrentModificationException 异常，而 hasNext 内部只判断了 size
+//        是否等于 cursor != size 当我们删除了一半元素以后，size 变成了 5 而新的
+//        list.iterator() 的 cursor 等于 0 ，0!=5 for 循环继续，那么当执行到
+//        list.remove（5）的时候就会抛出角标越界了。
+
+//        所以，注释掉 list.remove(i);也会越界。0!=9，for 循环继续，
+
+
+        for (int i = 0; list.iterator().hasNext(); i++) {
+//            list.remove(i);
+            System.out.println("秘密" + list.get(i));
+        }
+
     }
 }
