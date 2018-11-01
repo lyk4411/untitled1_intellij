8bc735f2a1b6caa40c3b4014289d381d3c1d00bc
ykliu <moneyflying_2006@hotmail.com>
2018-11-01 19:13:26 +0800

    core java ch 8

---
+++ src/main/java/learnJVM/ch02/HeapOOM.java
@@ -0,0 +1,29 @@
+package learnJVM.ch02;
+
+/**
+ * Created by lyk on 2018-11-1.
+ * Package name: learnJVM.ch02
+ * Porject name: untitled1
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Args：-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * @author zzm
+ */
+public class HeapOOM {
+
+    static class OOMObject {
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<OOMObject>();
+
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+}
+
