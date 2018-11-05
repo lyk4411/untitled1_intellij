829d54baf72daea69ec2c50c579e6c0876f9fa2f
ykliu <moneyflying_2006@hotmail.com>
2018-11-05 18:32:59 +0800

    core java ch 8

---
+++ src/main/java/learnJVM/ch04/ch04ex08.java
@@ -0,0 +1,34 @@
+package learnJVM.ch04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lyk on 2018-11-5.
+ * Package name: learnJVM.ch04
+ * Porject name: untitled1
+ */
+public class ch04ex08 {
+    /**
+     * 内存占位符对象，一个OOMObject大约占64K
+     */
+    static class OOMObject {
+        public byte[] placeholder = new byte[64 * 1024];
+    }
+
+    public static void fillHeap(int num) throws InterruptedException {
+        List<OOMObject> list = new ArrayList<OOMObject>();
+        for (int i = 0; i < num; i++) {
+            // 稍作延时，令监视曲线的变化更加明显
+            Thread.sleep(50);
+            list.add(new OOMObject());
+        }
+        System.gc();
+    }
+
+    public static void main(String[] args) throws Exception {
+        fillHeap(1000);
+    }
+
+
+}
