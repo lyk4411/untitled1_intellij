222bb02e3d90911f205530a4168eaedf09b1f380
ykliu <moneyflying_2006@hotmail.com>
2018-11-05 18:46:02 +0800

    core java ch 8

---
+++ src/main/java/learnJVM/ch04/ch04ex09.java
@@ -0,0 +1,55 @@
+package learnJVM.ch04;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by lyk on 2018-11-5.
+ * Package name: learnJVM.ch04
+ * Porject name: untitled1
+ */
+public class ch04ex09 {
+    /**
+     * 线程死循环演示
+     */
+    public static void createBusyThread() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true)   // 第41行
+                    ;
+            }
+        }, "testBusyThread");
+        thread.start();
+    }
+
+    /**
+     * 线程锁等待演示
+     */
+    public static void createLockThread(final Object lock) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "testLockThread");
+        thread.start();
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        createBusyThread();
+        br.readLine();
+        Object obj = new Object();
+        createLockThread(obj);
+    }
+
+
+}
