774ce52a380acd816b6e6276475ee479490b615a
ykliu <moneyflying_2006@hotmail.com>
2018-11-06 16:43:39 +0800

    core java ch 8

---
+++ src/main/java/learnJVM/ch06/TestClass.java
@@ -0,0 +1,17 @@
+package learnJVM.ch06;
+
+/**
+ * Created by lyk on 2018-11-6.
+ * Package name: learnJVM.ch06
+ * Porject name: untitled1
+ */
+
+public class TestClass {
+
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
+
