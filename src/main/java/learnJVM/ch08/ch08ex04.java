ad6c3ef4c02f35c43dd547fd342a7422b9af1053
ykliu <moneyflying_2006@hotmail.com>
2018-11-08 15:30:57 +0800

    core java ch 8

---
+++ src/main/java/learnJVM/ch08/ch08ex04.java
@@ -0,0 +1,15 @@
+package learnJVM.ch08;
+
+/**
+ * Created by lyk on 2018-11-8.
+ * Package name: learnJVM.ch08
+ * Porject name: untitled1
+ */
+public class ch08ex04 {
+    public static void main(String[] args) {
+        int a = 0;
+        System.out.println(a);
+    }
+
+
+}
