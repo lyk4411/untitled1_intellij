957c1089c664b23a4fe48f88c55a096964b2aa34
ykliu <moneyflying_2006@hotmail.com>
2018-11-08 09:11:16 +0800

    core java ch 8

---
+++ src/main/java/learnJVM/ch07/NotInitialization1.java
@@ -0,0 +1,12 @@
+package learnJVM.ch07;
+
+/**
+ * Created by lyk on 2018-11-8.
+ * Package name: learnJVM.ch07
+ * Porject name: untitled1
+ */
+public class NotInitialization1 {
+    public static void main(String[] args) {
+        SuperClass[] sca = new SuperClass[10];
+    }
+}
