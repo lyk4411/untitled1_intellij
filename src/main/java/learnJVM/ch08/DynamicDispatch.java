62f975d76f13a70692e28e3b7e192cdacdff652c
ykliu <moneyflying_2006@hotmail.com>
2018-11-09 09:32:36 +0800

    core java ch 8

---
+++ src/main/java/learnJVM/ch08/DynamicDispatch.java
@@ -0,0 +1,36 @@
+package learnJVM.ch08;
+
+/**
+ * Created by lyk on 2018-11-9.
+ * Package name: learnJVM.ch08
+ * Porject name: untitled1
+ */
+public class DynamicDispatch {
+
+    static abstract class Human {
+        protected abstract void sayHello();
+    }
+
+    static class Man extends Human {
+        @Override
+        protected void sayHello() {
+            System.out.println("man say hello");
+        }
+    }
+
+    static class Woman extends Human {
+        @Override
+        protected void sayHello() {
+            System.out.println("woman say hello");
+        }
+    }
+
+    public static void main(String[] args) {
+        Human man = new Man();
+        Human woman = new Woman();
+        man.sayHello();
+        woman.sayHello();
+        man = new Woman();
+        man.sayHello();
+    }
+}
