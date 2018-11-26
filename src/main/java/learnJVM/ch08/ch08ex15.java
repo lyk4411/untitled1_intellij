9e9aeb4883942fc5c6bfbb47c457f99b9db44586
ykliu <moneyflying_2006@hotmail.com>
2018-11-26 11:14:55 +0800

    core java ch 8

---
+++ src/main/java/learnJVM/ch08/ch08ex15.java
@@ -0,0 +1,41 @@
+package learnJVM.ch08;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
+
+import static java.lang.invoke.MethodHandles.lookup;
+/**
+ * Created by lyk on 2018-11-26.
+ * Package name: learnJVM.ch08
+ * Porject name: untitled1
+ */
+public class ch08ex15 {
+
+
+    class GrandFather {
+        void thinking() {
+            System.out.println("i am grandfather");
+        }
+    }
+
+    class Father extends GrandFather {
+        void thinking() {
+            System.out.println("i am father");
+        }
+    }
+
+    class Son extends Father {
+        void thinking() {
+            try {
+                MethodType mt = MethodType.methodType(void.class);
+                MethodHandle mh = lookup().findSpecial(GrandFather.class,
+                        "thinking", mt, getClass());
+                mh.invoke(this);
+                } catch (Throwable e) {
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        (new ch08ex15().new Son()).thinking();
+    }
+}
