a84b722ce31bdcf5183e3b1ac73543e4497248b9
ykliu <moneyflying_2006@hotmail.com>
2018-11-27 11:26:07 +0800

    core java ch 8

---
+++ src/main/java/learnJVM/ch10/BADLY_NAMED_CODE.java
@@ -0,0 +1,27 @@
+package learnJVM.ch10;
+
+/**
+ * Created by lyk on 2018-11-27.
+ * Package name: learnJVM.ch10
+ * Porject name: untitled1
+ */
+
+public class BADLY_NAMED_CODE {
+
+    enum colors {
+        red, blue, green;
+    }
+
+    static final int _FORTY_TWO = 42;
+
+    public static int NOT_A_CONSTANT = _FORTY_TWO;
+
+    protected void BADLY_NAMED_CODE() {
+        return;
+    }
+
+    public void NOTcamelCASEmethodNAME() {
+        return;
+    }
+}
+
