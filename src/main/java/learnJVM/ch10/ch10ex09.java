a2eef9c00b43df82f780354f5081ead358e7f70c
ykliu <moneyflying_2006@hotmail.com>
2018-11-27 11:02:15 +0800

    core java ch 8

---
+++ src/main/java/learnJVM/ch10/ch10ex09.java
@@ -0,0 +1,19 @@
+package learnJVM.ch10;
+
+/**
+ * Created by lyk on 2018-11-27.
+ * Package name: learnJVM.ch10
+ * Porject name: untitled1
+ */
+public class ch10ex09 {
+    public static void main(String[] args) {
+        if (false) {
+            System.out.println("block 1");
+        } else {
+            System.out.println("block 2");
+        }
+
+    }
+
+
+}
