62e3c11469aa49b2f6087824ad8cfdb41d1ea813
ykliu <moneyflying_2006@hotmail.com>
2018-11-29 15:58:45 +0800

    core java ch 8

---
+++ src/main/java/learnJVM/ch13/ch13ex03.java
@@ -0,0 +1,48 @@
+package learnJVM.ch13;
+
+import java.util.Vector;
+
+/**
+ * Created by lyk on 2018-11-29.
+ * Package name: learnJVM.ch13
+ * Porject name: untitled1
+ */
+public class ch13ex03 {
+    private static Vector<Integer> vector = new Vector<Integer>();
+
+    public static void main(String[] args) {
+        while (true) {
+            for (int i = 0; i < 10; i++) {
+                vector.add(i);
+            }
+
+            Thread removeThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    synchronized (vector) {
+                        for (int i = 0; i < vector.size(); i++) {
+                            vector.remove(i);
+                        }
+                    }
+                }
+            });
+
+            Thread printThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    synchronized (vector) {
+                        for (int i = 0; i < vector.size(); i++) {
+                            System.out.println((vector.get(i)));
+                        }
+                    }
+                }
+            });
+
+            removeThread.start();
+            printThread.start();
+
+            //不要同时产生过多的线程，否则会导致操作系统假死
+            while (Thread.activeCount() > 20);
+        }
+    }
+}
