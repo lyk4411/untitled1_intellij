7b66256a27dce7dc8ce985c6bd5ceafa5507f762
ykliu <moneyflying_2006@hotmail.com>
2018-12-10 09:26:57 +0800

    core java ch 8

--- src/main/java/learnJVM/ch13/classtest.java
+++ src/main/java/learnJVM/ch13/classtest.java
@@ -11,5 +11,15 @@ public class classtest {
         Class a =  ct.getClass();
         final String name = a.getName();
         System.out.println(name);
+
+        String str2 = new String("str")+new String("01");
+        str2.intern();
+        String str1 = "str01";
+        System.out.println(str2==str1);
+
+        String str3 = new String("str11")+new String("0111");
+        String str4 = "str110111";
+        str3.intern();
+        System.out.println(str3==str4);
     }
 }
