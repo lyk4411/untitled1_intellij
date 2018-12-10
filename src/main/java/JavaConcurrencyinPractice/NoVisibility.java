5575af2bee3a7e344f3a9e6f405b649025569da9
ykliu <moneyflying_2006@hotmail.com>
2018-12-10 19:01:01 +0800

    core java ch 8

---
+++ src/main/java/JavaConcurrencyinPractice/NoVisibility.java
@@ -0,0 +1,25 @@
+package JavaConcurrencyinPractice;
+
+/**
+ * Created by lyk on 2018-12-10.
+ * Package name: JavaConcurrencyinPractice
+ * Porject name: untitled1
+ */
+public class NoVisibility {
+    private static boolean ready;
+    private static int number;
+
+    private static class ReaderThread extends Thread {
+        public void run() {
+            while (!ready)
+                Thread.yield();
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReaderThread().start();
+        number = 42;
+        ready = true;
+    }
+}
---
+++ src/main/java/JavaConcurrencyinPractice/Sequence.java
@@ -0,0 +1,18 @@
+package JavaConcurrencyinPractice;
+
+import net.jcip.annotations.*;
+
+/**
+ * Sequence
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+
+@ThreadSafe
+public class Sequence {
+    @GuardedBy("this") private int nextValue;
+
+    public synchronized int getNext() {
+        return nextValue++;
+    }
+}
