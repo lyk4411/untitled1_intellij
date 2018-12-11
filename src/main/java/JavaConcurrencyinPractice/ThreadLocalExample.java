e184a34a13670d5a47659e10328483c377b82e5c
ykliu <moneyflying_2006@hotmail.com>
2018-12-11 19:08:22 +0800

    core java ch 8

---
+++ src/main/java/JavaConcurrencyinPractice/ThreadLocalExample.java
@@ -0,0 +1,35 @@
+package JavaConcurrencyinPractice;
+
+/**
+ * Created by lyk on 2018-12-11.
+ * Package name: JavaConcurrencyinPractice
+ * Porject name: untitled1
+ */
+public class ThreadLocalExample {
+    public static class MyRunnable implements Runnable {
+
+        private ThreadLocal threadLocal = new ThreadLocal();
+
+        @Override
+        public void run() {
+            threadLocal.set((int) (Math.random() * 100D));
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+
+            }
+            System.out.println(threadLocal.get());
+        }
+    }
+
+    public static void main(String[] args) {
+        MyRunnable sharedRunnableInstance = new MyRunnable();
+        Thread thread1 = new Thread(sharedRunnableInstance);
+        Thread thread2 = new Thread(sharedRunnableInstance);
+        thread1.start();
+        thread2.start();
+
+    }
+
+
+}
