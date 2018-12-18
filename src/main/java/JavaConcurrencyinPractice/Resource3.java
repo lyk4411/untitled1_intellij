9abfc18d81ed7fbdf00606834d0e32eb228083fb
ykliu <moneyflying_2006@hotmail.com>
2018-12-18 11:10:09 +0800

    core java ch 8

---
+++ src/main/java/JavaConcurrencyinPractice/Resource3.java
@@ -0,0 +1,39 @@
+package JavaConcurrencyinPractice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Created by lyk on 2018-12-18.
+ * Package name: JavaConcurrencyinPractice
+ * Porject name: untitled1
+ */
+
+public class Resource3 {
+
+    public static void main(String[] args) throws InterruptedException {
+        List<String> a = new ArrayList<String>();
+        a.add("a");
+        a.add("b");
+        a.add("c");
+        final CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<String>(a);
+        Thread t = new Thread(new Runnable() {
+            int count = -1;
+
+            @Override
+            public void run() {
+                while (true) {
+                    list.add(count++ + "");
+                }
+            }
+        });
+        t.setDaemon(true);
+        t.start();
+        Thread.currentThread().sleep(3);
+        for (String s : list) {
+            System.out.println(list.hashCode());
+            System.out.println(s);
+        }
+    }
+}
