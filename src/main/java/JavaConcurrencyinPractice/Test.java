46ae0430b944367e389744bf45ed3cc7db43e2ff
ykliu <moneyflying_2006@hotmail.com>
2018-12-19 18:20:17 +0800

    core java ch 8

---
+++ src/main/java/JavaConcurrencyinPractice/Test.java
@@ -0,0 +1,47 @@
+package JavaConcurrencyinPractice;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by lyk on 2018-12-19.
+ * Package name: JavaConcurrencyinPractice
+ * Porject name: untitled1
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        Task task = new Task();
+        Future<Integer> result = executor.submit(task);
+        executor.shutdown();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+
+        System.out.println("主线程在执行任务");
+
+        try {
+            System.out.println("task运行结果"+result.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("所有任务执行完毕");
+    }
+}
+class Task implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("子线程在进行计算");
+        Thread.sleep(300);
+        int sum = 0;
+        for(int i=0;i<100;i++)
+            sum += i;
+        return sum;
+    }
+}
