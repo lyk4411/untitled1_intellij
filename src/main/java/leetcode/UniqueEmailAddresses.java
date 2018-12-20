8ec6d0c053c070853e353a980faf8e921abc36e0
ykliu <moneyflying_2006@hotmail.com>
2018-12-20 19:25:26 +0800

    core java ch 8

---
+++ src/main/java/leetcode/UniqueEmailAddresses.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by lyk on 2018-12-20.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class UniqueEmailAddresses {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> seen = new HashSet();
+        for (String email: emails) {
+            int i = email.indexOf('@');
+            String local = email.substring(0, i);
+            String rest = email.substring(i);
+            if (local.contains("+")) {
+                local = local.substring(0, local.indexOf('+'));
+            }
+            local = local.replaceAll(".", "");
+            seen.add(local + rest);
+        }
+
+        return seen.size();
+    }
+
+    public static void main(String[] args) {
+        UniqueEmailAddresses uea = new UniqueEmailAddresses();
+        System.out.println(uea.numUniqueEmails(
+                new String[]{"test.email+alex@leetcode.com",
+                        "test.e.mail+bob.cathy@leetcode.com",
+                        "testemail+david@lee.tcode.com"}
+        ));
+
+    }
+}
