566332f21d346831cabc0d0d9d95a488f0c1ab96
ykliu <moneyflying_2006@hotmail.com>
2018-12-24 16:50:43 +0800

    core java ch 8

---
+++ src/main/java/leetcode/FindandReplacePattern.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by lyk on 2018-12-24.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class FindandReplacePattern {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> ans = new ArrayList();
+        for (String word: words)
+            if (match(word, pattern))
+                ans.add(word);
+        return ans;
+    }
+
+    public boolean match(String word, String pattern) {
+        Map<Character, Character> m1 = new HashMap();
+        Map<Character, Character> m2 = new HashMap();
+
+        for (int i = 0; i < word.length(); ++i) {
+            char w = word.charAt(i);
+            char p = pattern.charAt(i);
+            if (!m1.containsKey(w)) m1.put(w, p);
+            if (!m2.containsKey(p)) m2.put(p, w);
+            if (m1.get(w) != p || m2.get(p) != w)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        FindandReplacePattern frp = new FindandReplacePattern();
+        String[] words = new String[]{"abc","deq","mee","aqq","dkd","ccc"};
+        System.out.println(frp.findAndReplacePattern(words,"abb"));
+        System.out.println(frp.findAndReplacePattern(words,"xxx"));
+    }
+}
