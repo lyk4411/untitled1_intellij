d6a0680beb8753d8a4132fa22d8804326cea43b1
ykliu <moneyflying_2006@hotmail.com>
2018-12-24 19:14:51 +0800

    core java ch 8

---
+++ src/main/java/leetcode/FlipEquivalentBinaryTrees.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lyk on 2018-12-24.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class FlipEquivalentBinaryTrees {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        List<Integer> vals1 = new ArrayList();
+        List<Integer> vals2 = new ArrayList();
+        dfs(root1, vals1);
+        dfs(root2, vals2);
+        return vals1.equals(vals2);
+    }
+
+    public void dfs(TreeNode node, List<Integer> vals) {
+        if (node != null) {
+            vals.add(node.val);
+            int L = node.left != null ? node.left.val : -1;
+            int R = node.right != null ? node.right.val : -1;
+
+            if (L < R) {
+                dfs(node.left, vals);
+                dfs(node.right, vals);
+            } else {
+                dfs(node.right, vals);
+                dfs(node.left, vals);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        FlipEquivalentBinaryTrees febt = new FlipEquivalentBinaryTrees();
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        t1.left = t2;
+        t1.right = t3;
+        TreeNode a1 = new TreeNode(1);
+        TreeNode a2 = new TreeNode(2);
+        TreeNode a3 = new TreeNode(3);
+        a1.right = a2;
+        a1.left = a3;
+        System.out.println(febt.flipEquiv(t1, a1));
+    }
+}
