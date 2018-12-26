8367bd6dc2abbe6846d746de98c4e9426bfff8ed
ykliu <moneyflying_2006@hotmail.com>
2018-12-26 19:11:59 +0800

    core java ch 8

--- src/main/java/leetcode/MinimumAreaRectangle.java
+++ src/main/java/leetcode/MinimumAreaRectangle.java
@@ -1,7 +1,6 @@
 package leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by lyk on 2018-12-26.
@@ -9,25 +8,49 @@ import java.util.Set;
  * Porject name: untitled1
  */
 public class MinimumAreaRectangle {
-    public int minAreaRect(int[][] points) {
-        Set<Integer> pointSet = new HashSet();
-        for (int[] point: points)
-            pointSet.add(40001 * point[0] + point[1]);
-
-        int ans = Integer.MAX_VALUE;
-        for (int i = 0; i < points.length; ++i)
-            for (int j = i+1; j < points.length; ++j) {
-                if (points[i][0] != points[j][0] && points[i][1] != points[j][1]) {
-                    if (pointSet.contains(40001 * points[i][0] + points[j][1]) &&
-                            pointSet.contains(40001 * points[j][0] + points[i][1])) {
-                        ans = Math.min(ans, Math.abs(points[j][0] - points[i][0]) *
-                                Math.abs(points[j][1] - points[i][1]));
-                    }
-                }
-            }
-
-        return ans < Integer.MAX_VALUE ? ans : 0;
+//    public int minAreaRect(int[][] points) {
+//        Set<Integer> pointSet = new HashSet();
+//        for (int[] point: points)
+//            pointSet.add(40001 * point[0] + point[1]);
+//
+//        int ans = Integer.MAX_VALUE;
+//        for (int i = 0; i < points.length; ++i)
+//            for (int j = i+1; j < points.length; ++j) {
+//                if (points[i][0] != points[j][0] && points[i][1] != points[j][1]) {
+//                    if (pointSet.contains(40001 * points[i][0] + points[j][1]) &&
+//                            pointSet.contains(40001 * points[j][0] + points[i][1])) {
+//                        ans = Math.min(ans, Math.abs(points[j][0] - points[i][0]) *
+//                                Math.abs(points[j][1] - points[i][1]));
+//                    }
+//                }
+//            }
+//
+//        return ans < Integer.MAX_VALUE ? ans : 0;
+//    }
+public int minAreaRect(int[][] points) {
+    Map<Integer, List<Integer>> rows = new TreeMap();
+    for (int[] point: points) {
+        int x = point[0], y = point[1];
+        rows.computeIfAbsent(x, z-> new ArrayList()).add(y);
     }
+
+    int ans = Integer.MAX_VALUE;
+    Map<Integer, Integer> lastx = new HashMap();
+    for (int x: rows.keySet()) {
+        List<Integer> row = rows.get(x);
+        Collections.sort(row);
+        for (int i = 0; i < row.size(); ++i)
+            for (int j = i+1; j < row.size(); ++j) {
+                int y1 = row.get(i), y2 = row.get(j);
+                int code = 40001 * y1 + y2;
+                if (lastx.containsKey(code))
+                    ans = Math.min(ans, (x - lastx.get(code)) * (y2-y1));
+                lastx.put(code, x);
+            }
+    }
+
+    return ans < Integer.MAX_VALUE ? ans : 0;
+}
 
     public static void main(String[] args) {
         MinimumAreaRectangle mar = new MinimumAreaRectangle();
