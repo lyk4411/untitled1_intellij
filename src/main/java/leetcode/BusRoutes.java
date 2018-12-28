44a94009df00aeee7bfde881864e57ebe22a1938
ykliu <moneyflying_2006@hotmail.com>
2018-12-28 20:41:57 +0800

    core java ch 8

---
+++ src/main/java/leetcode/BusRoutes.java
@@ -0,0 +1,88 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * Created by lyk on 2018-12-28.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class BusRoutes {
+    public int numBusesToDestination(int[][] routes, int S, int T) {
+        if (S==T) return 0;
+        int N = routes.length;
+
+        List<List<Integer>> graph = new ArrayList();
+        for (int i = 0; i < N; ++i) {
+            Arrays.sort(routes[i]);
+            graph.add(new ArrayList());
+        }
+        Set<Integer> seen = new HashSet();
+        Set<Integer> targets = new HashSet();
+        Queue<Point> queue = new ArrayDeque();
+
+        // Build the graph.  Two buses are connected if
+        // they share at least one bus stop.
+        for (int i = 0; i < N; ++i)
+            for (int j = i+1; j < N; ++j)
+                if (intersect(routes[i], routes[j])) {
+                    graph.get(i).add(j);
+                    graph.get(j).add(i);
+                }
+        System.out.println(graph);
+
+        // Initialize seen, queue, targets.
+        // seen represents whether a node has ever been enqueued to queue.
+        // queue handles our breadth first search.
+        // targets is the set of goal states we have.
+        for (int i = 0; i < N; ++i) {
+            if (Arrays.binarySearch(routes[i], S) >= 0) {
+                seen.add(i);
+                queue.offer(new Point(i, 0));
+            }
+            if (Arrays.binarySearch(routes[i], T) >= 0)
+                targets.add(i);
+        }
+
+        while (!queue.isEmpty()) {
+            Point info = queue.poll();
+            int node = info.x, depth = info.y;
+            if (targets.contains(node)) return depth+1;
+            for (Integer nei: graph.get(node)) {
+                if (!seen.contains(nei)) {
+                    seen.add(nei);
+                    queue.offer(new Point(nei, depth+1));
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public boolean intersect(int[] A, int[] B) {
+        int i = 0, j = 0;
+        while (i < A.length && j < B.length) {
+            if (A[i] == B[j]) return true;
+            if (A[i] < B[j]) i++; else j++;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        BusRoutes br = new BusRoutes();
+        System.out.println(br.numBusesToDestination(
+                new int[][]{{1, 2, 7},{3, 6, 7},}, 1, 6));
+    }
+
+    class Point {
+        int x;
+        int y;
+        Point() { x = 0; y = 0; }
+        Point(int a, int b) { x = a; y = b; }
+
+        @Override
+        public String toString() {
+            return "[x = " + x + ", y = " + y + "]";
+        }
+    }
+}
