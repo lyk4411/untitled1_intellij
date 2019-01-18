7bed33ce8a63679a447f151ae1d7e27ae2fbdf85
ykliu <moneyflying_2006@hotmail.com>
2019-01-18 11:14:03 +0800

    core java ch 8

--- src/main/java/effectiveJava/Chapter3/Item8/composition/ColorPoint.java
+++ src/main/java/effectiveJava/Chapter3/Item8/composition/ColorPoint.java
@@ -6,8 +6,10 @@ public class ColorPoint {
     private final Color color;
 
     public ColorPoint(int x, int y, Color color) {
-        if (color == null)
+        if (color == null){
+            System.out.println("Null color.");
             throw new NullPointerException();
+        }
         point = new Point(x, y);
         this.color = color;
     }
@@ -19,14 +21,30 @@ public class ColorPoint {
         return point;
     }
 
-    @Override public boolean equals(Object o) {
+    @Override
+    public boolean equals(Object o) {
         if (!(o instanceof ColorPoint))
             return false;
         ColorPoint cp = (ColorPoint) o;
         return cp.point.equals(point) && cp.color.equals(color);
     }
 
-    @Override public int hashCode() {
+    @Override
+    public int hashCode() {
         return point.hashCode() * 33 + color.hashCode();
+    }
+
+    public static void main(String[] args) {
+        // First equals function violates symmetry
+        Point p = new Point(1, 2);
+        ColorPoint cp = new ColorPoint(1, 2, Color.RED);
+        System.out.println(p.equals(cp) + " " + cp.equals(p));
+
+        // Second equals function violates transitivity
+        ColorPoint p1 = new ColorPoint(1, 2, Color.RED);
+        Point p2 = new Point(1, 2);
+        ColorPoint p3 = new ColorPoint(1, 2, Color.BLUE);
+        System.out.printf("%s %s %s%n",
+                p1.equals(p2), p2.equals(p3), p1.equals(p3));
     }
 }
