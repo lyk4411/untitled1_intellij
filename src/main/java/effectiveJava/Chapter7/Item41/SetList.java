18acb28fd2ee763112f0c04eecc25b76116f68bb
ykliu <moneyflying_2006@hotmail.com>
2019-01-24 15:26:14 +0800

    core java ch 8

--- src/main/java/effectiveJava/Chapter7/Item41/SetList.java
+++ src/main/java/effectiveJava/Chapter7/Item41/SetList.java
@@ -6,17 +6,20 @@ public class SetList {
     public static void main(String[] args) {
         Set<Integer> set = new TreeSet<Integer>();
         List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list1 = new ArrayList<Integer>();
 
         for (int i = -3; i < 3; i++) {
             set.add(i);
             list.add(i);
+            list1.add(i);
         }
 
         for (int i = 0; i < 3; i++) {
             set.remove(i);
             list.remove(i);
+            list1.remove(Integer.valueOf(i));
         }
 
-        System.out.println(set + " " + list);
+        System.out.println(set + " " + list + " " + list1);
     }
 }
