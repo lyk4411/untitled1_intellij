93023141512ff9ff377f214674b1bc15b22561fb
ykliu <moneyflying_2006@hotmail.com>
2019-01-28 14:49:19 +0800

    core java ch 8

--- src/main/java/effectiveJava/Chapter10/Item69/ConcurrentTimer.java
+++ src/main/java/effectiveJava/Chapter10/Item69/ConcurrentTimer.java
@@ -11,21 +11,18 @@ public class ConcurrentTimer {
         final CountDownLatch start = new CountDownLatch(1);
         final CountDownLatch done = new CountDownLatch(concurrency);
 
-        for (int i = 0; i < concurrency; i++) {
-            executor.execute(new Runnable() {
-                public void run() {
-                    ready.countDown(); // Tell timer we're ready
-                    try {
-                        start.await(); // Wait till peers are ready
-                        action.run();
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    } finally {
-                        done.countDown();  // Tell timer we're done
-                    }
+        for (int i = 0; i < concurrency; i++)
+            executor.execute(() -> {
+                ready.countDown(); // Tell timer we're ready
+                try {
+                    start.await(); // Wait till peers are ready
+                    action.run();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    done.countDown();  // Tell timer we're done
                 }
             });
-        }
 
         ready.await();     // Wait for all workers to be ready
         long startNanos = System.nanoTime();
