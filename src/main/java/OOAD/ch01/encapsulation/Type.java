f3da36e770a72f4c6bf0eafdffdd8db69a40c55c
ykliu <moneyflying_2006@hotmail.com>
2019-01-29 18:29:33 +0800

    core java ch 8

---
+++ src/main/java/OOAD/ch01/encapsulation/Type.java
@@ -0,0 +1,14 @@
+package OOAD.ch01.encapsulation;
+
+public enum Type {
+
+  ACOUSTIC, ELECTRIC;
+
+  public String toString() {
+    switch(this) {
+      case ACOUSTIC: return "acoustic";
+      case ELECTRIC: return "electric";
+      default:       return "unspecified";
+    }
+  }
+}
