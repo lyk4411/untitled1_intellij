27ab72694a6cadd9f7b599ddd350aa2261fea0ab
ykliu <moneyflying_2006@hotmail.com>
2019-01-30 15:03:20 +0800

    core java ch 8

---
+++ src/main/java/OOAD/ch04/randy/BarkRecognizer.java
@@ -0,0 +1,20 @@
+package OOAD.ch04.randy;
+
+public class BarkRecognizer {
+
+  private DogDoor door;
+
+  public BarkRecognizer(DogDoor door) {
+    this.door = door;
+  }
+
+  public void recognize(String bark) {
+    System.out.println("   BarkRecognizer: Heard a '" +
+        bark + "'");
+    if (door.getAllowedBark().equals(bark)) {
+      door.open();
+    } else {
+      System.out.println("This dog is not allowed.");
+    }
+  }
+}
