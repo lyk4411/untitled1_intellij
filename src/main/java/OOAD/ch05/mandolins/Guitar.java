c0ce517d155c1422e01fb01b50f21d3bc42a5689
ykliu <moneyflying_2006@hotmail.com>
2019-01-30 15:49:56 +0800

    core java ch 8

---
+++ src/main/java/OOAD/ch05/mandolins/Guitar.java
@@ -0,0 +1,8 @@
+package OOAD.ch05.mandolins;
+
+public class Guitar extends Instrument {
+ 
+  public Guitar(String serialNumber, double price, GuitarSpec spec) {
+    super(serialNumber, price, spec);
+  }
+}
---
+++ src/main/java/OOAD/ch05/mandolins/Type.java
@@ -0,0 +1,14 @@
+package OOAD.ch05.mandolins;
+
+public enum Type {
+
+  ACOUSTIC, ELECTRIC;
+
+  public String toString() {
+    switch(this) {
+      case ACOUSTIC: return "acoustic";
+      case ELECTRIC: return "electric";
+      default:       return "unspecified";
+    }
+  }
+}
