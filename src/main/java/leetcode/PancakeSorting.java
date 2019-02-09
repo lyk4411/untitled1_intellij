428e0dcf1e594ca9a39b0710871cad9267d3b775
ykliu <moneyflying_2006@hotmail.com>
2019-02-09 10:21:08 +0800

    core java ch 8

--- src/main/java/leetcode/PancakeSorting.java
+++ src/main/java/leetcode/PancakeSorting.java
@@ -10,29 +10,38 @@ import java.util.List;
  * Porject name: untitled1
  */
 public class PancakeSorting {
-    public List<Integer> pancakeSort(int[] A) {
-        List<Integer> ans = new ArrayList();
-        int N = A.length;
-
-        Integer[] B = new Integer[N];
-        for (int i = 0; i < N; ++i)
-            B[i] = i+1;
-        Arrays.sort(B, (i, j) -> A[j-1] - A[i-1]);
-
-        for (int i: B) {
-            for (int f: ans)
-                if (i <= f)
-                    i = f+1 - i;
-            ans.add(i);
-            ans.add(N--);
+    public void reverse(int[] A, int l, int r) {
+        while (l < r) {
+            int tmp = A[l];
+            A[l] = A[r];
+            A[r] = tmp;
+            l++;
+            r--;
         }
+    }
 
-        return ans;
+    public List<Integer> pancakeSort(int[] A) {
+        List<Integer> res = new ArrayList();
+        int[] Abackup = new int[A.length];
+        for (int i = 0; i < Abackup.length; i++) Abackup[i] = A[i];
+        Arrays.sort(Abackup);
+        for (int i = Abackup.length - 1; i >= 0; i--) {
+            int j = Abackup.length - 1;
+            for (; j >= 0; j--) if (A[j] == Abackup[i]) break;
+            if (i == j) continue;
+            if (j != 0) {
+                res.add(j + 1);
+                reverse(A, 0, j);
+            }
+            res.add(i + 1);
+            reverse(A, 0, i);
+        }
+        return res;
     }
 
     public static void main(String[] args) {
         PancakeSorting ps = new PancakeSorting();
-        System.out.println(ps.pancakeSort(new int[]{3,2,4,1}));
-        System.out.println(ps.pancakeSort(new int[]{1,2,3,4}));
+        System.out.println(ps.pancakeSort(new int[]{3, 2, 4, 1}));
+        System.out.println(ps.pancakeSort(new int[]{1, 2, 3, 4}));
     }
 }
