308f37c3837d9e6f07d4f899eb41ed09e0c06d91
ykliu <moneyflying_2006@hotmail.com>
2019-02-13 16:53:06 +0800

    core java ch 8

---
+++ src/main/java/java8inaction/ch06/tReduce.java
@@ -0,0 +1,35 @@
+package java8inaction.ch06;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/**
+ * Created by lyk on 2019-2-13.
+ * Package name: java8inaction.ch06
+ * Porject name: untitled1
+ */
+public class tReduce {
+    public static void main(String[] args) {
+        Stream<Integer> stream = Arrays.asList(1,2,3,4,5,6,7,8).stream();
+        myList<Integer> numbers = stream.reduce(
+                new myList<>(),
+                 (myList<Integer> l, Integer e) -> {
+                     System.out.println("l:" + l + " and e:" + e);
+                     l.add(e);
+                     return l;
+                 },
+                (myList<Integer> l1, myList<Integer> l2) -> {
+                    return new myList<>();
+                }
+
+        );
+        System.out.println(numbers);
+    }
+    static class myList<Integer> extends ArrayList<Integer>{
+        myList(){
+            super();
+            System.out.println("init...");
+        }
+    }
+}
