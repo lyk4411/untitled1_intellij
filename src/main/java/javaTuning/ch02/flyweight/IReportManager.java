92f3a740a2e962c1cbed898256905969666f1b9a
ykliu <moneyflying_2006@hotmail.com>
2019-03-04 14:07:13 +0800

    core java ch 8

---
+++ src/main/java/javaTuning/ch02/flyweight/IReportManager.java
@@ -0,0 +1,5 @@
+package javaTuning.ch02.flyweight;
+
+public interface IReportManager {
+	public String createReport();
+}
---
+++ src/main/java/javaTuning/ch02/flyweight/Main.java
@@ -0,0 +1,10 @@
+package javaTuning.ch02.flyweight;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ReportManagerFactory rmf=new ReportManagerFactory();
+		IReportManager rm=rmf.getFinancialReportManager("A");
+		System.out.println(rm.createReport());
+	}
+}
