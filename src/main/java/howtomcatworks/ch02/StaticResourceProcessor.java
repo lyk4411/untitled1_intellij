b3817bf919ef1ebc4b3ae4f374312094bfdabecf
ykliu <moneyflying_2006@hotmail.com>
2019-03-11 16:18:51 +0800

    core java ch 8

---
+++ src/main/java/howtomcatworks/ch02/StaticResourceProcessor.java
@@ -0,0 +1,14 @@
+package howtomcatworks.ch02;
+
+import java.io.IOException;
+
+public class StaticResourceProcessor {
+
+  public void process(Request request, Response response) {
+    try {
+      response.sendStaticResource();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
