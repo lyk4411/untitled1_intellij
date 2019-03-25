04fae675fd578c5a59833c0fb86701ac110acdb1
ykliu <moneyflying_2006@hotmail.com>
2019-03-25 11:23:01 +0800

    core java ch 8

--- src/main/java/leetcode/DistributeCoinsinBinaryTree.java
+++ src/main/java/leetcode/DistributeCoinsinBinaryTree.java
@@ -6,6 +6,11 @@ package leetcode;
  * Porject name: untitled1
  */
 public class DistributeCoinsinBinaryTree {
+//    If the leaf of a tree has 0 coins (an excess of -1 from what it needs), then we should
+// push a coin from its parent onto the leaf. If it has say, 4 coins (an excess of 3), then
+// we should push 3 coins off the leaf. In total, the number of moves from that leaf to or
+// from its parent is excess = Math.abs(num_coins - 1). Afterwards, we never have to consider
+// this leaf again in the rest of our calculation.
     int ans;
     public int distributeCoins(TreeNode root) {
         ans = 0;
