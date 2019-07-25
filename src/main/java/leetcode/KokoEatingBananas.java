a54165fd0c24bfdbf056cc6f6f9021ea54d8a0d3
ykliu <moneyflying_2006@hotmail.com>
2019-07-26 07:59:43 +0800

    core java ch 8

---
+++ src/main/java/leetcode/KokoEatingBananas.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.stream.IntStream;
+
+/**
+ * Created by lyk on 2019-7-26.
+ * Package name: leetcode
+ * Porject name: untitled1
+ */
+public class KokoEatingBananas {
+    public int minEatingSpeed(int[] piles, int H) {
+        int lo = 1;
+        int hi = IntStream.of(piles).max().getAsInt();
+        while (lo < hi) {
+            int mi = (lo + hi) / 2;
+            if (!possible(piles, H, mi))
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+
+        return lo;
+    }
+
+    // Can Koko eat all bananas in H hours with eating speed K?
+    public boolean possible(int[] piles, int H, int K) {
+        int time = 0;
+        for (int p: piles)
+            time += (p-1) / K + 1;
+        return time <= H;
+    }
+
+    public static void main(String[] args) {
+        KokoEatingBananas keb = new KokoEatingBananas();
+        int[] piles1 = new int[]{3,6,7,11};
+        int H1 = 8;
+        System.out.println(keb.minEatingSpeed(piles1, H1));
+    }
+}
