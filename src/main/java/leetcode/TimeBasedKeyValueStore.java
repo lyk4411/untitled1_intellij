1d6d94407d276877715ed498467703e59f7c8016
ykliu <moneyflying_2006@hotmail.com>
2019-07-26 15:38:47 +0800

    core java ch 8

--- src/main/java/leetcode/TimeBasedKeyValueStore.java
+++ src/main/java/leetcode/TimeBasedKeyValueStore.java
@@ -1,8 +1,8 @@
 package leetcode;
 
-import javafx.util.Pair;
-
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by lyk on 2019-7-26.
@@ -10,7 +10,7 @@ import java.util.*;
  * Porject name: untitled1
  */
 public class TimeBasedKeyValueStore {
-    Map<String, List<Pair<Integer, String>>> M;
+    Map<String, TreeMap<Integer, String>> M;
 
     public TimeBasedKeyValueStore() {
         M = new HashMap();
@@ -18,29 +18,49 @@ public class TimeBasedKeyValueStore {
 
     public void set(String key, String value, int timestamp) {
         if (!M.containsKey(key))
-            M.put(key, new ArrayList<Pair<Integer, String>>());
+            M.put(key, new TreeMap());
 
-        M.get(key).add(new Pair(timestamp, value));
+        M.get(key).put(timestamp, value);
     }
 
     public String get(String key, int timestamp) {
         if (!M.containsKey(key)) return "";
 
-        List<Pair<Integer, String>> A = M.get(key);
-//        如果搜索键包含在列表中，则返回搜索键的索引；否则返回 (-(插入点) - 1)。插入点
-//        被定义为将键插入列表的那一点：即第一个大于此键的元素索引；如果列表中的所有元素
-//        都小于指定的键，则为 list.size()。注意，这保证了当且仅当此键被找到时，返回的
-//        值将 >= 0。
-        int i = Collections.binarySearch(A, new Pair<Integer, String>(timestamp, "{"),
-                (a, b) -> Integer.compare(a.getKey(), b.getKey()));
-
-        if (i >= 0)
-            return A.get(i).getValue();
-        else if (i == -1)
-            return "";
-        else
-            return A.get(-i-2).getValue();
+        TreeMap<Integer, String> tree = M.get(key);
+        Integer t = tree.floorKey(timestamp);
+        return t != null ? tree.get(t) : "";
     }
+//    Map<String, List<Pair<Integer, String>>> M;
+//
+//    public TimeBasedKeyValueStore() {
+//        M = new HashMap();
+//    }
+//
+//    public void set(String key, String value, int timestamp) {
+//        if (!M.containsKey(key))
+//            M.put(key, new ArrayList<Pair<Integer, String>>());
+//
+//        M.get(key).add(new Pair(timestamp, value));
+//    }
+//
+//    public String get(String key, int timestamp) {
+//        if (!M.containsKey(key)) return "";
+//
+//        List<Pair<Integer, String>> A = M.get(key);
+////        如果搜索键包含在列表中，则返回搜索键的索引；否则返回 (-(插入点) - 1)。插入点
+////        被定义为将键插入列表的那一点：即第一个大于此键的元素索引；如果列表中的所有元素
+////        都小于指定的键，则为 list.size()。注意，这保证了当且仅当此键被找到时，返回的
+////        值将 >= 0。
+//        int i = Collections.binarySearch(A, new Pair<Integer, String>(timestamp, "{"),
+//                (a, b) -> Integer.compare(a.getKey(), b.getKey()));
+//
+//        if (i >= 0)
+//            return A.get(i).getValue();
+//        else if (i == -1)
+//            return "";
+//        else
+//            return A.get(-i-2).getValue();
+//    }
 
     public static void main(String[] args) {
         TimeBasedKeyValueStore kv = new TimeBasedKeyValueStore();
