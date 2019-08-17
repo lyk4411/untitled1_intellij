2065419078b1694521f20bd8e417b72ecf23755d
ykliu <moneyflying_2006@hotmail.com>
2019-08-17 11:40:06 +0800

    core java ch 8

--- src/main/java/leetcode/OnlineElection.java
+++ src/main/java/leetcode/OnlineElection.java
@@ -1,9 +1,6 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by lyk on 2019-8-17.
@@ -11,59 +8,20 @@ import java.util.Map;
  * Porject name: untitled1
  */
 public class OnlineElection {
-    List<List<Vote>> A;
-
+    public TreeMap<Integer, Integer> tm = new TreeMap<>();
     public OnlineElection(int[] persons, int[] times) {
-        A = new ArrayList();
-        Map<Integer, Integer> count = new HashMap();
-        for (int i = 0; i < persons.length; ++i) {
-            int p = persons[i], t = times[i];
-            int c = count.getOrDefault(p, 0) + 1;
-
-            count.put(p, c);
-            while (A.size() <= c)
-                A.add(new ArrayList<Vote>());
-            A.get(c).add(new Vote(p, t));
+        int[] count = new int[persons.length];
+        for (int i = 0, max = -1; i < times.length; ++i) {
+            ++count[persons[i]];
+            if (max <= count[persons[i]]) {
+                max=count[persons[i]];
+                tm.put(times[i], persons[i]);
+            }
         }
     }
-
     public int q(int t) {
-        // Binary search on A[i][0].time for smallest i
-        // such that A[i][0].time > t
-        int lo = 1, hi = A.size();
-        while (lo < hi) {
-            int mi = lo + (hi - lo) / 2;
-            if (A.get(mi).get(0).time <= t)
-                lo = mi + 1;
-            else
-                hi = mi;
-        }
-        int i = lo - 1;
-
-        // Binary search on A[i][j].time for smallest j
-        // such that A[i][j].time > t
-        lo = 0;
-        hi = A.get(i).size();
-        while (lo < hi) {
-            int mi = lo + (hi - lo) / 2;
-            if (A.get(i).get(mi).time <= t)
-                lo = mi + 1;
-            else
-                hi = mi;
-        }
-        int j = Math.max(lo - 1, 0);
-        return A.get(i).get(j).person;
+        return tm.floorEntry(t).getValue();
     }
-
-    class Vote {
-        int person, time;
-
-        Vote(int p, int t) {
-            person = p;
-            time = t;
-        }
-    }
-
     public static void main(String[] args) {
         OnlineElection oe = new OnlineElection(
                 new int[]{0,1,1,0,0,1,0},
