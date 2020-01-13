b775a6f0bfee6c820e564b61c69a6fbd85905d3c
ykliu <moneyflying_2006@hotmail.com>
2020-01-13 10:35:43 +0800

    core java ch 8

--- src/main/java/leetcode/RemoveCoveredIntervals.java
+++ src/main/java/leetcode/RemoveCoveredIntervals.java
@@ -13,7 +13,6 @@ public class RemoveCoveredIntervals {
         Arrays.sort(A, (a, b) -> a[0] - b[0]);
         for (int[] v : A) {
             if (v[0] > left && v[1] > right) {
-                left = v[0];
                 ++res;
             }
             right = Math.max(right, v[1]);
