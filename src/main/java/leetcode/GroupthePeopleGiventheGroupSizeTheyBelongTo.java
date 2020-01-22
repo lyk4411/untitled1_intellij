7a3e9ff784faa03c04a40cfdc0ba53af3344bb09
ykliu <moneyflying_2006@hotmail.com>
2020-01-22 16:58:04 +0800

    core java ch 8

--- src/main/java/leetcode/GroupthePeopleGiventheGroupSizeTheyBelongTo.java
+++ src/main/java/leetcode/GroupthePeopleGiventheGroupSizeTheyBelongTo.java
@@ -35,7 +35,9 @@ public class GroupthePeopleGiventheGroupSizeTheyBelongTo {
         GroupthePeopleGiventheGroupSizeTheyBelongTo gp = new GroupthePeopleGiventheGroupSizeTheyBelongTo();
         int[] groupSizes1 = new int[]{3,3,3,3,3,1,3};
         int[] groupSizes2 = new int[]{2,1,3,3,3,2};
+        int[] groupSizes3 = new int[]{4,4,4,4,4,4,4,4,4,4,4,4};
         System.out.println(gp.groupThePeople(groupSizes1));
         System.out.println(gp.groupThePeople(groupSizes2));
+        System.out.println(gp.groupThePeople(groupSizes3));
     }
 }
